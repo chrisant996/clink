ca22832528e59b1ea9541c3eb5abd67a8925e99d
Martin Ridgers <martin.ridgers+clink@gmail.com>
2012-08-13 00:37:02 +0200

    Added basic Perforce (p4) completion.

---
+++ clink/dll/clink_p4.lua
@@ -0,0 +1,39 @@
+--
+-- Copyright (c) 2012 Martin Ridgers
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--
+
+--------------------------------------------------------------------------------
+local p4_tree = {
+    "add", "annotate", "attribute", "branch", "branches", "browse", "change",
+    "changes", "changelist", "changelists", "client", "clients", "copy",
+    "counter", "counters", "cstat", "delete", "depot", "depots", "describe",
+    "diff", "diff2", "dirs", "edit", "filelog", "files", "fix", "fixes",
+    "flush", "fstat", "grep", "group", "groups", "have", "help", "info",
+    "integrate", "integrated", "interchanges", "istat", "job", "jobs", "label",
+    "labels", "labelsync", "legal", "list", "lock", "logger", "login",
+    "logout", "merge", "move", "opened", "passwd", "populate", "print",
+    "protect", "protects", "reconcile", "rename", "reopen", "resolve",
+    "resolved", "revert", "review", "reviews", "set", "shelve", "status",
+    "sizes", "stream", "streams", "submit", "sync", "tag", "tickets", "unlock",
+    "unshelve", "update", "user", "users", "where", "workspace", "workspaces"
+}
+
+clink.arg.register_tree("p4", p4_tree)
