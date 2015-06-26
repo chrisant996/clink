7ea67e51e27ad6106b08da3ce5a171a8189b57cc
Martin Ridgers <martin.ridgers+clink@gmail.com>
2015-06-26 22:25:59 +0200

    Added get_state() method.

--- clink/lib/lua/lua_root.h
+++ clink/lib/lua/lua_root.h
@@ -24,19 +24,28 @@
 #include "clink_lua_api.h"
 #include "lua_match_generator.h"
 
+struct lua_State;
+
 //------------------------------------------------------------------------------
 class lua_root
     : public lua_match_generator
 {
 public:
-                        lua_root();
-                        ~lua_root();
-    void                initialise();
-    bool                do_string(const char* string);
-    bool                do_file(const char* path);
+                     lua_root();
+                     ~lua_root();
+    void             initialise();
+    bool             do_string(const char* string);
+    bool             do_file(const char* path);
+    lua_State*       get_state() const;
 
 private:
-    void                shutdown();
-    struct lua_State*   m_state;
-    clink_lua_api       m_clink;
+    void             shutdown();
+    lua_State*       m_state;
+    clink_lua_api    m_clink;
 };
+
+//------------------------------------------------------------------------------
+inline lua_State* lua_root::get_state() const
+{
+    return m_state;
+}
