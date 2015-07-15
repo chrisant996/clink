9572f251cb95c6dc301969a40b27e9942b6e0b74
Martin Ridgers <martin.ridgers+clink@gmail.com>
2015-07-15 19:58:35 +0200

    Tidy up.

--- clink/dll/process/hook.cpp
+++ clink/dll/process/hook.cpp
@@ -52,41 +52,37 @@ void* hook_iat(
     int find_by_name
 )
 {
-    void* func_addr;
-    void* prev_addr;
-    void** imp;
-
     LOG("Attempting to hook IAT for module %p", base);
     LOG("Target is %s,%s (by_name=%d)", dll, func_name, find_by_name);
 
+    void** import;
+
     // Find entry and replace it.
     if (find_by_name)
-    {
-        imp = get_import_by_name(base, nullptr, func_name);
-    }
+        import = get_import_by_name(base, nullptr, func_name);
     else
     {
         // Get the address of the function we're going to hook.
-        func_addr = get_proc_addr(dll, func_name);
+        void* func_addr = get_proc_addr(dll, func_name);
         if (func_addr == nullptr)
         {
             LOG("Failed to find function '%s' in '%s'", func_name, dll);
             return nullptr;
         }
 
-        imp = get_import_by_addr(base, nullptr, func_addr);
+        import = get_import_by_addr(base, nullptr, func_addr);
     }
 
-    if (imp == nullptr)
+    if (import == nullptr)
     {
         LOG("Unable to find import in IAT (by_name=%d)", find_by_name);
         return nullptr;
     }
 
-    LOG("Found import at %p (value = %p)", imp, *imp);
+    LOG("Found import at %p (value = %p)", import, *import);
 
-    prev_addr = *imp;
-    write_addr(imp, hook);
+    void* prev_addr = *import;
+    write_addr(import, hook);
 
     FlushInstructionCache(current_proc(), 0, 0);
     return prev_addr;
@@ -95,30 +91,22 @@ void* hook_iat(
 //------------------------------------------------------------------------------
 static char* alloc_trampoline(void* hint)
 {
-    static const int size = 0x100;
-    void* trampoline;
-    void* vm_alloc_base;
-    char* tramp_page;
     SYSTEM_INFO sys_info;
-
     GetSystemInfo(&sys_info);
 
-    do
+    void* trampoline = nullptr;
+    while (trampoline == nullptr)
     {
-        vm_alloc_base = get_alloc_base(hint);
+        void* vm_alloc_base = get_alloc_base(hint);
         vm_alloc_base = vm_alloc_base ? vm_alloc_base : hint;
-        tramp_page = (char*)vm_alloc_base - sys_info.dwPageSize;
 
-        trampoline = VirtualAlloc(
-            tramp_page,
-            sys_info.dwPageSize,
-            MEM_COMMIT|MEM_RESERVE,
-            PAGE_EXECUTE_READWRITE
-        );
+        char* tramp_page = (char*)vm_alloc_base - sys_info.dwAllocationGranularity;
+
+        trampoline = VirtualAlloc(tramp_page, sys_info.dwPageSize,
+            MEM_COMMIT|MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 
         hint = tramp_page;
     }
-    while (trampoline == nullptr);
 
     return (char*)trampoline;
 }
@@ -126,7 +114,6 @@ static char* alloc_trampoline(void* hint)
 //------------------------------------------------------------------------------
 static int get_mask_size(unsigned mask)
 {
-    // Just for laughs, a sledgehammer for a nut.
     mask &= 0x01010101;
     mask += mask >> 16;
     mask += mask >> 8;
@@ -136,16 +123,15 @@ static int get_mask_size(unsigned mask)
 //------------------------------------------------------------------------------
 static char* write_rel_jmp(char* write, void* dest)
 {
-    intptr_t disp;
+    // jmp <displacement>
+    intptr_t disp = (intptr_t)dest;
+    disp -= (intptr_t)write;
+    disp -= 5;
+
     struct {
         char a;
         char b[4];
     } buffer;
-
-    // jmp <displacement>
-    disp = (intptr_t)dest;
-    disp -= (intptr_t)write;
-    disp -= 5;
 
     buffer.a = (unsigned char)0xe9;
     *(int*)buffer.b = (int)disp;
@@ -162,21 +148,10 @@ static char* write_rel_jmp(char* write, void* dest)
 //------------------------------------------------------------------------------
 static char* write_trampoline_out(char* write, void* to_hook, void* hook)
 {
-    struct {
-        char a[2];
-        char b[4];
-        char c[sizeof(void*)];
-    } inst;
-    short temp;
-    unsigned rel_addr;
-    int i;
-    char* patch;
-
-    rel_addr = 0;
-    patch = (char*)to_hook - 5;
+    char* patch = (char*)to_hook - 5;
 
     // Check we've got a nop slide or int3 block to patch into.
-    for (i = 0; i < 5; ++i)
+    for (int i = 0; i < 5; ++i)
     {
         unsigned char c = patch[i];
         if (c != 0x90 && c != 0xcc)
@@ -188,7 +163,7 @@ static char* write_trampoline_out(char* write, void* to_hook, void* hook)
 
     // Patch the API.
     patch = write_rel_jmp(patch, write);
-    temp = (unsigned short)0xf9eb;
+    short temp = (unsigned short)0xf9eb;
     if (!write_vm(current_proc(), patch, &temp, sizeof(temp)))
     {
         LOG("VM write to %p failed (err = %d)", patch, GetLastError());
@@ -196,8 +171,15 @@ static char* write_trampoline_out(char* write, void* to_hook, void* hook)
     }
 
     // Long jmp.
+    struct {
+        char a[2];
+        char b[4];
+        char c[sizeof(void*)];
+    } inst;
+
     *(short*)inst.a = 0x25ff;
 
+    unsigned rel_addr = 0;
 #ifdef _M_IX86
     rel_addr = (intptr_t)write + 6;
 #endif
@@ -217,16 +199,14 @@ static char* write_trampoline_out(char* write, void* to_hook, void* hook)
 //------------------------------------------------------------------------------
 static char* write_trampoline_in(char* write, void* to_hook, int n)
 {
-    int i;
-
-    // Copy
-    for (i = 0; i < n; ++i)
+    for (int i = 0; i < n; ++i)
     {
         if (!write_vm(current_proc(), write, (char*)to_hook + i, 1))
         {
             LOG("VM write to %p failed (err = %d)", write, GetLastError());
             return nullptr;
         }
+
         ++write;
     }
 
@@ -248,16 +228,10 @@ static char* write_trampoline_in(char* write, void* to_hook, int n)
 //------------------------------------------------------------------------------
 static int get_instruction_length(void* addr)
 {
-    unsigned prolog;
-    int i;
-
-    struct asm_tag_t
-    {
+    static const struct {
         unsigned expected;
         unsigned mask;
-    };
-
-    struct asm_tag_t asm_tags[] = {
+    } asm_tags[] = {
 #ifdef _M_X64
         { 0x38ec8348, 0xffffffff },  // sub rsp,38h
         { 0x0000f3ff, 0x0000ffff },  // push rbx
@@ -270,30 +244,22 @@ static int get_instruction_length(void* addr)
         { 0x000000e9, 0x000000ff },  // jmp addr32        = 5-byte length
     };
 
-    prolog = *(unsigned*)(addr);
-    for (i = 0; i < sizeof_array(asm_tags); ++i)
+    unsigned prolog = *(unsigned*)(addr);
+    for (int i = 0; i < sizeof_array(asm_tags); ++i)
     {
-        int length;
         unsigned expected = asm_tags[i].expected;
         unsigned mask = asm_tags[i].mask;
 
         if (expected != (prolog & mask))
-        {
             continue;
-        }
 
-        length = get_mask_size(mask);
+        int length = get_mask_size(mask);
 
         // Checks for instructions that "expected" only partially matches.
         if (expected == 0x0000b848)
-        {
             length = 10;
-        }
         else if (expected == 0xe9)
-        {
-            // jmp [imm32]
-            length = 5;
-        }
+            length = 5; // jmp [imm32]
 
         LOG("Matched prolog %08X (mask = %08X)", prolog, mask);
         return length;
@@ -305,7 +271,6 @@ static int get_instruction_length(void* addr)
 //------------------------------------------------------------------------------
 static void* follow_jump(void* addr)
 {
-    void* dest = addr;
     unsigned char* t = (unsigned char*)addr;
     int* imm = (int*)(t + 2);
 
@@ -317,6 +282,7 @@ static void* follow_jump(void* addr)
     if ((t[1] & 070) != 040)
         return addr;
 
+    void* dest = addr;
     switch (t[1] & 007)
     {
     case 5:
@@ -337,8 +303,6 @@ static void* follow_jump(void* addr)
 static void* hook_jmp_impl(void* to_hook, void* hook)
 {
     struct region_info_t region_info;
-    char* trampoline;
-    char* write;
     int inst_len;
 
     LOG("Attempting to hook at %p with %p", to_hook, hook);
@@ -355,7 +319,7 @@ static void* hook_jmp_impl(void* to_hook, void* hook)
     }
 
     // Prepare
-    trampoline = write = alloc_trampoline(to_hook);
+    char* trampoline = alloc_trampoline(to_hook);
     if (trampoline == nullptr)
     {
         LOG("Failed to allocate a page for trampolines.");
@@ -363,7 +327,7 @@ static void* hook_jmp_impl(void* to_hook, void* hook)
     }
 
     // In
-    write = write_trampoline_in(trampoline, to_hook, inst_len);
+    char* write = write_trampoline_in(trampoline, to_hook, inst_len);
     if (write == nullptr)
     {
         LOG("Failed to write trampoline in.");
