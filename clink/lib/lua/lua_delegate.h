b73091425b71cad606b859cc94c56a3fbc88f101
Martin Ridgers <martin.ridgers+clink@gmail.com>
2015-09-13 22:11:50 +0200

    Repurpose lua_delegate as lua_bindable for scoped binding of objects.

--- clink/lib/lua/lua_delegate.h
+++ clink/lib/lua/lua_delegate.h
@@ -9,52 +9,119 @@ extern "C" {
 }
 
 //------------------------------------------------------------------------------
-class lua_delegate
+template <class T>
+class lua_bindable
 {
 public:
-    template <class T>
-    static void         push(lua_State* state, T* t, int (T::*m)(lua_State*));
+    typedef int         (T::*method_t)(lua_State*);
+
+    struct method
+    {
+        const char*     name;
+        method_t        ptr;
+    };
+
+                        lua_bindable(const char* name, const method* methods);
+    void                lua_bind(lua_State* state);
+    void                lua_unbind(lua_State* state);
+    bool                lua_push(lua_State* state);
 
 private:
-    typedef int         (lua_delegate::*method_t)(lua_State*);
-    lua_delegate*       self;
-    method_t            method;
+
     static int          call(lua_State* state);
-    static const char*  get_metatable_name();
+    const char*         m_name;
+    const method*       m_methods;
+    int                 m_registry_ref;
 };
 
 //------------------------------------------------------------------------------
 template <class T>
-void lua_delegate::push(lua_State* state, T* t, int (T::*m)(lua_State*))
+lua_bindable<T>::lua_bindable(const char* name, const method* methods)
+: m_methods(methods)
+, m_name(name)
+, m_registry_ref(LUA_REFNIL)
 {
-    if (luaL_newmetatable(state, get_metatable_name()))
-    {
-        lua_pushcfunction(state, &lua_delegate::call);
-        lua_setfield(state, -2, "__call");
-    }
-    lua_pop(state, 1);
-
-    auto* d = (lua_delegate*)lua_newuserdata(state, sizeof(lua_delegate));
-    luaL_setmetatable(state, get_metatable_name());
-
-    d->self = (lua_delegate*)t;
-    d->method = (method_t)m;
 }
 
 //------------------------------------------------------------------------------
-inline int lua_delegate::call(lua_State* state)
+template <class T>
+void lua_bindable<T>::lua_bind(lua_State* state)
 {
-    void* addr = luaL_checkudata(state, 1, get_metatable_name());
-    const auto* d = (lua_delegate*)addr;
-    if (d == nullptr)
+    void* self = lua_newuserdata(state, sizeof(void*));
+    *(void**)self = this;
+
+    if (luaL_newmetatable(state, m_name))
+    {
+        lua_createtable(state, 0, 0);
+
+        auto* methods = m_methods;
+        while (methods != nullptr && methods->name != nullptr)
+        {
+            auto* ptr = (method_t*)lua_newuserdata(state, sizeof(method_t));
+            *ptr = methods->ptr;
+
+            if (luaL_newmetatable(state, "lua_bindable"))
+            {
+                lua_pushcfunction(state, &lua_bindable<T>::call);
+                lua_setfield(state, -2, "__call");
+            }
+
+            lua_setmetatable(state, -2);
+            lua_setfield(state, -2, methods->name);
+
+            ++methods;
+        }
+
+        lua_setfield(state, -2, "__index");
+    }
+
+    lua_setmetatable(state, -2);
+    m_registry_ref = luaL_ref(state, LUA_REGISTRYINDEX);
+}
+
+//------------------------------------------------------------------------------
+template <class T>
+void lua_bindable<T>::lua_unbind(lua_State* state)
+{
+    if (m_registry_ref == LUA_REFNIL)
+        return;
+
+    lua_rawgeti(state, LUA_REGISTRYINDEX, m_registry_ref);
+    if (void* self = lua_touserdata(state, -1))
+        *(void**)self = nullptr;
+    lua_pop(state, 1);
+
+    luaL_unref(state, LUA_REGISTRYINDEX, m_registry_ref);
+    m_registry_ref = LUA_REFNIL;
+}
+
+//------------------------------------------------------------------------------
+template <class T>
+bool lua_bindable<T>::lua_push(lua_State* state)
+{
+    if (m_registry_ref != LUA_REFNIL)
+    {
+        lua_rawgeti(state, LUA_REGISTRYINDEX, m_registry_ref);
+        return true;
+    }
+
+    return false;
+}
+
+//------------------------------------------------------------------------------
+template <class T>
+int lua_bindable<T>::call(lua_State* state)
+{
+    auto* const* self = (T* const*)lua_touserdata(state, 2);
+    if (self == nullptr || *self == nullptr)
+        return 0;
+
+    auto* const ptr = (method_t const*)lua_touserdata(state, 1);
+    if (ptr == nullptr)
         return 0;
 
     lua_remove(state, 1);
-    return ((d->self)->*(d->method))(state);
-}
+    lua_remove(state, 1);
 
-//------------------------------------------------------------------------------
-inline const char* lua_delegate::get_metatable_name()
-{
-    return "_luadel_mt";
+    return ((*self)->*(*ptr))(state);
 }
