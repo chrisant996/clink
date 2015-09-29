d2ce7b7bb340867b096f68fcda49984fc51fcf20
Martin Ridgers <martin.ridgers+clink@gmail.com>
2015-09-29 20:54:55 +0200

    Comment tweak.

--- clink/lib/file_match_generator.cpp
+++ clink/lib/file_match_generator.cpp
@@ -24,7 +24,7 @@ bool file_match_generator::generate(const line_state& line, matches_builder& bui
 {
     str<MAX_PATH> buffer;
 
-    // Get the path to match files from.
+    // Get the path to match files from, append a slash.
     buffer = line.word;
     path::get_directory(buffer);
     path::append(buffer, "");
