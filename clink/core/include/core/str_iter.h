2319100bdec1e89523a2f419755a58bbb60637be
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-01-04 21:56:07 +0100

    Expand inline methods out.

--- clink/core/include/core/str_iter.h
+++ clink/core/include/core/str_iter.h
@@ -6,17 +6,29 @@
 #include "str.h"
 
 //------------------------------------------------------------------------------
-template <typename TYPE>
+template <typename T>
 class str_iter_impl
 {
 public:
-                str_iter_impl(const TYPE* s) : m_ptr(s) {}
-                str_iter_impl(const str_impl<TYPE>& s) : m_ptr(s.c_str()) {}
+                str_iter_impl(const T* s);
+                str_iter_impl(const str_impl<T>& s);
     int         next();
 
 private:
-    const TYPE* m_ptr;
+    const T*    m_ptr;
 };
+
+//------------------------------------------------------------------------------
+template <typename T> str_iter_impl<T>::str_iter_impl(const T* s)
+: m_ptr(s)
+{
+}
+
+//------------------------------------------------------------------------------
+template <typename T> str_iter_impl<T>::str_iter_impl(const str_impl<T>& s)
+: m_ptr(s.c_str())
+{
+}
 
 //------------------------------------------------------------------------------
 typedef str_iter_impl<char>     str_iter;
