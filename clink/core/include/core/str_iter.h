e4e3bbcff939003e09bbbff94df1983e87929ce7
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-01-04 21:57:12 +0100

    Added method to return iterator's current location.

--- clink/core/include/core/str_iter.h
+++ clink/core/include/core/str_iter.h
@@ -12,6 +12,7 @@ class str_iter_impl
 public:
                 str_iter_impl(const T* s);
                 str_iter_impl(const str_impl<T>& s);
+    const T*    get_pointer() const;
     int         next();
 
 private:
@@ -29,6 +30,12 @@ template <typename T> str_iter_impl<T>::str_iter_impl(const str_impl<T>& s)
 : m_ptr(s.c_str())
 {
 }
+
+//------------------------------------------------------------------------------
+template <typename T> const T* str_iter_impl<T>::get_pointer() const
+{
+    return m_ptr;
+};
 
 //------------------------------------------------------------------------------
 typedef str_iter_impl<char>     str_iter;
