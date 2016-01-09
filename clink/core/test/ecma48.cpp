edecd1cf6e699e847deed8f54cc1eae80f557d3d
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-01-09 23:37:15 +0100

    Split ECMA-48 tests into individual cases rather than sections.

--- clink/core/test/ecma48.cpp
+++ clink/core/test/ecma48.cpp
@@ -6,161 +6,178 @@
 #include <core/ecma48_iter.h>
 #include <core/base.h>
 
+static ecma48_state g_state;
+
 TEST_CASE("Ecma48 chars") {
-    ecma48_state state;
+    const char* input = "abc123";
+
     ecma48_code code;
 
-    SECTION("chars") {
-        const char* input = "abc123";
+    ecma48_iter iter(input, g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_chars);
+    REQUIRE(code.str == input);
+    REQUIRE(code.length == 6);
 
-        ecma48_iter iter(input, state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_chars);
-        REQUIRE(code.str == input);
-        REQUIRE(code.length == 6);
+    REQUIRE(iter.next(code) == false);
+}
 
-        REQUIRE(iter.next(code) == false);
-    }
+TEST_CASE("Ecma48 c0") {
+    ecma48_code code;
 
-    SECTION("c0") {
-        ecma48_iter iter("\x01 \x10\x1f", state);
+    ecma48_iter iter("\x01 \x10\x1f", g_state);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c0);
-        REQUIRE(code.c0 == 0x01);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c0);
+    REQUIRE(code.c0 == 0x01);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_chars);
-        REQUIRE(code.length == 1);
-        REQUIRE(code.str[0] == ' ');
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_chars);
+    REQUIRE(code.length == 1);
+    REQUIRE(code.str[0] == ' ');
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c0);
-        REQUIRE(code.c0 == 0x10);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c0);
+    REQUIRE(code.c0 == 0x10);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c0);
-        REQUIRE(code.c0 == 0x1f);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c0);
+    REQUIRE(code.c0 == 0x1f);
 
-        REQUIRE(iter.next(code) == false);
-    }
+    REQUIRE(iter.next(code) == false);
+}
 
-    SECTION("c1") {
-        ecma48_iter iter("\x1b\x40\x1b\x50\x1b\x5f", state);
+TEST_CASE("Ecma48 c1") {
+    ecma48_code code;
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c1);
-        REQUIRE(code.c1 == 0x40);
+    ecma48_iter iter("\x1b\x40\x1b\x50\x1b\x5f", g_state);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c1);
-        REQUIRE(code.c1 == 0x50);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c1);
+    REQUIRE(code.c1 == 0x40);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c1);
-        REQUIRE(code.c1 == 0x5f);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c1);
+    REQUIRE(code.c1 == 0x50);
 
-        REQUIRE(iter.next(code) == false);
-    }
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c1);
+    REQUIRE(code.c1 == 0x5f);
 
-    SECTION("icf") {
-        ecma48_iter iter("\x1b\x60\x1b\x7f", state);
+    REQUIRE(iter.next(code) == false);
+}
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_icf);
-        REQUIRE(code.icf == 0x60);
+TEST_CASE("Ecma48 icf") {
+    ecma48_code code;
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_icf);
-        REQUIRE(code.icf == 0x7f);
+    ecma48_iter iter("\x1b\x60\x1b\x7f", g_state);
 
-        REQUIRE(iter.next(code) == false);
-    }
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_icf);
+    REQUIRE(code.icf == 0x60);
 
-    SECTION("csi") {
-        ecma48_iter iter("\x1b[\x40\x1b[\x20\x7e", state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_icf);
+    REQUIRE(code.icf == 0x7f);
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_csi);
-        REQUIRE(code.csi->func == 0x40);
-        REQUIRE(code.csi->param_count == 0);
+    REQUIRE(iter.next(code) == false);
+}
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_csi);
-        REQUIRE(code.csi->func == 0x207e);
-        REQUIRE(code.csi->param_count == 0);
+TEST_CASE("Ecma48 csi") {
+    ecma48_code code;
 
-        REQUIRE(iter.next(code) == false);
-    }
+    ecma48_iter iter("\x1b[\x40\x1b[\x20\x7e", g_state);
 
-    SECTION("csi params") {
-        ecma48_iter iter("\x1b[1;12;123\x40", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.csi->param_count == 3);
-        REQUIRE(code.csi->params[0] == 1);
-        REQUIRE(code.csi->params[1] == 12);
-        REQUIRE(code.csi->params[2] == 123);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_csi);
+    REQUIRE(code.csi->func == 0x40);
+    REQUIRE(code.csi->param_count == 0);
 
-        new (&iter) ecma48_iter("\x1b[;@", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.csi->param_count == 2);
-        REQUIRE(code.csi->params[0] == 0);
-        REQUIRE(code.csi->params[1] == 0);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_csi);
+    REQUIRE(code.csi->func == 0x207e);
+    REQUIRE(code.csi->param_count == 0);
 
-        new (&iter) ecma48_iter("\x1b[;;;;;;;;;;;;1;2;3;4;5m", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.csi->param_count == 8);
-    }
+    REQUIRE(iter.next(code) == false);
+}
 
-    SECTION("csi invalid") {
-        ecma48_iter iter("\x1b[1;2\01", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c0);
-        REQUIRE(code.c0 == 1);
-    }
+TEST_CASE("Ecma48 csi params") {
+    ecma48_code code;
 
-    SECTION("stream") {
-        ecma48_iter iter("\x1b[1;2", state);
-        REQUIRE(iter.next(code) == false);
+    ecma48_iter iter("\x1b[1;12;123\x40", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.csi->param_count == 3);
+    REQUIRE(code.csi->params[0] == 1);
+    REQUIRE(code.csi->params[1] == 12);
+    REQUIRE(code.csi->params[2] == 123);
 
-        new (&iter) ecma48_iter("1m", state);
-        REQUIRE(iter.next(code) == true);
+    new (&iter) ecma48_iter("\x1b[;@", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.csi->param_count == 2);
+    REQUIRE(code.csi->params[0] == 0);
+    REQUIRE(code.csi->params[1] == 0);
 
-        REQUIRE(code.type == ecma48_code::type_csi);
-        REQUIRE(code.csi->func == 'm');
-        REQUIRE(code.csi->param_count == 2);
-        REQUIRE(code.csi->params[0] == 1);
-        REQUIRE(code.csi->params[1] == 21);
-    }
+    new (&iter) ecma48_iter("\x1b[;;;;;;;;;;;;1;2;3;4;5m", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.csi->param_count == 8);
+}
 
-    SECTION("split") {
-        ecma48_iter iter(" \x1b[1;2x@@@@", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_chars);
-        REQUIRE(code.length == 1);
-        REQUIRE(code.str[0] == ' ');
+TEST_CASE("Ecma48 csi invalid") {
+    ecma48_code code;
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_csi);
-        REQUIRE(code.csi->func == 'x');
+    ecma48_iter iter("\x1b[1;2\01", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c0);
+    REQUIRE(code.c0 == 1);
+}
 
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_chars);
-        REQUIRE(code.length == 4);
-        REQUIRE(code.str[0] == '@');
+TEST_CASE("Ecma48 stream") {
+    ecma48_code code;
 
-        REQUIRE(iter.next(code) == false);
-    }
+    ecma48_iter iter("\x1b[1;2", g_state);
+    REQUIRE(iter.next(code) == false);
 
-    SECTION("utf8") {
-        ecma48_iter iter("\xc2\x9bz", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_csi);
-        REQUIRE(code.csi->func == 'z');
+    new (&iter) ecma48_iter("1m", g_state);
+    REQUIRE(iter.next(code) == true);
 
-        new (&iter) ecma48_iter("\xc2\x9c", state);
-        REQUIRE(iter.next(code) == true);
-        REQUIRE(code.type == ecma48_code::type_c1);
-        REQUIRE(code.c1 == 0x5c);
-    }
+    REQUIRE(code.type == ecma48_code::type_csi);
+    REQUIRE(code.csi->func == 'm');
+    REQUIRE(code.csi->param_count == 2);
+    REQUIRE(code.csi->params[0] == 1);
+    REQUIRE(code.csi->params[1] == 21);
+}
+
+TEST_CASE("Ecma48 split") {
+    ecma48_code code;
+
+    ecma48_iter iter(" \x1b[1;2x@@@@", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_chars);
+    REQUIRE(code.length == 1);
+    REQUIRE(code.str[0] == ' ');
+
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_csi);
+    REQUIRE(code.csi->func == 'x');
+
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_chars);
+    REQUIRE(code.length == 4);
+    REQUIRE(code.str[0] == '@');
+
+    REQUIRE(iter.next(code) == false);
+}
+
+TEST_CASE("Ecma48 utf8") {
+    ecma48_code code;
+
+    ecma48_iter iter("\xc2\x9bz", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_csi);
+    REQUIRE(code.csi->func == 'z');
+
+    new (&iter) ecma48_iter("\xc2\x9c", g_state);
+    REQUIRE(iter.next(code) == true);
+    REQUIRE(code.type == ecma48_code::type_c1);
+    REQUIRE(code.c1 == 0x5c);
 }
