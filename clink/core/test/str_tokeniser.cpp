7df2778ef55490b45fafcc26add10f817d8e9236
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-02-15 21:39:23 +0100

    Tests for string tokenisation.

---
+++ clink/core/test/str_tokeniser.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) 2016 Martin Ridgers
+// License: http://opensource.org/licenses/MIT
+
+#include "pch.h"
+
+#include <core/str.h>
+#include <core/str_tokeniser.h>
+
+TEST_CASE("str_tokeniser : basic")
+{
+    str<> s;
+    str_tokeniser t("a;b;c", ";");
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'a');
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'b');
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'c');
+    REQUIRE(t.next(s) == false);
+}
+
+TEST_CASE("str_tokeniser : multi delims")
+{
+    str<> s;
+    str_tokeniser t("a;b-c.d", ";-.");
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'a');
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'b');
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'c');
+    REQUIRE(t.next(s)); REQUIRE(s[0] == 'd');
+    REQUIRE(t.next(s) == false);
+}
+
+TEST_CASE("str_tokeniser : ends")
+{
+    str<> s;
+    auto inputs = { "a;b;c", ";a;b;c", "a;b;c;" };
+    for (auto input : inputs)
+    {
+        str_tokeniser t(input, ";");
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'a');
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'b');
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'c');
+        REQUIRE(t.next(s) == false);
+    }
+}
+
+TEST_CASE("str_tokeniser : delim runs")
+{
+    str<> s;
+    auto inputs = { "a;;b--c", "-;a;-b;c", "a;b;-c-;" };
+    for (auto input : inputs)
+    {
+        str_tokeniser t(input, ";-");
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'a');
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'b');
+        REQUIRE(t.next(s)); REQUIRE(s[0] == 'c');
+        REQUIRE(t.next(s) == false);
+    }
+}
