dfc69f49a5cc8284537becefe6ed3c306b296796
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-02-29 00:18:37 +0100

    Modified djb hash.

---
+++ clink/core/include/core/str_hash.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2016 Martin Ridgers
+// License: http://opensource.org/licenses/MIT
+
+#pragma once
+
+//------------------------------------------------------------------------------
+template <typename T> unsigned int str_hash_impl(const T* in)
+{
+    unsigned int hash = 5381;
+
+    while (int c = *in++)
+        hash = ((hash << 5) + hash) ^ c;
+
+    return hash;
+}
+
+//------------------------------------------------------------------------------
+inline unsigned int str_hash(const char* in)
+{
+    return str_hash_impl<char>(in);
+}
+
+//------------------------------------------------------------------------------
+inline unsigned int wstr_hash(const wchar_t* in)
+{
+    return str_hash_impl<wchar_t>(in);
+}
