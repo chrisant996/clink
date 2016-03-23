3a0103276984f88a6cc855f1e79eeca594aecf42
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-03-23 01:05:29 +0100

    Extracting of words from the line into a line state.

--- clink/app/loader/testbed.cpp
+++ clink/app/loader/testbed.cpp
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include "rl/rl_line_editor.h"
 
+#include <core/array.h>
+#include <core/base.h>
 #include <core/str_tokeniser.h>
 #include <terminal/ecma48_terminal.h>
 #include <matches/column_printer.h>
@@ -19,28 +21,106 @@ int testbed(int, char**)
     line_editor::desc desc = { "testbed", terminal, printer };
     auto* line_editor = create_rl_line_editor(desc);
 
-    match_system& match_system = line_editor->get_match_system();
-
-    auto* file_generator = new file_match_generator();
-    match_system.add_generator(file_generator, 0);
+    file_match_generator file_generator;
+    line_editor->get_match_system().add_generator(&file_generator, 0);
 
     terminal->begin();
     rl_callback_handler_install("testbed>", end_line);
+
+    static const char* word_delims = " \t";
+    static const char* partial_delims = "\\/";
+
+    unsigned int match_key = ~0;
+
     for (int i = 0; ; ++i)
     {
-        // Should matches be updated?
+        // Get line state from backend.
+        const char* line_buffer = rl_line_buffer;
+        const int line_cursor = rl_point;
+
+        // Collect words.
+        struct word {
+            unsigned short offset;
+            unsigned short length;
+            unsigned short partial;
+            unsigned short delim;
+        };
+        fixed_array<word, 2> words;
+
         int word_count = 0;
-        str_iter token_iter(rl_line_buffer, rl_point);
-        str_tokeniser tokens(token_iter, " \t");
-        const char* word_start;
-        int word_length;
+
+        str_iter token_iter(line_buffer, line_cursor);
+        str_tokeniser tokens(token_iter, word_delims);
+        const char* word_start = nullptr;
+        int word_length = 0;
         while (tokens.next(word_start, word_length))
-            ++word_count;
+        {
+            word* word = words.push_back();
+            if (word == nullptr)
+                word = words.back();
 
-        if (!rl_point || strchr(" \t", rl_line_buffer[rl_point - 1]))
-            ++word_count;
+            *word = { short(word_start - line_buffer), word_length };
 
-        printf("wc: %02d\n", word_count);
+            // Find the best-fit delimiter.
+            /* MODE4
+            const char* best_delim = word_delims;
+            const char* c = word_start - 1;
+            while (c > line_buffer)
+            {
+                const char* delim = strchr(word_delims, *c);
+                if (delim == nullptr)
+                    break;
+
+                best_delim = max(delim, best_delim);
+                --c;
+            }
+            word->delim = *best_delim;
+            */
+        }
+
+        // Add an empty word if the cursor at the beginning of one.
+        word* end_word = words.back();
+        if (end_word == nullptr ||
+            end_word->offset + end_word->length < line_cursor)
+        {
+            words.push_back();
+            *(words.back()) = { line_cursor };
+        }
+
+        // Adjust the completing word for partiality.
+        end_word = words.back();
+        for (int j = end_word->length - 1; j >= 0; --j)
+        {
+            int c = line_buffer[end_word->offset + j];
+            if (strchr(partial_delims, c) != nullptr)
+            {
+                end_word->partial = j;
+                break;
+            }
+        }
+
+        // SPAM!
+        int j = 0;
+        for (auto word : words)
+            printf("%02d:%02d,%02d,%02d,%02x ", j++, word.offset, word.partial, word.length, word.delim);
+        puts("");
+        // SPAM!
+
+        // Should we generate new matches?
+        unsigned int next_match_key = end_word->offset << 20;
+        next_match_key |= (end_word->partial & 0x3ff) << 10;
+        if ((match_key & ~0x3ff) != next_match_key)
+        {
+            puts("Generate!");
+        }
+
+        // Should we sort and select matches?
+        next_match_key |= end_word->length & 0x3ff;
+        if (match_key != next_match_key)
+        {
+            match_key = next_match_key;
+            puts("Selort!");
+        }
 
         // Send to backend.
         rl_forced_update_display();
@@ -57,7 +137,9 @@ int testbed(int, char**)
                 break;
         }
 
-        printf("\n%03d [%08x]\n", i, rl_readline_state);
+        // SPAM!
+        printf("\n\n%03d [%08x]\n", i, rl_readline_state);
+        // SPAM!
     }
     terminal->end();
 
