965a330077ea35e3b1328572051290730cfb68f0
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-04-02 00:06:55 +0200

    Missing forwarding of argument.

--- clink/core/include/core/str_hash.h
+++ clink/core/include/core/str_hash.h
@@ -21,11 +21,11 @@ template <typename T> unsigned int str_hash_impl(const T* in, unsigned int lengt
 //------------------------------------------------------------------------------
 inline unsigned int str_hash(const char* in, int length=-1)
 {
-    return str_hash_impl<char>(in);
+    return str_hash_impl<char>(in, length);
 }
 
 //------------------------------------------------------------------------------
 inline unsigned int wstr_hash(const wchar_t* in, int length=-1)
 {
-    return str_hash_impl<wchar_t>(in);
+    return str_hash_impl<wchar_t>(in, length);
 }
