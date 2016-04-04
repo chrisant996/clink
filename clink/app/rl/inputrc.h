ade27d1b2635d6477aeabae24a1bebee83cb33e4
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-04-05 00:47:39 +0200

    Reordered embedded Readline inputrc.

--- clink/app/rl/inputrc.h
+++ clink/app/rl/inputrc.h
@@ -4,72 +4,66 @@
 #pragma once
 
 const char* clink_inputrc[] = {
-    "set bell-style               visible",
-    "set completion-ignore-case   on",
-    "set completion-map-case      on",
-    "set completion-display-width 106",
-    "set convert-meta             off",   // for correct utf8 input
-    "set output-meta              on",    // for correct utf8 output
-    "set skip-completed-text      on",
-    "set visible-stats            off",
-
-    "\"\\eOD\": backward-word",           // ctrl-left
-    "\"\\eOC\": forward-word",            // ctrl-right
-    "\"\\e[4\": end-of-line",             // end
-    "\"\\e[1\": beginning-of-line",       // home
-    "\"\\e[3\": delete-char",             // del
-    "\"\\e[t\": enter-scroll-mode",       // shift-pgup
-    "\"\\eO4\": kill-line",               // ctrl-end
-    "\"\\eO1\": backward-kill-line",      // ctrl-home
-    "\"\\e[5\": history-search-backward", // pgup
-    "\"\\e[6\": history-search-forward",  // pgdn
+    "M-OD: backward-word",           // ctrl-left
+    "M-OC: forward-word",            // ctrl-right
+    "M-[4: end-of-line",             // end
+    "M-[1: beginning-of-line",       // home
+    "M-[3: delete-char",             // del
+    "M-[t: enter-scroll-mode",       // shift-pgup
+    "M-O4: kill-line",               // ctrl-end
+    "M-O1: backward-kill-line",      // ctrl-home
+    "M-[5: history-search-backward", // pgup
+    "M-[6: history-search-forward",  // pgdn
 
     "set keymap emacs",
-    "C-v:     paste-from-clipboard",
-    "C-q:     reload-lua-state",
-    "C-z:     undo",
-    "M-h:     show-rl-help",
-    "M-C-c:   copy-line-to-clipboard",
-    "C-c:     ctrl-c",
-    "M-a:     \"..\\\\\"",
+    "C-v:   paste-from-clipboard",
+    "C-q:   reload-lua-state",
+    "C-z:   undo",
+    "M-h:   show-rl-help",
+    "M-C-c: copy-line-to-clipboard",
+    "C-c:   ctrl-c",
+    "M-a:   \"..\\\\\"",
 
     "set keymap vi-insert",
-    "C-v:     paste-from-clipboard",
-    "C-z:     undo",
-    "M-h:     show-rl-help",
-    "M-C-c:   copy-line-to-clipboard",
-    "C-c:     ctrl-c",
-    "M-a:     \"..\\\\\"",
+    "C-v:   paste-from-clipboard",
+    "C-z:   undo",
+    "M-h:   show-rl-help",
+    "M-C-c: copy-line-to-clipboard",
+    "C-c:   ctrl-c",
+    "M-a:   \"..\\\\\"",
 
     "set keymap vi-move",
-    "C-v:     paste-from-clipboard",
-    "C-z:     undo",
-    "M-h:     show-rl-help",
-    "M-C-c:   copy-line-to-clipboard",
-    "C-c:     ctrl-c",
-    "M-a:     \"..\\\\\"",
+    "C-v:   paste-from-clipboard",
+    "C-z:   undo",
+    "M-h:   show-rl-help",
+    "M-C-c: copy-line-to-clipboard",
+    "C-c:   ctrl-c",
+    "M-a:   \"..\\\\\"",
 
 #if MODE4
     "$if cmd.exe",
 #endif
         "set keymap emacs",
-        "\"\\eO5\": up-directory",
-        "M-C-u:     up-directory",
-        "M-C-e:     expand-env-vars",
+        "M-O5:  up-directory",
+        "M-C-u: up-directory",
+        "M-C-e: expand-env-vars",
 
         "set keymap vi-insert",
-        "\"\\eO5\": up-directory",
-        "M-C-u:     up-directory",
-        "M-C-e:     expand-env-vars",
+        "M-O5:  up-directory",
+        "M-C-u: up-directory",
+        "M-C-e: expand-env-vars",
 
         "set keymap vi-move",
-        "\"\\eO5\": up-directory",
-        "M-C-u:     up-directory",
-        "M-C-e:     expand-env-vars",
+        "M-O5:  up-directory",
+        "M-C-u: up-directory",
+        "M-C-e: expand-env-vars",
 #if MODE4
     "$endif",
 #endif
 
+    "set bell-style   visible", // MODE4
+    "set convert-meta off",   // For correct utf8 input.
+    "set output-meta  on",    // For correct utf8 output
     "set keymap emacs",
     nullptr,
 };
