c688916a96f17ca57b0bf11a0a57f7e20ce252d3
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-04-18 12:28:56 +0200

    Whitespace adjustment.

--- clink/core/include/core/os.h
+++ clink/core/include/core/os.h
@@ -8,20 +8,22 @@ class str_base;
 //------------------------------------------------------------------------------
 namespace os
 {
-    enum {
-        path_type_invalid,
-        path_type_file,
-        path_type_dir,
-    };
 
-    int     get_path_type(const char* path);
-    void    get_current_dir(str_base& out);
-    bool    set_current_dir(const char* dir);
-    bool    make_dir(const char* dir);
-    bool    remove_dir(const char* dir);
-    bool    unlink(const char* path);
-    bool    move(const char* src_path, const char* dest_path);
-    bool    copy(const char* src_path, const char* dest_path);
-    bool    get_temp_dir(str_base& out);
-    bool    get_env(const char* name, str_base& out);
+enum {
+    path_type_invalid,
+    path_type_file,
+    path_type_dir,
+};
+
+int     get_path_type(const char* path);
+void    get_current_dir(str_base& out);
+bool    set_current_dir(const char* dir);
+bool    make_dir(const char* dir);
+bool    remove_dir(const char* dir);
+bool    unlink(const char* path);
+bool    move(const char* src_path, const char* dest_path);
+bool    copy(const char* src_path, const char* dest_path);
+bool    get_temp_dir(str_base& out);
+bool    get_env(const char* name, str_base& out);
+
 }; // namespace os
--- clink/core/include/core/path.h
+++ clink/core/include/core/path.h
@@ -8,19 +8,21 @@ class str_base;
 //------------------------------------------------------------------------------
 namespace path
 {
-    void        clean(str_base& in_out, int sep=0);
-    void        clean(char* in_out, int sep=0);
-    bool        is_separator(int c);
-    const char* next_element(const char* in);
-    bool        get_base_name(const char* in, str_base& out);
-    bool        get_directory(const char* in, str_base& out);
-    bool        get_directory(str_base& in_out);
-    bool        get_drive(const char* in, str_base& out);
-    bool        get_drive(str_base& in_out);
-    bool        get_extension(const char* in, str_base& out);
-    bool        get_name(const char* in, str_base& out);
-    const char* get_name(const char* in);
-    bool        is_root(const char* path);
-    bool        join(const char* lhs, const char* rhs, str_base& out);
-    bool        append(str_base& out, const char* rhs);
+
+void        clean(str_base& in_out, int sep=0);
+void        clean(char* in_out, int sep=0);
+bool        is_separator(int c);
+const char* next_element(const char* in);
+bool        get_base_name(const char* in, str_base& out);
+bool        get_directory(const char* in, str_base& out);
+bool        get_directory(str_base& in_out);
+bool        get_drive(const char* in, str_base& out);
+bool        get_drive(str_base& in_out);
+bool        get_extension(const char* in, str_base& out);
+bool        get_name(const char* in, str_base& out);
+const char* get_name(const char* in);
+bool        is_root(const char* path);
+bool        join(const char* lhs, const char* rhs, str_base& out);
+bool        append(str_base& out, const char* rhs);
+
 }; // namespace path
