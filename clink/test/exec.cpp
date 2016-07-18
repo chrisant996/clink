fe0b39d3f9a19c2050134c0575c6b65b56fbcc70
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-07-18 20:37:59 +0200

    Moved remaining exec tests to the C++ and deleted obsolete Lua version.

--- clink/test/exec.cpp
+++ clink/test/exec.cpp
@@ -63,11 +63,163 @@ TEST_CASE("Executable match generation.") {
     };
     env_fixture env(exec_env);
 
+#if MODE4
+    clink.test.test_matches(
+        "Nothing",
+        "abc123",
+        {}
+    )
+#endif
+
     SECTION("PATH") {
         exec_test::tester("one_p", "one_path.exe", nullptr);
     }
 
-    // MODE4 - missing tests
+#if MODE4
+    clink.test.test_output(
+        "path case mapped",
+        "one-p",
+        "one_path.exe "
+    )
+
+    clink.test.test_matches(
+        "path matches",
+        "one_",
+        { "one_path.exe", "one_two.py" }
+    )
+
+    clink.test.test_matches(
+        "cmd.exe commands",
+        "p",
+        { "path", "pause", "popd", "prompt", "pushd" }
+    )
+
+    clink.test.test_matches(
+        "relative path",
+        ".\\",
+        { "one_local.exe", "two_local.exe", "one_dir\\", "foodir\\", "jumble\\" }
+    )
+
+    clink.test.test_output(
+        "relative path dir",
+        ".\\foodir\\",
+        ".\\foodir\\two_dir_local.exe "
+    )
+
+    clink.test.test_output(
+        "relative path dir (with '_')",
+        ".\\one_dir\\t",
+        ".\\one_dir\\two_dir_local.exe "
+    )
+
+    clink.test.test_output(
+        "separator | 1",
+        "nullcmd | one_p",
+        "nullcmd | one_path.exe "
+    )
+
+    clink.test.test_output(
+        "separator | 2",
+        "nullcmd |one_p",
+        "nullcmd |one_path.exe "
+    )
+
+    clink.test.test_matches(
+        "separator & 1",
+        "nullcmd & one_",
+        { "one_path.exe", "one_two.py" }
+    )
+
+    clink.test.test_matches(
+        "separator & 2",
+        "nullcmd &one_",
+        { "one_path.exe", "one_two.py" }
+    )
+
+    clink.test.test_output(
+        "separator && 1",
+        "nullcmd && one_p",
+        "nullcmd && one_path.exe "
+    )
+
+    clink.test.test_output(
+        "separator && 2",
+        "nullcmd &&one_p",
+        "nullcmd &&one_path.exe "
+    )
+
+    clink.test.test_output(
+        "spaces (path)",
+        "spa",
+        "\"spa ce.exe\" "
+    )
+
+    clink.test.test_output(
+        "spaces (relative)",
+        ".\\one_dir\\spa",
+        "\".\\one_dir\\spa ce.exe\" "
+    )
+
+    clink.test.test_matches(
+        "separator false positive",
+        "nullcmd \"&&\" o\t",
+        { "one_local.exe", "one_local.txt", "one_dir\\" }
+    )
+
+    clink.test.test_output(
+        "last char . 1",
+        "one_path.",
+        "one_path.exe "
+    )
+
+    clink.test.test_output(
+        "last char . 2",
+        "jumble\\three.",
+        "jumble\\three.exe "
+    )
+
+    clink.test.test_output(
+        "last char -",
+        "one_local-",
+        "one_local-"
+    )
+
+    --------------------------------------------------------------------------------
+    exec_match_style = 1
+    clink.test.test_matches(
+        "style - cwd (no dirs) 1",
+        "one_",
+        { "one_local.exe", "one_path.exe", "one_two.py" }
+    )
+
+    exec_match_style = 2
+    clink.test.test_matches(
+        "style - cwd (all)",
+        "one-\t",
+        { "one_local.exe", "one_path.exe", "one_two.py", "one_dir\\" }
+    )
+
+    --------------------------------------------------------------------------------
+    exec_match_style = 0
+    space_prefix_match_files = 1
+    clink.test.test_matches(
+        "space prefix; none",
+        "one_",
+        { "one_path.exe", "one_two.py" }
+    )
+
+    clink.test.test_matches(
+        "space prefix; space",
+        " one_",
+        { "one_dir\\", "one_local.txt", "one_local.exe" }
+    )
+
+    clink.test.test_matches(
+        "space prefix; spaces",
+        "   one_",
+        { "one_dir\\", "one_local.txt", "one_local.exe" }
+    )
+#endif // MODE4
 }
 
 #endif // MODE4
