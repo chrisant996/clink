43d955d81f5ab5cb2f4b2eaeb384066e3575c357
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-08-14 23:28:58 +0200

    Improved output from unhandled exception filter.

--- clink/app/src/utils/seh_scope.cpp
+++ clink/app/src/utils/seh_scope.cpp
@@ -11,17 +11,19 @@
 static LONG WINAPI exception_filter(EXCEPTION_POINTERS* info)
 {
 #if defined(_MSC_VER)
-    str<MAX_PATH> file_name;
-    get_config_dir(file_name);
-    file_name << "/mini_dump.dmp";
+    str<MAX_PATH, false> buffer;
+    get_config_dir(buffer);
+    buffer << "/clink.dmp";
 
     fputs("\n!!! CLINK'S CRASHED!", stderr);
-    fputs("\n!!! Something went wrong.", stderr);
-    fputs("\n!!! Writing mini dump file to: ", stderr);
-    fputs(file_name.c_str(), stderr);
-    fputs("\n", stderr);
+    fputs("\n!!!", stderr);
+    fputs("\n!!! Writing core dump", stderr);
+    fputs("\n!!! ", stderr);
+    fputs(buffer.c_str(), stderr);
 
-    HANDLE file = CreateFile(file_name.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
+    // Write a core dump file.
+    BOOL ok = FALSE;
+    HANDLE file = CreateFile(buffer.c_str(), GENERIC_WRITE, 0, nullptr, CREATE_ALWAYS, 0, nullptr);
     if (file != INVALID_HANDLE_VALUE)
     {
         DWORD pid = GetCurrentProcessId();
@@ -29,11 +31,50 @@ static LONG WINAPI exception_filter(EXCEPTION_POINTERS* info)
         if (process != nullptr)
         {
             MINIDUMP_EXCEPTION_INFORMATION mdei = { GetCurrentThreadId(), info, FALSE };
-            MiniDumpWriteDump(process, pid, file, MiniDumpNormal, &mdei, nullptr, nullptr);
+            ok = MiniDumpWriteDump(process, pid, file, MiniDumpNormal, &mdei, nullptr, nullptr);
         }
         CloseHandle(process);
         CloseHandle(file);
     }
+    fputs(ok ? "\n!!! ...ok" : "\n!!! ...failed", stderr);
+    fputs("\n!!!", stderr);
+
+    // Output some useful modules' base addresses
+    buffer.format("\n!!! Clink: 0x%p", GetModuleHandle(CLINK_DLL));
+    fputs(buffer.c_str(), stderr);
+
+    buffer.format("\n!!! Host: 0x%p", GetModuleHandle(nullptr));
+    fputs(buffer.c_str(), stderr);
+
+    // Output information about the exception that occurred.
+    EXCEPTION_RECORD& record = *(info->ExceptionRecord);
+    if (record.ExceptionCode == EXCEPTION_ACCESS_VIOLATION)
+    {
+        uintptr_t access = record.ExceptionInformation[0];
+        uintptr_t addr = record.ExceptionInformation[1];
+        buffer.format("\n!!! Guru: 0x%08x addr=0x%p (%d)", record.ExceptionCode, addr, access);
+    }
+    else
+        buffer.format("\n!!! Guru: 0x%08x", record.ExceptionCode);
+    fputs(buffer.c_str(), stderr);
+
+    // Output a backtrace.
+    fputs("\n!!!", stderr);
+    fputs("\n!!! Backtrace:", stderr);
+    bool skip = true;
+    void* backtrace[48];
+    int bt_length = CaptureStackBackTrace(0, sizeof_array(backtrace), backtrace, nullptr);
+    for (int i = 0; i < bt_length; ++i)
+    {
+        if (skip &= (backtrace[i] != record.ExceptionAddress))
+            continue;
+
+        buffer.format("\n!!! 0x%p", backtrace[i]);
+        fputs(buffer.c_str(), stderr);
+    }
+
+    fputs("\n\nPress spacebar to exit...", stderr);
+    while (getc(stdin) != ' ');
 #endif // _MSC_VER
 
     // Would be awesome if we could unhook ourself, unload, and allow cmd.exe
