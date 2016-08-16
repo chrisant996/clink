d2b6d5a8d559d6d0de379ff76b452541bb74cab9
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-08-16 23:22:26 +0200

    Tests for word quoting.

--- clink/lib/test/quotes.cpp
+++ clink/lib/test/quotes.cpp
@@ -2,32 +2,83 @@
 // License: http://opensource.org/licenses/MIT
 
 #include "pch.h"
+#include "fs_fixture.h"
+#include "line_editor_tester.h"
 
-#if MODE4
+#include <core/str_compare.h>
+#include <lib/match_generator.h>
 
---------------------------------------------------------------------------------
-clink.test.test_fs({
-    "pre_nospace",
-    "pre_space 1",
-    "pre_space_space 2"
-})
+//------------------------------------------------------------------------------
+TEST_CASE("Quoting") {
+    static const char* space_fs[] = {
+        "pre_nospace",
+        "pre_space 1",
+        "pre_space_space 2",
+        "single space",
+        "dir/space 1",
+        "dir/space 2",
+        "dir/space_3",
+        "dir/single space",
+        nullptr,
+    };
 
-clink.test.test_output(
-    "No quote added",
-    "nullcmd pr",
-    "nullcmd pre_"
-)
+    fs_fixture fs(space_fs);
 
-clink.test.test_output(
-    "Quote added",
-    "nullcmd pre_s",
-    "nullcmd \"pre_space"
-)
+    line_editor_tester tester;
+    editor_backend* backend = classic_match_ui_create();
 
-clink.test.test_output(
-    "Quote added case mapped",
-    "nullcmd pre-s",
-    "nullcmd \"pre_space"
-)
+    line_editor* editor = tester.get_editor();
+    editor->add_backend(*backend);
+    editor->add_generator(file_match_generator());
 
-#endif // MODE4
+    SECTION("None") {
+        tester.set_input("pr\t");
+        tester.set_expected_output("pre_");
+        tester.run();
+    }
+
+    SECTION("Close exisiting") {
+        tester.set_input("\"singl\t");
+        tester.set_expected_output("\"single space\" ");
+        tester.run();
+    }
+
+    SECTION("Surround") {
+        tester.set_input("sing\t");
+        tester.set_expected_output("\"single space\" ");
+        tester.run();
+    }
+
+    SECTION("Prefix") {
+        tester.set_input("pre_s\t");
+        tester.set_expected_output("\"pre_space");
+        tester.run();
+    }
+
+    SECTION("Prefix (case mapped)") {
+        str_compare_scope _(str_compare_scope::relaxed);
+        tester.set_input("pre-s\t");
+        tester.set_expected_output("\"pre_space");
+        tester.run();
+    }
+
+    SECTION("Dir (close exisiting)") {
+        tester.set_input("\"dir/sing\t");
+        tester.set_expected_output("\"dir\\single space\" ");
+        tester.run();
+    }
+
+    SECTION("Dir (surround)") {
+        tester.set_input("dir/sing\t");
+        tester.set_expected_output("\"dir\\single space\" ");
+        tester.run();
+    }
+
+    SECTION("Dir (prefix)") {
+        tester.set_input("dir\\spac\t");
+        tester.set_expected_output("\"dir\\space");
+        tester.run();
+    }
+
+    classic_match_ui_destroy(backend);
+}
