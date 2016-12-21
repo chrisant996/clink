8c56c11449cbf9938572cac66da555c90ca2e71d
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-12-21 22:19:23 +0100

    Architecture-specific embedded Lua scripts.

--- embed.lua
+++ embed.lua
@@ -9,21 +9,29 @@ end
 
 --------------------------------------------------------------------------------
 local function do_embed()
-    local luac = os.matchfiles(".build/*/bin/final/luac_x64.exe")[1]
-    if not luac then
-        error("Unable to find Lua compiler binary (final).")
-    end
+    -- Find the Lua compilers.
+    local archs = {
+        ["64"] = { luac = os.matchfiles(".build/*/bin/final/luac_x64.exe")[1] },
+        ["86"] = { luac = os.matchfiles(".build/*/bin/final/luac_x86.exe")[1] },
+    }
 
-    luac = luac:gsub("/", "\\")
+    for name, arch in pairs(archs) do
+        if not arch.luac then
+            error("Unable to find Lua compiler binary (x"..name.." final).")
+        end
+
+        arch.luac = arch.luac:gsub("/", "\\")
+    end
 
     local manifests = os.matchfiles("clink/**/_manifest.lua")
     for _, manifest in ipairs(manifests) do
         local root = path.getdirectory(manifest)
 
         out = path.join(root, "lua_scripts.cpp")
-        print("-- " .. out)
+        print("\n"..out)
         out = io.open(out, "w")
         out:write("#include \"pch.h\"\n")
+        out:write("#include <core/base.h>\n")
         out:write("#if defined(CLINK_FINAL)\n")
 
         -- Write each sanitised script to 'out' as a global variable.
@@ -35,28 +43,36 @@ local function do_embed()
             table.insert(symbols, symbol)
 
             file = path.join(root, file)
+            print("   "..file)
 
-            -- Compile the input Lua script to binary.
-            exec(luac.." -s -o .build/embed_temp "..file)
-            local bin_in = io.open(".build/embed_temp", "rb")
-            local bin_data = bin_in:read("*a")
-            bin_in:close()
-            os.remove(".build/embed_temp")
+            for name, arch in pairs(archs) do
+                out:write("#if ARCHITECTURE == "..name.."\n")
 
-            print("     " .. file .. "  -->  " .. symbol)
-            local crlf_counter = 0
-            out:write("const unsigned char " .. symbol .. "_[] = {\n")
-            for byte in string.gmatch(bin_data, ".") do
-                out:write(string.format("0x%02x, ", byte:byte()))
-                crlf_counter = crlf_counter + 1
-                if crlf_counter > 16 then
-                    out:write("\n")
-                    crlf_counter = 0
+                -- Compile the input Lua script to binary.
+                exec(arch.luac.." -s -o .build/embed_temp "..file)
+                local bin_in = io.open(".build/embed_temp", "rb")
+                local bin_data = bin_in:read("*a")
+                bin_in:close()
+                os.remove(".build/embed_temp")
+
+                local crlf_counter = 0
+                out:write("const unsigned char " .. symbol .. "_[] = {\n")
+                for byte in string.gmatch(bin_data, ".") do
+                    out:write(string.format("0x%02x, ", byte:byte()))
+                    crlf_counter = crlf_counter + 1
+                    if crlf_counter > 16 then
+                        out:write("\n")
+                        crlf_counter = 0
+                    end
                 end
+                out:write("};\n")
+                out:write("unsigned char const* "..symbol.." = "..symbol.."_;\n")
+                out:write("int "..symbol.."_len = sizeof("..symbol.."_);\n")
+
+                out:write("#endif // ARCHITECTURE == "..name.."\n")
+
+                print("      x"..name.." : "..tostring(#bin_data).." bytes")
             end
-            out:write("};\n")
-            out:write("unsigned char const* "..symbol.." = "..symbol.."_;\n")
-            out:write("int "..symbol.."_len = sizeof("..symbol.."_);\n")
         end
 
         -- Some debug stuff so loose files can be loaded in debug builds.
