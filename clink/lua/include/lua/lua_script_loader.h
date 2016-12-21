e810aaebc5d10043633730711f636b956dc4f12f
Martin Ridgers <martin.ridgers+clink@gmail.com>
2016-12-21 21:08:27 +0100

    Adjusted Lua script loader macro so it requires a ';'

--- clink/lua/include/lua/lua_script_loader.h
+++ clink/lua/include/lua/lua_script_loader.h
@@ -7,14 +7,14 @@ void lua_load_script_impl(class lua_state&, const char*);
 
 #if defined(CLINK_FINAL)
     #define lua_load_script(state, module, name)                                \
-        {                                                                       \
+        do {                                                                    \
             extern const char* module##_##name##_lua_script;                    \
             lua_load_script_impl(state, module##_##name##_lua_script);          \
-        }
+        } while(0)
 #else
     #define lua_load_script(state, module, name)                                \
-        {                                                                       \
+        do {                                                                    \
             extern const char* module##_##name##_lua_file;                      \
             lua_load_script_impl(state, module##_##name##_lua_file);            \
-        }
+        } while(0)
 #endif // CLINK_FINAL
