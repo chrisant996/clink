c3761d1d6eb00d2f74d5ecc68450485dfeae15c0
Martin Ridgers <martin.ridgers+clink@gmail.com>
2017-09-26 23:37:50 +0200

    Added no_copy struct.

--- clink/core/include/core/base.h
+++ clink/core/include/core/base.h
@@ -38,3 +38,16 @@ template <class A> A min(A a, A b) { return (a < b) ? a : b; }
 
 #undef max
 template <class A> A max(A a, A b) { return (a > b) ? a : b; }
+
+//------------------------------------------------------------------------------
+struct no_copy
+{
+            no_copy() = default;
+            ~no_copy() = default;
+
+private:
+            no_copy(const no_copy&) = delete;
+            no_copy(const no_copy&&) = delete;
+    void    operator = (const no_copy&) = delete;
+    void    operator = (const no_copy&&) = delete;
+};
