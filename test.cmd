413095cbba2874665d2668af71f2da650f781f1b
Chris Antos <sparrowhawk996@gmail.com>
2020-09-23 02:41:48 -0700

    Enhance my test runner script.

--- test.cmd
+++ test.cmd
@@ -1,2 +1,22 @@
 @echo off
-%~dp0.build\vs2019\bin\debug\clink_test_x64.exe
+if x%1x == x?x goto :usage
+if x%1x == x/?x goto :usage
+if x%1x == x-?x goto :usage
+if x%1x == x/hx goto :usage
+if x%1x == x-hx goto :usage
+if x%1x == x/helpx goto :usage
+if x%1x == x--helpx goto :usage
+if x%1x == xhelpx goto :usage
+
+if x%1x == x/dbgx call devenv /debugexe %~dp0.build\vs2019\bin\debug\clink_test_x64.exe
+if not x%1x == x/dbgx %~dp0.build\vs2019\bin\debug\clink_test_x64.exe
+goto :eof
+
+:usage
+echo Usage:  test [/? /dbg]
+echo.
+echo   Run clink_test_x64.exe.
+echo.
+echo.  /?        Show usage info.
+echo   /dbg      Run test under the debugger.
+goto :eof
