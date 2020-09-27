85a7189aebc84f68122f24e73b106785031c181f
Chris Antos <sparrowhawk996@gmail.com>
2020-09-27 13:45:14 -0700

    Oops, add the other half of the getenv fix.

--- clink/lib/src/rl/rl_module.cpp
+++ clink/lib/src/rl/rl_module.cpp
@@ -97,6 +97,20 @@ static void load_user_inputrc()
 #endif // PLATFORM_WINDOWS
 }
 
+//------------------------------------------------------------------------------
+extern "C" const char* host_get_env(const char* name)
+{
+    static int rotate = 0;
+    static str<> rotating_tmp[10];
+
+    str<>& s = rotating_tmp[rotate];
+    rotate = (rotate + 1) % sizeof_array(rotating_tmp);
+    if (!os::get_env(name, s))
+        return nullptr;
+    return s.c_str();
+}
+
+//------------------------------------------------------------------------------
 
 
 //------------------------------------------------------------------------------
