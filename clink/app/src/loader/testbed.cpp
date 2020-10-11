9b88fb462740188e187d742a1988b33abadfdfde
Chris Antos <sparrowhawk996@gmail.com>
2020-10-10 21:18:47 -0700

    Get `clink testbed` working again.
    Looks like it hasn't worked since 2016 (see 439af5b).

--- clink/app/src/loader/testbed.cpp
+++ clink/app/src/loader/testbed.cpp
@@ -6,13 +6,18 @@
 #include <core/str_compare.h>
 #include <lib/line_editor.h>
 #include <lib/match_generator.h>
+#include <terminal/terminal.h>
+#include <terminal/printer.h>
 
 //------------------------------------------------------------------------------
 int testbed(int, char**)
 {
     str_compare_scope _(str_compare_scope::relaxed);
 
-    line_editor::desc desc;
+    terminal term = terminal_create();
+    printer printer(*term.out);
+
+    line_editor::desc desc = { term.in, term.out, &printer };
     line_editor* editor = line_editor_create(desc);
     editor->add_generator(file_match_generator());
 
