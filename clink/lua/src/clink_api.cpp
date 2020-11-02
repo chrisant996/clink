851482b8801915471c38d03c16a9e20967cd7680
Chris Antos <sparrowhawk996@gmail.com>
2020-11-01 17:39:04 -0800

    Move `getscreeninfo` to `os` API set.

--- clink/lua/src/clink_api.cpp
+++ clink/lua/src/clink_api.cpp
@@ -193,50 +193,6 @@ static int get_host_process(lua_State* state)
 
 
 //------------------------------------------------------------------------------
-/// -name:  clink.getscreeninfo
-/// -ret:   table
-/// Returns dimensions of the terminal's buffer (buf*) and visible window (win*).
-/// The returned table has the following scheme: { bufwidth:int, bufheight:int,
-/// winwidth:int, winheight:int }.
-static int get_screen_info(lua_State* state)
-{
-    int i;
-    int buffer_width, buffer_height;
-    int window_width, window_height;
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-
-    struct table_t {
-        const char* name;
-        int         value;
-    };
-
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    buffer_width = csbi.dwSize.X;
-    buffer_height = csbi.dwSize.Y;
-    window_width = csbi.srWindow.Right - csbi.srWindow.Left;
-    window_height = csbi.srWindow.Bottom - csbi.srWindow.Top;
-
-    lua_createtable(state, 0, 4);
-    {
-        struct table_t table[] = {
-            { "bufwidth", buffer_width },
-            { "bufheight", buffer_height },
-            { "winwidth", window_width },
-            { "winheight", window_height },
-        };
-
-        for (i = 0; i < sizeof_array(table); ++i)
-        {
-            lua_pushstring(state, table[i].name);
-            lua_pushinteger(state, table[i].value);
-            lua_rawset(state, -3);
-        }
-    }
-
-    return 1;
-}
-
-//------------------------------------------------------------------------------
 extern int set_current_dir(lua_State* state);
 extern int glob_dirs(lua_State* state);
 extern int glob_files(lua_State* state);
@@ -244,6 +200,7 @@ extern int get_aliases(lua_State* state);
 extern int get_current_dir(lua_State* state);
 extern int get_env(lua_State* state);
 extern int get_env_names(lua_State* state);
+extern int get_screen_info(lua_State* state);
 extern int is_dir(lua_State* state);
 
 //------------------------------------------------------------------------------
@@ -277,10 +234,6 @@ void clink_lua_initialise(lua_state& lua)
 #endif
         { "suppress_char_append",   &suppress_char_append },
         { "suppress_quoting",       &suppress_quoting },
-
-        // Clink 1.0.0a1 had only this API still under "clink.".
-        // TODO : move this somewhere else.
-        { "getscreeninfo",  &get_screen_info },
     };
 
     lua_State* state = lua.get_state();
--- clink/lua/src/os_api.cpp
+++ clink/lua/src/os_api.cpp
@@ -400,6 +400,50 @@ int get_aliases(lua_State* state)
 }
 
 //------------------------------------------------------------------------------
+/// -name:  os.getscreeninfo
+/// -ret:   table
+/// Returns dimensions of the terminal's buffer (<em>buf</em>*) and visible
+/// window (<em>win</em>*). The returned table has the following scheme:
+/// <em>{ bufwidth:int, bufheight:int, winwidth:int, winheight:int }.</em>
+int get_screen_info(lua_State* state)
+{
+    int i;
+    int buffer_width, buffer_height;
+    int window_width, window_height;
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+    struct table_t {
+        const char* name;
+        int         value;
+    };
+
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    buffer_width = csbi.dwSize.X;
+    buffer_height = csbi.dwSize.Y;
+    window_width = csbi.srWindow.Right - csbi.srWindow.Left;
+    window_height = csbi.srWindow.Bottom - csbi.srWindow.Top;
+
+    lua_createtable(state, 0, 4);
+    {
+        struct table_t table[] = {
+            { "bufwidth", buffer_width },
+            { "bufheight", buffer_height },
+            { "winwidth", window_width },
+            { "winheight", window_height },
+        };
+
+        for (i = 0; i < sizeof_array(table); ++i)
+        {
+            lua_pushstring(state, table[i].name);
+            lua_pushinteger(state, table[i].value);
+            lua_rawset(state, -3);
+        }
+    }
+
+    return 1;
+}
+
+//------------------------------------------------------------------------------
 void os_lua_initialise(lua_state& lua)
 {
     struct {
@@ -423,6 +467,7 @@ void os_lua_initialise(lua_state& lua)
         { "getenvnames", &get_env_names },
         { "gethost",     &get_host },
         { "getaliases",  &get_aliases },
+        { "getscreeninfo", &get_screen_info },
     };
 
     lua_State* state = lua.get_state();
