ab0ea53c66aea06836de29408e81c307d92042ed
Chris Antos <sparrowhawk996@gmail.com>
2020-11-03 03:48:52 -0800

    Remove more dead code.

--- clink/lua/src/match_builder_lua.cpp
+++ clink/lua/src/match_builder_lua.cpp
@@ -222,26 +222,6 @@ bool match_builder_lua::add_match_impl(lua_State* state, int stack_index, match_
             desc.match = lua_tostring(state, -1);
         lua_pop(state, 1);
 
-#ifdef NYI_MATCHES
-        lua_pushliteral(state, "displayable");
-        lua_rawget(state, stack_index);
-        if (lua_isstring(state, -1))
-            desc.displayable = lua_tostring(state, -1);
-        lua_pop(state, 1);
-
-        lua_pushliteral(state, "aux");
-        lua_rawget(state, stack_index);
-        if (lua_isstring(state, -1))
-            desc.aux = lua_tostring(state, -1);
-        lua_pop(state, 1);
-#endif
-
-        lua_pushliteral(state, "suffix");
-        lua_rawget(state, stack_index);
-        if (lua_isstring(state, -1))
-            desc.suffix = lua_tostring(state, -1)[0];
-        lua_pop(state, 1);
-
         lua_pushliteral(state, "type");
         lua_rawget(state, stack_index);
         if (lua_isstring(state, -1))
