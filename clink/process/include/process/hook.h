38a4f7415b0e6019172a2dfada9f93fa721fc355
Chris Antos <sparrowhawk996@gmail.com>
2020-11-06 19:46:23 -0800

    Remove hook_jmp, superseded by Detours.

--- clink/process/include/process/hook.h
+++ clink/process/include/process/hook.h
@@ -6,7 +6,6 @@
 typedef void (__stdcall *hookptr_t)();
 
 hookptr_t hook_iat(void* base, const char* dll, const char* func_name, hookptr_t hook, int find_by_name);
-hookptr_t hook_jmp(void* module, const char* func_name, hookptr_t hook);
 
 struct repair_iat_node;
 
