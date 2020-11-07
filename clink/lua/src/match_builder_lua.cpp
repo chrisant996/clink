631d847f34244dd7ba8513ac9481054655acc7a8
Chris Antos <sparrowhawk996@gmail.com>
2020-11-07 03:04:24 -0800

    Doc adjustments.

--- clink/lua/src/match_builder_lua.cpp
+++ clink/lua/src/match_builder_lua.cpp
@@ -56,10 +56,8 @@ match_builder_lua::~match_builder_lua()
 /// -show:  builder:addmatch({ match=".git", type="dir hidden" })
 /// Adds a match.  If <em>match</em> is a string, in which case it's added as a
 /// match and <em>type</em> (or "none") is the match type.  Or <em>match</em>
-/// can be a table with the following scheme: <em>{ match:string, [suffix:string],
-/// [type:string] }</em>.  If <em>suffix</em> is provided, the first character
-/// is used as a suffix when completing the match (for example "%" for env var
-/// matches).  If <em>type</em> is not provided then "none" is used, otherwise
+/// can be a table with the following scheme: <em>{ match:string, [type:string]
+/// }</em>.  If <em>type</em> is not provided then "none" is used, otherwise
 /// <em>type</em> can be "word", "alias" (doskey macro), "file", "dir", or
 /// "link" (symlink).<br/>
 /// <br/>
