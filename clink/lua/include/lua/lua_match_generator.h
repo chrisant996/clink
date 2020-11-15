76ae916701997a8ef6297f01df9d04f5d4149374
Chris Antos <sparrowhawk996@gmail.com>
2020-11-15 04:30:26 -0800

    Remove stray method declarations.

--- clink/lua/include/lua/lua_match_generator.h
+++ clink/lua/include/lua/lua_match_generator.h
@@ -18,10 +18,6 @@ public:
 private:
     virtual bool    generate(const line_state& line, match_builder& builder) override;
     virtual void    get_word_break_info(const line_state& line, word_break_info& info) const override;
-    void            initialise();
     void            print_error(const char* error) const;
-    void            lua_pushlinestate(const line_state& line);
-    bool            load_script(const char* script);
-    void            load_scripts(const char* path);
     lua_state&      m_state;
 };
