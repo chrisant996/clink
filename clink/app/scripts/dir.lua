e5b4e1545ba9dc7c4b35c998375861814781d9bd
Chris Antos <sparrowhawk996@gmail.com>
2020-11-24 17:50:26 -0800

    Update self completions.

--- clink/app/scripts/dir.lua
+++ clink/app/scripts/dir.lua
@@ -2,21 +2,24 @@
 -- License: http://opensource.org/licenses/MIT
 
 --------------------------------------------------------------------------------
-clink.argmatcher("cd", "chdir", "pushd", "rd", "rmdir", "md", "mkdir"):addarg(
-    function (match_word, word_index, line_state)
-        local word = line_state:getword(word_index)
-        local matches = {}
-        local root = path.getdirectory(word) or ""
-        for _, d in ipairs(os.globdirs(word.."*")) do
-            local dir = path.join(root, d)
+function clink.dir_matches(match_word, word_index, line_state)
+    local word = line_state:getword(word_index)
+    local matches = {}
+    local root = path.getdirectory(word) or ""
+    for _, d in ipairs(os.globdirs(word.."*")) do
+        local dir = path.join(root, d)
 -- TODO: PERFORMANCE: globdirs should return whether each file is hidden since
 -- it already had that knowledge.
-            if os.ishidden(dir) then
-                table.insert(matches, { match = dir, type = "dir,hidden" })
-            else
-                table.insert(matches, { match = dir, type = "dir" })
-            end
+        if os.ishidden(dir) then
+            table.insert(matches, { match = dir, type = "dir,hidden" })
+        else
+            table.insert(matches, { match = dir, type = "dir" })
         end
-        return matches
     end
+    return matches
+end
+
+--------------------------------------------------------------------------------
+clink.argmatcher("cd", "chdir", "pushd", "rd", "rmdir", "md", "mkdir"):addarg(
+    clink.dir_matches
 )
--- clink/app/scripts/self.lua
+++ clink/app/scripts/self.lua
@@ -5,21 +5,36 @@
 local nothing = clink.argmatcher()
 
 --------------------------------------------------------------------------------
+local dir_matcher = clink.argmatcher():addarg(clink.dir_matches)
+
+--------------------------------------------------------------------------------
 local inject = clink.argmatcher()
-:addflags("--help", "--pid", "--profile", "--quiet", "--nolog", "--scripts")
+:addflags(
+    "--help",
+    "--pid",
+    "--profile"..dir_matcher,
+    "--quiet",
+    "--nolog",
+    "--scripts"..dir_matcher)
 
 --------------------------------------------------------------------------------
 local autorun_dashdash = clink.argmatcher()
 :addarg("--" .. inject)
 
 local autorun = clink.argmatcher()
-:addflags("--allusers", "--help")
+:addflags(
+    "--allusers",
+    "--help")
 :addarg(
     "install"   .. autorun_dashdash,
     "uninstall" .. nothing,
     "show"      .. nothing,
-    "set"
-)
+    "set")
+
+--------------------------------------------------------------------------------
+local echo = clink.argmatcher()
+:addflags("--help")
+:nofiles()
 
 --------------------------------------------------------------------------------
 local function set_handler(match_word, word_index, line_state)
@@ -48,8 +63,7 @@ local history = clink.argmatcher()
     "clear"     .. nothing,
     "compact"   .. nothing,
     "delete"    .. nothing,
-    "expand"
-)
+    "expand")
 
 --------------------------------------------------------------------------------
 clink.argmatcher(
@@ -58,9 +72,12 @@ clink.argmatcher(
     "clink_x64.exe")
 :addarg(
     "autorun"   .. autorun,
-    "echo"      .. nothing,
+    "echo"      .. echo,
     "history"   .. history,
     "info"      .. nothing,
     "inject"    .. inject,
-    "set"       .. set
-)
+    "set"       .. set)
+:addflags(
+    "--help",
+    "--profile"..dir_matcher,
+    "--version")
