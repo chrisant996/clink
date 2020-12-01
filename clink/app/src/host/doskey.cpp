chrisant996/clink

commit cdb8558483cc51522ab7cc34d28c397756e35a33

Improved doskey support.
Added expand-doskey-alias command that expands the current line
according to the current doskey aliases.

Fixed so the filtered prompt is displayed between lines of a multi-line doskey
macro.  This also fixes issue #516 in the mridgers/clink repo.

clink/app/src/host/doskey.cpp
@@ -22,10 +22,10 @@ static setting_bool g_enhanced_doskey(
 
 
 //------------------------------------------------------------------------------
-class wstr_stream
+class str_stream
 {
 public:
-    typedef wchar_t         TYPE;
+    typedef char            TYPE;
 
     struct range_desc
     {
@@ -33,7 +33,7 @@ public:
         unsigned int        count;
     };
 
-                            wstr_stream();
+                            str_stream();
     void                    operator << (TYPE c);
     void                    operator << (const range_desc desc);
     void                    collect(str_impl<TYPE>& out);
@@ -42,13 +42,13 @@ public:
 
 private:
     void                    grow(unsigned int hint=128);
-    wchar_t* __restrict     m_start;
-    wchar_t* __restrict     m_end;
-    wchar_t* __restrict     m_cursor;
+    char* __restrict        m_start;
+    char* __restrict        m_end;
+    char* __restrict        m_cursor;
 };
 
 //------------------------------------------------------------------------------
-wstr_stream::wstr_stream()
+str_stream::str_stream()
 : m_start(nullptr)
 , m_end(nullptr)
 , m_cursor(nullptr)
@@ -56,7 +56,7 @@ wstr_stream::wstr_stream()
 }
 
 //------------------------------------------------------------------------------
-void wstr_stream::operator << (TYPE c)
+void str_stream::operator << (TYPE c)
 {
     if (m_cursor >= m_end)
         grow();
@@ -65,7 +65,7 @@ void wstr_stream::operator << (TYPE c)
 }
 
 //------------------------------------------------------------------------------
-void wstr_stream::operator << (const range_desc desc)
+void str_stream::operator << (const range_desc desc)
 {
     if (m_cursor + desc.count >= m_end)
         grow(desc.count);
@@ -75,26 +75,26 @@ void wstr_stream::operator << (const range_desc desc)
 }
 
 //------------------------------------------------------------------------------
-wstr_stream::range_desc wstr_stream::range(const TYPE* ptr, unsigned int count)
+str_stream::range_desc str_stream::range(const TYPE* ptr, unsigned int count)
 {
     return { ptr, count };
 }
 
 //------------------------------------------------------------------------------
-wstr_stream::range_desc wstr_stream::range(const str_iter_impl<TYPE>& iter)
+str_stream::range_desc str_stream::range(const str_iter_impl<TYPE>& iter)
 {
     return { iter.get_pointer(), iter.length() };
 }
 
 //------------------------------------------------------------------------------
-void wstr_stream::collect(str_impl<TYPE>& out)
+void str_stream::collect(str_impl<TYPE>& out)
 {
     out.attach(m_start, int(m_cursor - m_start));
     m_start = m_end = m_cursor = nullptr;
 }
 
 //------------------------------------------------------------------------------
-void wstr_stream::grow(unsigned int hint)
+void str_stream::grow(unsigned int hint)
 {
     hint = (hint + 127) & ~127;
     unsigned int size = int(m_end - m_start) + hint;
@@ -120,7 +120,7 @@ void doskey_alias::reset()
 }
 
 //------------------------------------------------------------------------------
-bool doskey_alias::next(wstr_base& out)
+bool doskey_alias::next(str_base& out)
 {
     if (!*m_cursor)
         return false;
@@ -163,44 +163,46 @@ bool doskey::remove_alias(const char* alias)
 }
 
 //------------------------------------------------------------------------------
-bool doskey::resolve_impl(const wstr_iter& in, wstr_stream* out)
+bool doskey::resolve_impl(const str_iter& in, str_stream* out)
 {
+    // Early out if not output location was provided.
+    if (out == nullptr)
+        return true;
+
     // Find the alias for which to retrieve text for.
-    wstr_tokeniser tokens(in, " ");
-    wstr_iter token;
+    str_tokeniser tokens(in, " ");
+    str_iter token;
     if (!tokens.next(token))
         return false;
 
     // Legacy doskey doesn't allow macros that begin with whitespace so if the
     // token does it won't ever resolve as an alias.
-    const wchar_t* alias_ptr = token.get_pointer();
+    const char* alias_ptr = token.get_pointer();
     if (!g_enhanced_doskey.get() && alias_ptr != in.get_pointer())
         return false;
 
-    wstr<32> alias;
+    str<32> alias;
     alias.concat(alias_ptr, token.length());
+    wstr<32> walias(alias.c_str());
 
     // Find the alias' text. First check it exists.
     wchar_t unused;
     wstr<32> wshell(m_shell_name);
-    if (!GetConsoleAliasW(alias.data(), &unused, sizeof(unused), wshell.data()))
+    if (!GetConsoleAliasW(walias.data(), &unused, sizeof(unused), wshell.data()))
         if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
             return false;
 
     // It does. Allocate space and fetch it.
-    wstr<4> text;
-    text.reserve(512);
-    GetConsoleAliasW(alias.data(), text.data(), text.size(), wshell.data());
-
-    // Early out if not output location was provided.
-    if (out == nullptr)
-        return true;
+    wstr<4> wtext;
+    wtext.reserve(8192);
+    GetConsoleAliasW(walias.data(), wtext.data(), wtext.size(), wshell.data());
+    str<4> text(wtext.c_str());
 
     // Collect the remaining tokens.
     if (g_enhanced_doskey.get())
         tokens.add_quote_pair("\"");
 
-    struct arg_desc { const wchar_t* ptr; int length; };
+    struct arg_desc { const char* ptr; int length; };
     fixed_array<arg_desc, 10> args;
     arg_desc* desc;
     while (tokens.next(token) && (desc = args.push_back()))
@@ -210,8 +212,8 @@ bool doskey::resolve_impl(const wstr_iter& in, wstr_stream* out)
     }
 
     // Expand the alias' text into 'out'.
-    wstr_stream& stream = *out;
-    const wchar_t* read = text.c_str();
+    str_stream& stream = *out;
+    const char* read = text.c_str();
     for (int c = *read; c = *read; ++read)
     {
         if (c != '$')
@@ -251,14 +253,14 @@ bool doskey::resolve_impl(const wstr_iter& in, wstr_stream* out)
         // 'c' is now the arg index or -1 if it is all of them to be inserted.
         if (c < 0)
         {
-            const wchar_t* end = in.get_pointer() + in.length();
-            const wchar_t* start = args.front()->ptr;
-            stream << wstr_stream::range(start, int(end - start));
+            const char* end = in.get_pointer() + in.length();
+            const char* start = args.front()->ptr;
+            stream << str_stream::range(start, int(end - start));
         }
         else if (c < arg_count)
         {
             const arg_desc& desc = args.front()[c];
-            stream << wstr_stream::range(desc.ptr, desc.length);
+            stream << str_stream::range(desc.ptr, desc.length);
         }
     }
 
@@ -266,19 +268,19 @@ bool doskey::resolve_impl(const wstr_iter& in, wstr_stream* out)
 }
 
 //------------------------------------------------------------------------------
-void doskey::resolve(const wchar_t* chars, doskey_alias& out)
+void doskey::resolve(const char* chars, doskey_alias& out)
 {
     out.reset();
 
-    wstr_stream stream;
+    str_stream stream;
     if (g_enhanced_doskey.get())
     {
-        wstr_iter command;
+        str_iter command;
 
         // Coarse check to see if there's any aliases to resolve
         {
             bool resolves = false;
-            wstr_tokeniser commands(chars, "&|");
+            str_tokeniser commands(chars, "&|");
             commands.add_quote_pair("\"");
             while (commands.next(command))
                 if (resolves = resolve_impl(command, nullptr))
@@ -290,18 +292,18 @@ void doskey::resolve(const wchar_t* chars, doskey_alias& out)
 
         // This line will expand aliases so lets do that.
         {
-            const wchar_t* last = chars;
-            wstr_tokeniser commands(chars, "&|");
+            const char* last = chars;
+            str_tokeniser commands(chars, "&|");
             commands.add_quote_pair("\"");
             while (commands.next(command))
             {
                 // Copy delimiters into the output buffer verbatim.
                 if (int delim_length = int(command.get_pointer() - last))
-                    stream << wstr_stream::range(last, delim_length);
+                    stream << str_stream::range(last, delim_length);
                 last = command.get_pointer() + command.length();
 
                 if (!resolve_impl(command, &stream))
-                    stream << wstr_stream::range(command);
+                    stream << str_stream::range(command);
             }
 
             // Append any trailing delimiters too.
@@ -309,7 +311,7 @@ void doskey::resolve(const wchar_t* chars, doskey_alias& out)
                 stream << *last++;
         }
     }
-    else if (!resolve_impl(wstr_iter(chars), &stream))
+    else if (!resolve_impl(str_iter(chars), &stream))
         return;
 
     // Double null-terminated as aliases with $T become and array of commands.

clink/app/test/doskey.cpp
@@ -39,14 +39,14 @@ TEST_CASE("Doskey expand : simple")
         doskey doskey("shell");
         doskey.add_alias("alias", "text");
 
-        wstr<> line(L"alias");
+        str<> line("alias");
 
         doskey_alias alias;
         doskey.resolve(line.data(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L"text") == true);
+        REQUIRE(line.equals("text") == true);
         REQUIRE(alias.next(line) == false);
 
         doskey.remove_alias("alias");
@@ -63,7 +63,7 @@ TEST_CASE("Doskey expand : leading")
         doskey doskey("shell");
         doskey.add_alias("alias", "text");
 
-        wstr<> line(L" alias");
+        str<> line(" alias");
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
@@ -86,23 +86,23 @@ TEST_CASE("Doskey args $1-9")
         doskey doskey("shell");
         doskey.add_alias("alias", " $1$2 $3$5$6$7$8$9 "); // no $4 deliberately
 
-        wstr<> line(L"alias a b c d e f g h i j k l");
+        str<> line(L"alias a b c d e f g h i j k l");
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L" ab cefghi ") == true);
+        REQUIRE(line.equals(" ab cefghi ") == true);
         REQUIRE(alias.next(line) == false);
 
-        line = L"alias a b c d e";
+        line = "alias a b c d e";
 
         doskey.resolve(line.c_str(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L" ab ce ") == true);
+        REQUIRE(line.equals(" ab ce ") == true);
         REQUIRE(alias.next(line) == false);
 
         doskey.remove_alias("alias");
@@ -119,14 +119,14 @@ TEST_CASE("Doskey args $*")
         doskey doskey("shell");
         doskey.add_alias("alias", " $* ");
 
-        wstr<> line(L"alias a b c d e f g h i j k l m n o p");
+        str<> line(L"alias a b c d e f g h i j k l m n o p");
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L" a b c d e f g h i j k l m n o p ") == true);
+        REQUIRE(line.equals(" a b c d e f g h i j k l m n o p ") == true);
         REQUIRE(alias.next(line) == false);
 
         doskey.remove_alias("alias");
@@ -138,10 +138,10 @@ TEST_CASE("Doskey args $*")
         doskey doskey("shell");
         doskey.add_alias("alias", "$*");
 
-        wstr<> line;
-        line << L"alias ";
+        str<> line;
+        line << "alias ";
         for (int i = 0; i < 12; ++i)
-            line << L"0123456789abcdef";
+            line << "0123456789abcdef";
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
@@ -158,14 +158,14 @@ TEST_CASE("Doskey $? chars")
         doskey doskey("shell");
         doskey.add_alias("alias", "$$ $g$G $l$L $b$B $Z");
 
-        wstr<> line(L"alias");
+        str<> line("alias");
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L"$ >> << || $Z") == true);
+        REQUIRE(line.equals("$ >> << || $Z") == true);
         REQUIRE(alias.next(line) == false);
 
         doskey.remove_alias("alias");
@@ -182,20 +182,20 @@ TEST_CASE("Doskey multi-command")
         doskey doskey("shell");
         doskey.add_alias("alias", "one $3 $t $2 two_$T$*three");
 
-        wstr<> line(L"alias a b c");
+        str<> line(L"alias a b c");
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
         REQUIRE(alias);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L"one c ") == true);
+        REQUIRE(line.equals("one c ") == true);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L" b two_") == true);
+        REQUIRE(line.equals(" b two_") == true);
 
         REQUIRE(alias.next(line) == true);
-        REQUIRE(line.equals(L"a b cthree") == true);
+        REQUIRE(line.equals("a b cthree") == true);
 
         REQUIRE(alias.next(line) == false);
 
@@ -211,16 +211,16 @@ TEST_CASE("Doskey pipe/redirect")
     doskey doskey("shell");
     doskey.add_alias("alias", "one $*");
 
-    wstr<> line(L"alias|piped");
+    str<> line("alias|piped");
     doskey_alias alias;
     doskey.resolve(line.c_str(), alias);
     REQUIRE(!alias);
 
-    line = L"alias |piped";
+    line = "alias |piped";
     doskey.resolve(line.c_str(), alias);
     REQUIRE(alias);
     REQUIRE(alias.next(line) == true);
-    REQUIRE(line.equals(L"one |piped") == true);
+    REQUIRE(line.equals("one |piped") == true);
 
     doskey.remove_alias("alias");
 }
@@ -232,8 +232,8 @@ TEST_CASE("Doskey pipe/redirect : new")
 
     doskey doskey("shell");
 
-    auto test = [&] (const wchar_t* input, const wchar_t* output) {
-        wstr<> line(input);
+    auto test = [&] (const char* input, const char* output) {
+        str<> line(input);
 
         doskey_alias alias;
         doskey.resolve(line.c_str(), alias);
@@ -246,27 +246,27 @@ TEST_CASE("Doskey pipe/redirect : new")
 
     doskey.add_alias("alias", "one");
     SECTION("Basic 1")
-    { test(L"alias|piped", L"one|piped"); }
+    { test("alias|piped", "one|piped"); }
     SECTION("Basic 2")
-    { test(L"alias|alias", L"one|one"); }
+    { test("alias|alias", "one|one"); }
     SECTION("Basic 3")
-    { test(L"alias|alias&alias", L"one|one&one"); }
+    { test("alias|alias&alias", "one|one&one"); }
     SECTION("Basic 4")
-    { test(L"&|alias", L"&|one"); }
+    { test("&|alias", "&|one"); }
     SECTION("Basic 5")
-    { test(L"alias||", L"one||"); }
+    { test("alias||", "one||"); }
     SECTION("Basic 6")
-    { test(L"&&alias&|", L"&&one&|"); }
+    { test("&&alias&|", "&&one&|"); }
     SECTION("Basic 5")
-    { test(L"alias|x|alias", L"one|x|one"); }
+    { test("alias|x|alias", "one|x|one"); }
     doskey.remove_alias("alias");
 
-    #define ARGS L"two \"three four\" 5"
+    #define ARGS "two \"three four\" 5"
     doskey.add_alias("alias", "cmd $1 $2 $3");
     SECTION("Args 1")
-    { test(L"alias " ARGS L"|piped", L"cmd " ARGS L"|piped"); }
+    { test("alias " ARGS "|piped", "cmd " ARGS "|piped"); }
     SECTION("Args 2")
-    { test(L"alias " ARGS L"|alias " ARGS, L"cmd " ARGS L"|cmd " ARGS); }
+    { test("alias " ARGS "|alias " ARGS, "cmd " ARGS "|cmd " ARGS); }
     doskey.remove_alias("alias");
     #undef ARGS
 }

clink/app/src/host/doskey.h
@@ -13,13 +13,13 @@ class doskey_alias
 public:
                     doskey_alias();
     void            reset();
-    bool            next(wstr_base& out);
+    bool            next(str_base& out);
     explicit        operator bool () const;
 
 private:
     friend class    doskey;
-    wstr<32>        m_buffer;
-    const wchar_t*  m_cursor;
+    str<32>         m_buffer;
+    const char*     m_cursor;
 };
 
 
@@ -31,9 +31,9 @@ public:
                     doskey(const char* shell_name);
     bool            add_alias(const char* alias, const char* text);
     bool            remove_alias(const char* alias);
-    void            resolve(const wchar_t* chars, doskey_alias& out);
+    void            resolve(const char* chars, doskey_alias& out);
 
 private:
-    bool            resolve_impl(const wstr_iter& in, class wstr_stream* out);
+    bool            resolve_impl(const str_iter& in, class str_stream* out);
     const char*     m_shell_name;
 };

clink/app/src/host/host.cpp
@@ -6,6 +6,8 @@
 #include "host_lua.h"
 #include "host_module.h"
 #include "prompt.h"
+#include "doskey.h"
+#include "terminal/terminal_out.h"
 #include "utils/app_context.h"
 #include "utils/scroller.h"
 
@@ -60,8 +62,109 @@ void host_remove_history(int rl_history_index, const char* line)
 }
 
 //------------------------------------------------------------------------------
+static void write_line_feed()
+{
+    HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD written;
+    WriteConsoleW(handle, L"\n", 1, &written, nullptr);
+}
+
+//------------------------------------------------------------------------------
+static bool intercept_directory(const char* line, str_base& dir)
+{
+    // Skip leading whitespace.
+    while (*line == ' ' || *line == '\t')
+        line++;
+
+    // Strip all quotes (it may be surprising, but this is what CMD.EXE does).
+    str<> tmp;
+    while (*line)
+    {
+        if (*line != '\"')
+            tmp.concat(line, 1);
+        line++;
+    }
+
+    // Truncate trailing whitespcae.
+    while (tmp.length())
+    {
+        char ch = tmp.c_str()[tmp.length() - 1];
+        if (ch != ' ' && ch != '\t')
+            break;
+        tmp.truncate(tmp.length() - 1);
+    }
+
+    if (!tmp.length() ||
+        os::get_path_type(tmp.c_str()) != os::path_type_dir)
+        return false;
+
+    if (!tmp.equals(".") &&
+        !tmp.equals(".."))
+    {
+        // If all dots, convert into valid path syntax moving N-1 levels.
+        // Examples:
+        //  - "..." becomes "..\..\"
+        //  - "...." becomes "..\..\..\"
+        int num_dots = 0;
+        for (const char* p = tmp.c_str(); *p; ++p, ++num_dots)
+        {
+            if (*p != '.')
+            {
+                num_dots = -1;
+                break;
+            }
+        }
+        if (num_dots >= 3)
+        {
+            tmp.clear();
+            while (num_dots > 1)
+            {
+                tmp.concat("..\\");
+                --num_dots;
+            }
+        }
+
+        // If the input doesn't end with a separator, don't handle it.
+        // Otherwise it would interfere with launching something found on the
+        // PATH but which happens to have the same name as a subdirectory of the
+        // current working directory.
+        if (!path::is_separator(tmp.c_str()[tmp.length() - 1]))
+        {
+            // But allow a special case for "..\.." and "..\..\..", etc.
+            const char* p = tmp.c_str();
+            while (true)
+            {
+                if (p[0] != '.' || p[1] != '.')
+                    return false;
+                if (p[2] == '\0')
+                    break;
+                if (!path::is_separator(p[2]))
+                    return false;
+                p += 3;
+            }
+        }
+
+        // Stop trimming trailing path separators once there's only 1 character,
+        // or when the preceding character is a colon.  Otherwise there's no way
+        // to change to the root directory.
+        while (tmp.length() > 1 && path::is_separator(tmp.c_str()[tmp.length() - 1]))
+        {
+            if (tmp.length() > 2 && tmp.c_str()[tmp.length() - 2] == ':')
+                break;
+            tmp.truncate(tmp.length() - 1);
+        }
+    }
+
+    dir = tmp.c_str();
+    return true;
+}
+
+
+
+//------------------------------------------------------------------------------
 host::host(const char* name)
 : m_name(name)
+, m_doskey("cmd.exe")
 {
 }
 
@@ -142,10 +245,25 @@ bool host::edit_line(const char* prompt, str_base& out)
 
     s_history_db = &m_history;
 
+    bool resolved = false;
     bool ret = false;
     while (1)
     {
-        if (ret = editor->edit(out))
+        // Doskey is implemented on the server side of a ReadConsoleW() call
+        // (i.e. in conhost.exe). Commands separated by a "$T" are returned one
+        // command at a time through successive calls to ReadConsoleW().
+        resolved = false;
+        if (m_doskey_alias.next(out))
+        {
+            terminal.out->begin();
+            terminal.out->write(filtered_prompt.c_str(), filtered_prompt.length());
+            terminal.out->write(out.c_str(), out.length());
+            terminal.out->end();
+            write_line_feed();
+            resolved = true;
+            ret = true;
+        }
+        else if (ret = editor->edit(out))
         {
             // Handle history event expansion.
             if (m_history.expand(out.c_str(), out) == history_db::expand_print)
@@ -184,7 +302,24 @@ bool host::edit_line(const char* prompt, str_base& out)
             // Add the line to the history.
             m_history.add(out.c_str());
         }
+
+        if (ret)
+        {
+            // If the line is a directory, change to the directory and return a
+            // blank line.
+            if (intercept_directory(out.c_str(), cwd.m_path))
+            {
+                out.clear();
+                write_line_feed();
+            }
+        }
         break;
+    }
+
+    if (!resolved)
+    {
+        m_doskey.resolve(out.c_str(), m_doskey_alias);
+        m_doskey_alias.next(out);
     }
 
     s_history_db = nullptr;

clink/app/src/host/host.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "doskey.h"
 #include "history/history_db.h"
 
 #include <lib/line_editor.h>
@@ -28,5 +29,7 @@ protected:
 private:
     void            filter_prompt(const char* in, str_base& out);
     const char*     m_name;
+    doskey          m_doskey;
+    doskey_alias    m_doskey_alias;
     history_db      m_history;
 };

clink/app/src/host/host_cmd.cpp
@@ -132,101 +132,11 @@ void tag_prompt()
 }
 
 //------------------------------------------------------------------------------
-static bool intercept_directory(const char* line)
-{
-    // Skip leading whitespace.
-    while (*line == ' ' || *line == '\t')
-        line++;
-
-    // Strip all quotes (it may be surprising, but this is what CMD.EXE does).
-    str<> tmp;
-    while (*line)
-    {
-        if (*line != '\"')
-            tmp.concat(line, 1);
-        line++;
-    }
-
-    // Truncate trailing whitespcae.
-    while (tmp.length())
-    {
-        char ch = tmp.c_str()[tmp.length() - 1];
-        if (ch != ' ' && ch != '\t')
-            break;
-        tmp.truncate(tmp.length() - 1);
-    }
-
-    if (!tmp.length() ||
-        os::get_path_type(tmp.c_str()) != os::path_type_dir)
-        return false;
-
-    if (!tmp.equals(".") &&
-        !tmp.equals(".."))
-    {
-        // If all dots, convert into valid path syntax moving N-1 levels.
-        // Examples:
-        //  - "..." becomes "..\..\"
-        //  - "...." becomes "..\..\..\"
-        int num_dots = 0;
-        for (const char* p = tmp.c_str(); *p; ++p, ++num_dots)
-        {
-            if (*p != '.')
-            {
-                num_dots = -1;
-                break;
-            }
-        }
-        if (num_dots >= 3)
-        {
-            tmp.clear();
-            while (num_dots > 1)
-            {
-                tmp.concat("..\\");
-                --num_dots;
-            }
-        }
-
-        // If the input doesn't end with a separator, don't handle it.
-        // Otherwise it would interfere with launching something found on the
-        // PATH but which happens to have the same name as a subdirectory of the
-        // current working directory.
-        if (!path::is_separator(tmp.c_str()[tmp.length() - 1]))
-        {
-            // But allow a special case for "..\.." and "..\..\..", etc.
-            const char* p = tmp.c_str();
-            while (true)
-            {
-                if (p[0] != '.' || p[1] != '.')
-                    return false;
-                if (p[2] == '\0')
-                    break;
-                if (!path::is_separator(p[2]))
-                    return false;
-                p += 3;
-            }
-        }
-
-        // Stop trimming trailing path separators once there's only 1 character,
-        // or when the preceding character is a colon.  Otherwise there's no way
-        // to change to the root directory.
-        while (tmp.length() > 1 && path::is_separator(tmp.c_str()[tmp.length() - 1]))
-        {
-            if (tmp.length() > 2 && tmp.c_str()[tmp.length() - 2] == ':')
-                break;
-            tmp.truncate(tmp.length() - 1);
-        }
-    }
-
-    os::set_current_dir(tmp.c_str());
-    return true;
-}
-
-//------------------------------------------------------------------------------
 static void write_line_feed()
 {
     HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
     DWORD written;
-    WriteConsole(handle, L"\n", 1, &written, nullptr);
+    WriteConsoleW(handle, L"\n", 1, &written, nullptr);
 }
 
 
@@ -342,12 +252,8 @@ bool host_cmd::is_interactive() const
 //------------------------------------------------------------------------------
 void host_cmd::edit_line(const wchar_t* prompt, wchar_t* chars, int max_chars)
 {
-    // Doskey is implemented on the server side of a ReadConsoleW() call (i.e.
-    // in conhost.exe). Commands separated by a "$T" are returned one command
-    // at a time through successive calls to ReadConsoleW().
-    wstr_base out(chars, max_chars);
-    if (m_doskey_alias.next(out))
-        return;
+    bool resolved = false;
+    wstr_base wout(chars, max_chars);
 
     // Convert the prompt to Utf8 and parse backspaces in the string.
     str<128> utf8_prompt(m_prompt.get());
@@ -367,19 +273,8 @@ void host_cmd::edit_line(const wchar_t* prompt, wchar_t* chars, int max_chars)
         while (1)
         {
             out.clear();
-            bool ok = host::edit_line(utf8_prompt.c_str(), out);
-            if (ok)
+            if (host::edit_line(utf8_prompt.c_str(), out))
             {
-#ifdef CLINK_CHRISANT_MODS
-                // If the line is a directory, change to the directory and give
-                // the host a blank line.
-                if (intercept_directory(out.c_str()))
-                {
-                    out.clear();
-                    write_line_feed();
-                }
-#endif
-
                 to_utf16(chars, max_chars, out.c_str());
                 break;
             }
@@ -393,9 +288,6 @@ void host_cmd::edit_line(const wchar_t* prompt, wchar_t* chars, int max_chars)
             write_line_feed();
         }
     }
-
-    m_doskey.resolve(chars, m_doskey_alias);
-    m_doskey_alias.next(out);
 }
 
 //------------------------------------------------------------------------------

clink/app/src/host/host_module.cpp
@@ -3,12 +3,17 @@
 
 #include "pch.h"
 #include "host_module.h"
+#include "doskey.h"
 
 #include <core/path.h>
 #include <core/settings.h>
 #include <core/str.h>
 #include <lib/line_buffer.h>
 #include <terminal/printer.h>
+
+extern "C" {
+#include <readline/readline.h>
+}
 
 //------------------------------------------------------------------------------
 static setting_enum g_paste_crlf(
@@ -56,6 +61,27 @@ static setting_str g_key_expand_env(
     "\\M-C-e");
 
 
+
+//------------------------------------------------------------------------------
+// Expands a doskey alias (but only the first line, if $T is present).
+int expand_doskey_alias(int count, int invoking_key)
+{
+    doskey_alias alias;
+    doskey doskey("cmd.exe");
+    doskey.resolve(rl_line_buffer, alias);
+
+    str<> expand;
+    alias.next(expand);
+
+    rl_begin_undo_group();
+    rl_delete_text(0, rl_end);
+    rl_point = 0;
+    if (!expand.empty())
+        rl_insert_text(expand.c_str());
+    rl_end_undo_group();
+
+    return 0;
+}
 
 //------------------------------------------------------------------------------
 static void ctrl_c(
@@ -256,6 +282,9 @@ enum
 host_module::host_module(const char* host_name)
 : m_host_name(host_name)
 {
+#ifdef CLINK_CHRISANT_MODS
+    rl_add_funmap_entry("expand-doskey-alias", expand_doskey_alias);
+#endif
 }
 
 //------------------------------------------------------------------------------

commit 5d72fba76aa8a6f0330b1cf480190c985296396f

Fix `....` (broken by cdb8558).

clink/app/src/host/doskey.cpp
@@ -171,10 +171,6 @@ bool doskey::remove_alias(const char* alias)
 //------------------------------------------------------------------------------
 bool doskey::resolve_impl(const str_iter& in, str_stream* out)
 {
-    // Early out if not output location was provided.
-    if (out == nullptr)
-        return true;
-
     // Find the alias for which to retrieve text for.
     str_tokeniser tokens(in, " ");
     str_iter token;
@@ -203,6 +199,12 @@ bool doskey::resolve_impl(const str_iter& in, str_stream* out)
     wtext.reserve(8192, true/*exact*/);
     GetConsoleAliasW(walias.data(), wtext.data(), wtext.size(), wshell.data());
     str<4> text(wtext.c_str());
+
+    // Early out if no output location was provided:  return true because the
+    // command is an alias that needs to be expanded (this is unreachable if the
+    // GetConsoleAliasW call doesn't find an alias).
+    if (out == nullptr)
+        return true;
 
     // Collect the remaining tokens.
     if (g_enhanced_doskey.get())