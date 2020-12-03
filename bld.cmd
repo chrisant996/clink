af718eec453796fb5b20001623a72fcc0ee5716a
Chris Antos <sparrowhawk996@gmail.com>
2020-12-02 18:11:05 -0800

    Add scripts for building from the command line.

---
+++ bld.cmd
@@ -0,0 +1,107 @@
+@echo off
+setlocal
+
+set EC=echo
+set ASC=(B
+set LINE=(0
+set BOLD=[1m
+set UNDER=[4m
+set NEG=[7m
+set POS=[27m
+set CYAN=[36m
+set NORM=%ASC%[0m
+
+set __debug_config=Debug
+set __release_config=Release
+set __final_config=Final
+set __x86_platform=Win32
+set __x64_platform=x64
+
+set __AMD64=
+set __MULTICPU=-m:4
+set __STOPONERROR=
+set __INSTALL=
+set __CONFIG=%__debug_config%
+set __PLATFORM=%__x64_platform%
+set __TARGETS=
+set __MSBUILD="c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+rem if /i "%PROCESSOR_ARCHITECTURE%" == "AMD64" set __MSBUILD="c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe"
+
+goto :doarg
+
+:nextarg
+shift
+
+:doarg
+if "%1" == "?" goto :usage
+if "%1" == "??" goto :msbuildusage
+if "%1" == "/?" goto :usage
+if "%1" == "/??" goto :msbuildusage
+if "%1" == "-?" goto :usage
+if "%1" == "-??" goto :msbuildusage
+if "%1" == "/h" goto :usage
+if "%1" == "-h" goto :usage
+if "%1" == "/help" goto :usage
+if "%1" == "-help" goto :usage
+if "%1" == "--help" goto :usage
+if "%1" == "/x86" ( set __PLATFORM=%__x86_platform%&goto :nextarg )
+if "%1" == "/win32" ( set __PLATFORM=%__x86_platform%&goto :nextarg )
+if "%1" == "/x64" ( set __PLATFORM=%__x64_platform%&goto :nextarg )
+if "%1" == "/amd64" ( set __PLATFORM=%__x64_platform%&goto :nextarg )
+if "%1" == "/dbg" ( set __CONFIG=%__debug_config%&goto :nextarg )
+if "%1" == "/debug" ( set __CONFIG=%__debug_config%&goto :nextarg )
+if "%1" == "/fin" ( set __CONFIG=%__final_config%&goto :nextarg )
+if "%1" == "/final" ( set __CONFIG=%__final_config%&goto :nextarg )
+if "%1" == "/rel" ( set __CONFIG=%__release_config%&goto :nextarg )
+if "%1" == "/release" ( set __CONFIG=%__release_config%&goto :nextarg )
+if "%1" == "/single" ( set __MULTICPU=&goto :nextarg )
+if "%1" == "--single" ( set __MULTICPU=&goto :nextarg )
+if "%1" == "/multi" ( set __MULTICPU=-m:6&goto :nextarg )
+if "%1" == "--multi" ( set __MULTICPU=-m:6&goto :nextarg )
+if "%1" == "/t" ( set __TARGETS=%__TARGETS% -t:%2&shift&goto :nextarg )
+if "%1" == "-t" ( set __TARGETS=%__TARGETS% -t:%2&shift&goto :nextarg )
+if "%1" == "/target" ( set __TARGETS=%__TARGETS% -t:%2&shift&goto :nextarg )
+if "%1" == "--target" ( set __TARGETS=%__TARGETS% -t:%2&shift&goto :nextarg )
+:notminus
+
+set __ARGS=
+:appendarg
+if not "%1" == "" set __ARGS=%__ARGS% %1&shift&goto appendarg
+
+echo.
+%EC% %BOLD%%NEG% BUILDING %__PLATFORM% %__CONFIG% %POS%%NORM%
+%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=%__CONFIG% -p:Platform=%__PLATFORM% %__TARGETS% %__ARGS% ".build\vs2019\clink.sln"
+if errorlevel 1 goto :eof
+
+goto :eof
+
+:usage
+%EC% %BOLD%Usage:%NORM%  BLD [/dbg /rel /fin /x86 /x64 /multi /single] [msbuild_options]
+echo.
+echo Builds DEBUG x64 by default.
+echo.
+echo   /x86          Builds x86.
+echo   /x64          Builds x64 %CYAN%(default)%NORM%.
+echo   /dbg          Builds DEBUG %CYAN%(default)%NORM%.
+echo   /rel          Builds RELEASE.
+echo   /fin          Builds FINAL.
+echo.
+echo   /multi        Multi processor build %CYAN%(default)%NORM%.
+echo   /single       Single processor build.
+echo.
+echo   /t target     Build target.
+echo.
+echo.  /??           Show MSBuild help.
+echo.
+echo Aliases:
+echo   /x86             Or /win32.
+echo   /x64             Or /amd64.
+echo   /dbg             Or /debug.
+echo   /rel             Or /release.
+echo   /fin             Or /final.
+goto :eof
+
+:msbuildusage
+%__MSBUILD% -help
+goto :eof
+
---
+++ bldall.cmd
@@ -0,0 +1,153 @@
+@echo off
+setlocal
+
+set EC=echo
+set CP=cp.exe /uy
+set ASC=(B
+set LINE=(0
+set BOLD=[1m
+set UNDER=[4m
+set NEG=[7m%ASC%
+set POS=[27m%LINE%
+set NORM=%ASC%[0m
+
+set __X86=
+set __AMD64=
+set __DEBUG=
+set __RELEASE=
+set __FINAL=
+set __MULTICPU=-m:4
+set __STOPONERROR=
+set __INSTALL=
+set __INSTALLDIR=c:\wbin\clink
+set __MSBUILD="c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+rem if /i "%PROCESSOR_ARCHITECTURE%" == "AMD64" set __MSBUILD="c:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64\MSBuild.exe"
+
+set CP=cp.exe /uy
+set XCOPY=xcopy.exe /cdfiky
+
+goto testarg
+
+:nextarg
+shift
+:testarg
+if "%1" == "/x86" ( set __X86=1%&goto :nextarg )
+if "%1" == "/win32" ( set __X86=1%&goto :nextarg )
+if "%1" == "/x64" ( set __AMD64=1&goto :nextarg )
+if "%1" == "/amd64" ( set __AMD64=1&goto :nextarg )
+if "%1" == "/dbg" ( set __DEBUG=1&goto :nextarg )
+if "%1" == "/debug" ( set __DEBUG=1&goto :nextarg )
+if "%1" == "/rel" ( set __RELEASE=1&goto :nextarg )
+if "%1" == "/release" ( set __RELEASE=1&goto :nextarg )
+if "%1" == "/fin" ( set __FINAL=1&goto :nextarg )
+if "%1" == "/final" ( set __FINAL=1&goto :nextarg )
+rem if "%1" == "-q" ( set __MULTICPU=-m:1&set __STOPONERROR=yes&goto :nextarg )
+if "%1" == "install" ( set __INSTALL=final&set __FINAL=1&goto :nextarg )
+if "%1" == "install:debug" ( set __INSTALL=debug&set __DEBUG=1&goto :nextarg )
+if "%1" == "install:release" ( set __INSTALL=release&set __RELEASE=1&goto :nextarg )
+if "%1" == "install:final" ( set __INSTALL=final&set __FINAL=1&goto :nextarg )
+
+if not "%1" == "" (
+	%EC% %BOLD%Usage:%NORM%  BLDALL [/x86 /amd64 /dbg /shp] [install]
+	echo.
+	echo   By default, builds DEBUG for both x86 and x64.
+	echo.
+	echo Options:
+	echo   /x86             Build x86.
+	echo   /x64             Build x64.
+	echo   /dbg             Build DEBUG.
+	echo   /rel             Build RELEASE.
+	echo   /fin             Build FINAL.
+rem	echo   -q               Quit immediately on any error.
+	echo   install          Copy FINAL build results to the install directory.
+	echo   install:debug    Copy DEBUG build results to the install directory.
+	echo   install:release  Copy RELEASE build results to the install directory.
+	echo   install:final    Copy FINAL build results to the install directory.
+	echo.
+	echo Aliases:
+	echo   /x86             Or /win32.
+	echo   /x64             Or /amd64.
+	echo   /dbg             Or /debug.
+	echo   /rel             Or /release.
+	echo   /fin             Or /final.
+	goto :eof
+)
+
+if "%__X86%" == "" ( if "%__AMD64%" == "" (set __X86=1&set __AMD64=1) )
+if "%__DEBUG%" == "" ( if "%__RELEASE%" == "" ( if "%__FINAL%" == "" (set __DEBUG=1) ) )
+
+if not "%__X86%" == "" ( if not "%__DEBUG%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING x86 DEBUG %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Debug;Platform=Win32 ".build\vs2019\clink.sln"
+	if "%__AMD64%" == "" (
+		if errorlevel 1 goto :eof
+	)
+) )
+
+if not "%__AMD64%" == "" ( if not "%__DEBUG%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING amd64 DEBUG %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Debug;Platform=x64 ".build\vs2019\clink.sln"
+	if errorlevel 1 goto :eof
+) )
+
+if not "%__X86%" == "" ( if not "%__RELEASE%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING x86 RELEASE %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Release;Platform=Win32 ".build\vs2019\clink.sln"
+	if "%__AMD64%" == "" (
+		if errorlevel 1 goto :eof
+	)
+) )
+
+if not "%__AMD64%" == "" ( if not "%__RELEASE%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING amd64 RELEASE %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Release;Platform=x64 ".build\vs2019\clink.sln"
+	if errorlevel 1 goto :eof
+) )
+
+if not "%__X86%" == "" ( if not "%__FINAL%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING x86 FINAL %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Final;Platform=Win32 ".build\vs2019\clink.sln"
+	if "%__AMD64%" == "" (
+		if errorlevel 1 goto :eof
+	)
+) )
+
+if not "%__AMD64%" == "" ( if not "%__FINAL%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% BUILDING amd64 FINAL %POS%%NORM%
+	%__MSBUILD% -nologo -v:minimal %__MULTICPU% -p:Configuration=Final;Platform=x64 ".build\vs2019\clink.sln"
+	if errorlevel 1 goto :eof
+) )
+
+set __INSTALLTYPE=
+if "%__INSTALL%" == "debug" ( if not "%__DEBUG%" == "" ( if not "%__X86%" == "" ( if not "%__AMD64%" == "" (
+	set __INSTALLTYPE=DEBUG
+) ) ) )
+if "%__INSTALL%" == "release" ( if not "%__RELEASE%" == "" ( if not "%__X86%" == "" ( if not "%__AMD64%" == "" (
+	set __INSTALLTYPE=RELEASE
+) ) ) )
+if "%__INSTALL%" == "final" ( if not "%__FINAL%" == "" ( if not "%__X86%" == "" ( if not "%__AMD64%" == "" (
+	set __INSTALLTYPE=FINAL
+) ) ) )
+
+if not "%__INSTALLTYPE%" == "" (
+	echo.
+	%EC% %BOLD%%NEG% INSTALLING %__INSTALLTYPE% OUTPUTS %POS%%NORM%
+	if "%USERNAME%" == "%chrisant%" (
+		%CP% .build\vs2019\bin\%__INSTALLTYPE%\clink.bat;clink.lua;clink_x*.exe;clink_x*.pdb;clink_dll_x*.dll;clink_dll_x*.pdb "%__INSTALLDIR%"
+		%CP% .build\docs\clink.html "%__INSTALLDIR%"
+	) else (
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink.bat "%__INSTALLDIR%"
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink.lua "%__INSTALLDIR%"
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink_x*.exe "%__INSTALLDIR%"
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink_x*.pdb "%__INSTALLDIR%"
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink_dll_x*.dll "%__INSTALLDIR%"
+		%XCOPY% .build\vs2019\bin\%__INSTALLTYPE%\clink_dll_x*.pdb "%__INSTALLDIR%"
+		%XCOPY% .build\docs\clink.html "%__INSTALLDIR%"
+	)
+)
