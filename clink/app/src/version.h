0fd85a813ab00acb719994690ec650ac3a50d383
Chris Antos <sparrowhawk996@gmail.com>
2021-01-03 06:21:47 -0800

    Update version.

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -10,7 +10,7 @@
 
 #define CLINK_VERSION_MAJOR 1
 #define CLINK_VERSION_MINOR 1
-#define CLINK_VERSION_PATCH 17
+#define CLINK_VERSION_PATCH 18
 #define CLINK_VERSION_STR   AS_STR(CLINK_VERSION_MAJOR) ## "." ##\
                             AS_STR(CLINK_VERSION_MINOR) ## "." ##\
                             AS_STR(CLINK_VERSION_PATCH) ##\
