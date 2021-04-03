285dc5d64cbe9ef359f425a39f83ceb5837a5e32
Chris Antos <sparrowhawk996@gmail.com>
2021-04-03 14:13:28 -0700

    Fix product version string in version resource.

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -6,15 +6,22 @@
 #include "clink_commit.h"
 
 #if defined(RC_INVOKED)
-#   define AS_STR(x)        AS_STR_IMPL(x)
-#   define AS_STR_IMPL(x)   #x
+#   define AS_STR(x)            AS_STR_IMPL(x)
+#   define AS_STR_IMPL(x)       #x
+#   define AS_LSTR(x)           AS_LSTR_IMPL(x)
+#   define AS_LSTR_IMPL(x)      L#x
 #endif
 
-#define CLINK_VERSION_MAJOR 1
-#define CLINK_VERSION_MINOR 1
-#define CLINK_VERSION_PATCH 49
-#define CLINK_VERSION_STR   AS_STR(CLINK_VERSION_MAJOR) ## "." ##\
-                            AS_STR(CLINK_VERSION_MINOR) ## "." ##\
-                            AS_STR(CLINK_VERSION_PATCH) ##\
-                            /*"a1" ##*/\
-                            "." ## AS_STR(CLINK_COMMIT)
+#define CLINK_VERSION_MAJOR     1
+#define CLINK_VERSION_MINOR     1
+#define CLINK_VERSION_PATCH     49
+#define CLINK_VERSION_STR       AS_STR(CLINK_VERSION_MAJOR) ## "." ##\
+                                AS_STR(CLINK_VERSION_MINOR) ## "." ##\
+                                AS_STR(CLINK_VERSION_PATCH) ## "." ##\
+                                AS_STR(CLINK_COMMIT)
+#define CLINK_VERSION_LSTR      AS_LSTR(CLINK_VERSION_MAJOR) ## L"." ##\
+                                AS_LSTR(CLINK_VERSION_MINOR) ## L"." ##\
+                                AS_LSTR(CLINK_VERSION_PATCH) ## L"." ##\
+                                AS_LSTR(CLINK_COMMIT)
+
+
