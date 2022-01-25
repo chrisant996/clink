chrisant996/clink

commit 975597898095132e05a72ef02ae249633d669173

Auto-select when to right justify descriptions.
Right justify descriptions when there are more than 1 columns, or when
the max match length is more than 40% of the screen width.

clink/lib/src/selectcomplete_impl.cpp
@@ -228,7 +228,7 @@ cant_activate:
         assert(!m_comment_row_displayed);
         assert(!m_expanded);
         assert(!m_clear_display);
-        m_desc_below = m_matches.has_descriptions() && (m_matches.get_match_count() > 9);
+        m_desc_below = m_matches.has_descriptions() && (m_matches.get_match_count() > DESC_ONE_COLUMN_THRESHOLD);
         m_any_displayed = false;
         m_comment_row_displayed = false;
         m_can_prompt = g_preview_rows.get() <= 0;
@@ -968,15 +968,12 @@ void selectcomplete_impl::update_layout()
 #endif
         const bool best_fit = g_match_best_fit.get();
         const int limit_fit = g_match_limit_fitted.get();
-        m_widths = calculate_columns(&m_matches, best_fit ? limit_fit : -1, m_desc_below, col_extra);
+        const bool one_column = !m_desc_below && m_matches.get_match_count() <= DESC_ONE_COLUMN_THRESHOLD;
+        m_widths = calculate_columns(&m_matches, best_fit ? limit_fit : -1, one_column, m_desc_below, col_extra);
         m_calc_widths = false;
     }
 
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-    const int cols_that_fit = desc_inline ? 1 : m_widths.num_columns();
-#else
     const int cols_that_fit = m_widths.num_columns();
-#endif
     m_match_cols = max<int>(1, cols_that_fit);
     m_match_rows = (m_matches.get_match_count() + (m_match_cols - 1)) / m_match_cols;
 
@@ -1137,11 +1134,10 @@ void selectcomplete_impl::update_display()
                         if (i >= count)
                             break;
 
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-                        const int col_max = (show_descriptions ? m_screen_cols - 1 : m_widths.column_width(col)) - col_extra;
-#else
-                        const int col_max = ((show_descriptions && m_match_cols == 1) ? min<int>(m_widths.m_max_len, m_screen_cols - 1) : m_widths.column_width(col)) - col_extra;
-#endif
+                        const bool right_justify = m_widths.m_right_justify;
+                        const int col_max = ((show_descriptions && !right_justify) ?
+                                             m_screen_cols - 1 :
+                                             m_widths.column_width(col)) - col_extra;
 
                         const int selected = (i == m_index);
                         const char* const display = m_matches.get_match_display(i);
@@ -1204,25 +1200,19 @@ void selectcomplete_impl::update_display()
                         {
                             // Leave at least one space at end of line, or else
                             // "\x1b[K" can erase part of the intended output.
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-                            if (selected)
+                            const int pad_to = (right_justify ?
+                                max<int>(printed_len + m_widths.m_desc_padding, col_max - m_matches.get_match_visible_description(i)) :
+                                m_widths.m_max_match + 4);
+                            if (!right_justify)
                             {
-                                pad_filename(printed_len, m_widths.m_max_match, selected);
+                                pad_filename(printed_len, -m_widths.m_max_match, selected);
                                 printed_len = m_widths.m_max_match;
                             }
-                            const int pad_to = m_widths.m_max_match + 4;
-#else
-                            const unsigned int desc_cells = m_matches.get_match_visible_description(i);
-                            const int pad_to = max<int>(printed_len + m_widths.m_desc_padding, col_max - desc_cells);
-#endif
                             if (pad_to < m_screen_cols - 1)
                             {
                                 pad_filename(printed_len, pad_to, -1);
                                 printed_len = pad_to;
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-#else
-                                if (!selected)
-#endif
+                                if (!selected || !right_justify)
                                     append_tmpbuf_string(description_color, description_color_len);
                                 printed_len += ellipsify_to_callback(desc, col_max - printed_len, false/*expand_ctrl*/, append_tmpbuf_string);
                             }
@@ -1234,10 +1224,7 @@ void selectcomplete_impl::update_display()
                             pad_filename(printed_len, col_max + 1, -1);
                             printed_len = col_max + col_extra;
 
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-#else
                             if (!selected)
-#endif
                                 append_tmpbuf_string("\x1b[36m", 5);
 
                             char _extra[3];

clink/lib/include/lib/display_matches.h
@@ -2,8 +2,6 @@
 
 #include <core/str.h>
 
-#define ONLY_ONE_DESCRIPTION_COLUMN
-
 enum class match_type : unsigned char;
 
 struct match_display_filter_entry
@@ -50,6 +48,8 @@ int ellipsify(const char* in, int limit, str_base& out, bool expand_ctrl);
 
 void free_filtered_matches(match_display_filter_entry** filtered_matches);
 int printable_len(const char* match, match_type type);
+
+#define DESC_ONE_COLUMN_THRESHOLD       9
 
 // Flags in the PACKED MATCH FORMAT:
 #define MATCH_FLAG_APPEND_DISPLAY       0x01

clink/lib/src/column_widths.cpp
@@ -160,7 +160,7 @@ static bool init_column_info(int max_matches, size_t max_cols, size_t count, wid
 //------------------------------------------------------------------------------
 // Calculate the number of columns needed to represent the current set of
 // matches in the current display width.
-column_widths calculate_columns(match_adapter* adapter, int max_matches, bool omit_desc, width_t extra)
+column_widths calculate_columns(match_adapter* adapter, int max_matches, bool one_column, bool omit_desc, width_t extra)
 {
     column_widths widths;
 
@@ -180,7 +180,7 @@ column_widths calculate_columns(match_adapter* adapter, int max_matches, bool om
     const size_t count = adapter->get_match_count();
     size_t max_cols = count < max_idx ? count : max_idx;
 
-    const bool fixed_cols = !init_column_info(max_matches, max_cols, count, col_padding);
+    const bool fixed_cols = !init_column_info(max_matches, max_cols, count, col_padding) || one_column;
 
     // Find the length of the prefix common to all items: length as displayed
     // characters (common_length) and as a byte index into the matches (sind).
@@ -267,8 +267,6 @@ column_widths calculate_columns(match_adapter* adapter, int max_matches, bool om
 
         if (has_descriptions)
         {
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-#else
             const unsigned int desc_cells = adapter->get_match_visible_description(i);
             if (desc_cells)
             {
@@ -276,7 +274,6 @@ column_widths calculate_columns(match_adapter* adapter, int max_matches, bool om
                 if (max_desc < desc_cells)
                     max_desc = desc_cells;
             }
-#endif
         }
 
         if (max_len < len)
@@ -307,18 +304,18 @@ column_widths calculate_columns(match_adapter* adapter, int max_matches, bool om
     assert(common_length <= max_len);
     assert(sind <= max_len);
 
+    widths.m_col_padding = col_padding;
+    widths.m_desc_padding = desc_padding;
     widths.m_sind = sind;
     widths.m_max_len = max_len;
     widths.m_max_match = max_match;
     widths.m_max_desc = max_desc;
     widths.m_can_condense = can_condense;
-    widths.m_col_padding = col_padding;
-    widths.m_desc_padding = desc_padding;
 
     if (fixed_cols)
     {
         const size_t col_max = max_len;
-        const size_t limit = max<size_t>(line_length / col_max, 1);
+        const size_t limit = one_column ? 1 : max<size_t>(line_length / col_max, 1);
         for (size_t i = 0; i < limit; ++i)
             widths.m_widths.push_back(col_max);
     }
@@ -336,6 +333,8 @@ column_widths calculate_columns(match_adapter* adapter, int max_matches, bool om
         for (size_t i = 0; i < cols; ++i, --remove_padding)
             widths.m_widths.push_back(s_column_info[cols - 1].col_arr[i] - (remove_padding ? col_padding : 0));
     }
+
+    widths.m_right_justify = widths.num_columns() > 1 || widths.m_max_match > (line_length * 4) / 10;
 
     return widths;
 }

clink/lib/src/column_widths.h
@@ -26,11 +26,17 @@ struct column_widths
     width_t                 m_max_match = 0;    // Longest individual match string.
     width_t                 m_max_desc = 0;     // Longest individual description string.
     bool                    m_can_condense = false;
+    bool                    m_right_justify = false;
 };
 
 //------------------------------------------------------------------------------
 // Calculates column widths to fit as many columns of matches as possible.
-// MAX_MATCHES < 0 makes all columns are made the same width.
+// MAX_MATCHES < 0 makes all columns the same width.
 // MAX_MATCHES > 0 disables fitting if the matches exceeds MAX_MATCHES.
 // MAX_MATCHES == 0 is unlimited.
-column_widths calculate_columns(match_adapter* adapter, int max_matches=0, bool omit_desc=false, width_t extra=0);
+column_widths calculate_columns(
+    match_adapter* adapter,
+    int max_matches=0,
+    bool one_column=false,
+    bool omit_desc=false,
+    width_t extra=0);

clink/lib/src/display_matches.cpp
@@ -961,11 +961,7 @@ static int display_match_list_internal(match_adapter* adapter, const column_widt
 
     const int cols = complete_get_screenwidth();
     const int show_descriptions = adapter->has_descriptions();
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-    const int limit = show_descriptions ? 1 : widths.num_columns();
-#else
     const int limit = widths.num_columns();
-#endif
     assert(limit > 0);
 
     // How many iterations of the printing loop?
@@ -1008,11 +1004,9 @@ static int display_match_list_internal(match_adapter* adapter, const column_widt
             if (l >= count)
                 break;
 
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-            const int col_max = show_descriptions ? cols - 1 : widths.column_width(j);
-#else
-            const int col_max = (show_descriptions && limit == 1) ? min<int>(widths.m_max_len, cols - 1) : widths.column_width(j);
-#endif
+            const int col_max = ((show_descriptions && !widths.m_right_justify) ?
+                                 cols - 1 :
+                                 widths.column_width(j));
 
             const match_type type = adapter->get_match_type(l);
             const char* const match = adapter->get_match(l);
@@ -1041,12 +1035,9 @@ static int display_match_list_internal(match_adapter* adapter, const column_widt
                 const char* const description = adapter->get_match_description(l);
                 if (description && *description)
                 {
-#ifdef ONLY_ONE_DESCRIPTION_COLUMN
-                    const int pad_to = widths.m_max_match + 4;
-#else
-                    const unsigned int desc_cells = adapter->get_match_visible_description(l);
-                    const int pad_to = max<int>(printed_len + widths.m_desc_padding, col_max - desc_cells);
-#endif
+                    const int pad_to = (widths.m_right_justify ?
+                        max<int>(printed_len + widths.m_desc_padding, col_max - adapter->get_match_visible_description(l)) :
+                        widths.m_max_match + 4);
                     if (pad_to < cols - 1)
                     {
                         pad_filename(printed_len, pad_to, 0);
@@ -1174,13 +1165,14 @@ extern "C" void display_matches(char** matches)
         adapter->set_alt_matches(matches);
     }
 
+    const int count = adapter->get_match_count();
     const bool best_fit = g_match_best_fit.get();
     const int limit_fit = g_match_limit_fitted.get();
-    const column_widths widths = calculate_columns(adapter, best_fit ? limit_fit : -1);
+    const bool one_column = count <= DESC_ONE_COLUMN_THRESHOLD;
+    const column_widths widths = calculate_columns(adapter, best_fit ? limit_fit : -1, one_column);
 
     // If there are many items, then ask the user if she really wants to see
     // them all.
-    const int count = adapter->get_match_count();
     if ((rl_completion_auto_query_items && _rl_screenheight > 0) ?
         display_match_list_internal(adapter, widths, 1) >= (_rl_screenheight - (_rl_vis_botlin + 1)) :
         rl_completion_query_items > 0 && count >= rl_completion_query_items)

commit 260b3c4fa4adaa4e218954131efdfee5b30309dc

Fix accidentally using only one column.
When there are few matches in `clink-select-complete`, then it was
mistakenly using only one column.

Regression introduced by 975597898095132e05a72ef02ae249633d669173,
in v1.3.3.

clink/lib/src/selectcomplete_impl.cpp
@@ -954,8 +954,6 @@ void selectcomplete_impl::update_len()
 //------------------------------------------------------------------------------
 void selectcomplete_impl::update_layout()
 {
-    const bool desc_inline = !m_desc_below && m_matches.has_descriptions();
-
 #ifdef DEBUG
     m_annotate = !!dbg_get_env_int("DEBUG_SHOWTYPES");
     m_col_extra = m_annotate ? 3 : 0;   // Room for space hex hex.
@@ -970,7 +968,8 @@ void selectcomplete_impl::update_layout()
 #endif
         const bool best_fit = g_match_best_fit.get();
         const int limit_fit = g_match_limit_fitted.get();
-        const bool one_column = !m_desc_below && m_matches.get_match_count() <= DESC_ONE_COLUMN_THRESHOLD;
+        const bool desc_inline = !m_desc_below && m_matches.has_descriptions();
+        const bool one_column = desc_inline && m_matches.get_match_count() <= DESC_ONE_COLUMN_THRESHOLD;
         m_widths = calculate_columns(&m_matches, best_fit ? limit_fit : -1, one_column, m_desc_below, col_extra);
         m_calc_widths = false;
     }