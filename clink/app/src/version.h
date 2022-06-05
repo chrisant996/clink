ccd3bbab52b2803d4240b9080ca2aea123b71a25
Chris Antos <sparrowhawk996@gmail.com>
2022-06-05 11:47:16 -0700

    Update version.

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -14,7 +14,7 @@
 
 #define CLINK_VERSION_MAJOR     1
 #define CLINK_VERSION_MINOR     3
-#define CLINK_VERSION_PATCH     18
+#define CLINK_VERSION_PATCH     19
 
 #ifdef _MSC_VER
 #   undef CLINK_VERSION_STR
