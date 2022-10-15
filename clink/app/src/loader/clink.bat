ca8af8d0f20875edf2678343b6220b29ed851d67
Chris Antos <sparrowhawk996@gmail.com>
2022-10-15 12:16:34 -0700

    Fixed #361; detect CLINK_NOAUTORUN faster.
    The only reliable place to detect `%CLINK_NOAUTORUN%` is inside the
    Clink executable code itself.
    
    But that incurs the performance hit of launching Clink, which can be
    noticable when spawning a large number of CMD processes rapidly.
    
    To improve the performance, the `clink.bat` script now detects autorun
    even before launching Clink.  Since the batch script has to take some
    shortcuts to be safe and avoid syntax errors, the Clink executable still
    also checks in order to assure reliability.

--- clink/app/src/loader/clink.bat
+++ clink/app/src/loader/clink.bat
@@ -2,26 +2,27 @@
 :: License: http://opensource.org/licenses/MIT
 
 @echo off
+setlocal enableextensions
 set clink_profile_arg=
 set clink_quiet_arg=
 
 :: Mimic cmd.exe's behaviour when starting from the start menu.
 if /i "%1"=="startmenu" (
     cd /d "%userprofile%"
-    shift /1
+    shift
 )
 
 :: Check for the --profile option.
 if /i "%1"=="--profile" (
     set clink_profile_arg=--profile "%~2"
-    shift /1
-    shift /1
+    shift
+    shift
 )
 
 :: Check for the --quiet option.
 if /i "%1"=="--quiet" (
     set clink_quiet_arg= --quiet
-    shift /1
+    shift
 )
 
 :: If the .bat is run without any arguments, then start a cmd.exe instance.
@@ -29,6 +30,9 @@ if "%1"=="" (
     call :launch
     goto :end
 )
+
+:: Test for autorun.
+if defined CLINK_NOAUTORUN if /i "%1"=="inject" if /i "%2"=="--autorun" goto :end
 
 :: Pass through to appropriate loader.
 if /i "%processor_architecture%"=="x86" (
@@ -42,8 +46,7 @@ if /i "%processor_architecture%"=="x86" (
 )
 
 :end
-set clink_profile_arg=
-set clink_quiet_arg=
+endlocal
 goto :eof
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
