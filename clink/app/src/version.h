78e7c70e834dd0776a3f906e02d747ca0c23bc5d
Chris Antos <sparrowhawk996@gmail.com>
2023-04-01 10:22:03 -0700

    Increment version.

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -14,7 +14,7 @@
 
 #define CLINK_VERSION_MAJOR     1
 #define CLINK_VERSION_MINOR     4
-#define CLINK_VERSION_PATCH     23
+#define CLINK_VERSION_PATCH     24
 
 #ifdef _MSC_VER
 #   undef CLINK_VERSION_STR
