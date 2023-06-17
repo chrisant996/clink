93270ca870c61311fc87089e472501ab27d35133
Chris Antos <sparrowhawk996@gmail.com>
2023-06-17 11:30:03 -0700

    Added tests for the hooks Clink makes

---
+++ clink/process/test/hook.cpp
@@ -0,0 +1,149 @@
+// Copyright (c) 2021 Martin Ridgers
+// License: http://opensource.org/licenses/MIT
+
+#include "pch.h"
+
+#include <process/process.h>
+#include <utils/hook_setter.h>
+
+typedef process::funcptr_t funcptr_t;
+
+//------------------------------------------------------------------------------
+enum
+{
+    visitor_set_env_var     = 1 << 0,
+    visitor_write_console   = 1 << 1,
+    visitor_get_env_var     = 1 << 2,
+    visitor_read_console    = 1 << 3,
+    visitor_set_title       = 1 << 4,
+};
+
+struct originals
+{
+    BOOL    (WINAPI *set_env_var)(const wchar_t*, const wchar_t*);
+    BOOL    (WINAPI *write_console)(HANDLE, const void*, DWORD, LPDWORD, LPVOID);
+    DWORD   (WINAPI *get_env_var)(const wchar_t*, wchar_t*, DWORD);
+    BOOL    (WINAPI *read_console)(HANDLE, void*, DWORD, LPDWORD, PCONSOLE_READCONSOLE_CONTROL);
+    BOOL    (WINAPI *set_title)(const wchar_t*);
+};
+
+static originals    g_originals;
+static unsigned int g_visited_bits;
+
+//------------------------------------------------------------------------------
+BOOL WINAPI set_env_var(const wchar_t* name, const wchar_t* value)
+{
+    g_visited_bits |= visitor_set_env_var;
+    return g_originals.set_env_var(name, value);
+}
+
+//------------------------------------------------------------------------------
+BOOL WINAPI write_console(
+    HANDLE output,
+    const void* chars,
+    DWORD to_write,
+    LPDWORD written,
+    LPVOID unused)
+{
+    g_visited_bits |= visitor_write_console;
+    return g_originals.write_console(output, chars, to_write, written, unused);
+}
+
+//------------------------------------------------------------------------------
+DWORD WINAPI get_env_var(const wchar_t* name, wchar_t* buffer, DWORD size)
+{
+    g_visited_bits |= visitor_get_env_var;
+    return g_originals.get_env_var(name, buffer, size);
+}
+
+//------------------------------------------------------------------------------
+BOOL WINAPI read_console(HANDLE, void*, DWORD, LPDWORD, PCONSOLE_READCONSOLE_CONTROL)
+{
+    g_visited_bits |= visitor_read_console;
+    return TRUE;
+}
+
+//------------------------------------------------------------------------------
+BOOL WINAPI set_title(const wchar_t*)
+{
+    g_visited_bits |= visitor_set_title;
+    return TRUE;
+}
+
+
+
+//------------------------------------------------------------------------------
+funcptr_t hook_iat(void* /*base*/, const char* /*dll*/, const char* name, funcptr_t func, int /*find_by_name*/)
+{
+    funcptr_t original;
+
+    hook_setter hooks;
+    if (!hooks.attach(hook_type::iat, nullptr, name, func, &original))
+        return nullptr;
+    if (!hooks.commit())
+        return nullptr;
+
+    return original;
+}
+
+
+
+//------------------------------------------------------------------------------
+TEST_CASE("Hooks")
+{
+    void* base = GetModuleHandle(nullptr);
+
+    funcptr_t original;
+
+    // Attach hooks.
+
+    g_originals.set_env_var = SetEnvironmentVariableW;
+    original = hook_iat(base, nullptr, "SetEnvironmentVariableW", funcptr_t(set_env_var), 1);
+    REQUIRE(original == funcptr_t(g_originals.set_env_var));
+
+    g_originals.write_console = WriteConsoleW;
+    original = hook_iat(base, nullptr, "WriteConsoleW", funcptr_t(write_console), 1);
+    REQUIRE(original == funcptr_t(g_originals.write_console));
+
+    g_originals.get_env_var = GetEnvironmentVariableW;
+    original = hook_iat(base, nullptr, "GetEnvironmentVariableW", funcptr_t(get_env_var), 1);
+    REQUIRE(original == funcptr_t(g_originals.get_env_var));
+
+    g_originals.read_console = ReadConsoleW;
+    original = hook_iat(base, nullptr, "ReadConsoleW", funcptr_t(read_console), 1);
+    REQUIRE(original == funcptr_t(g_originals.read_console));
+
+    g_originals.set_title = SetConsoleTitleW;
+    original = hook_iat(base, nullptr, "SetConsoleTitleW", funcptr_t(set_title), 1);
+    REQUIRE(original == funcptr_t(g_originals.set_title));
+
+    // Verify hooks.
+
+    g_visited_bits = 0;
+
+    SetEnvironmentVariableW(L"clink_test", nullptr);
+    REQUIRE(g_visited_bits & visitor_set_env_var);
+
+    WriteConsoleW(nullptr, nullptr, 0, nullptr, nullptr);
+    REQUIRE(g_visited_bits & visitor_write_console);
+
+    WCHAR buffer[64];
+    GetEnvironmentVariableW(L"clink_test", buffer, _countof(buffer));
+    REQUIRE(g_visited_bits & visitor_get_env_var);
+
+    ReadConsoleW(nullptr, nullptr, 0, nullptr, nullptr);
+    REQUIRE(g_visited_bits & visitor_read_console);
+
+    SetConsoleTitleW(nullptr);
+    REQUIRE(g_visited_bits & visitor_set_title);
+
+    // Detach hooks so they don't affect subsequent tests.
+
+    hook_setter hooks;
+    hooks.detach(hook_type::iat, nullptr, "SetEnvironmentVariableW", &g_originals.set_env_var, set_env_var);
+    hooks.detach(hook_type::iat, nullptr, "WriteConsoleW", &g_originals.write_console, write_console);
+    hooks.detach(hook_type::iat, nullptr, "GetEnvironmentVariableW", &g_originals.get_env_var, get_env_var);
+    hooks.detach(hook_type::iat, nullptr, "ReadConsoleW", &g_originals.read_console, read_console);
+    hooks.detach(hook_type::iat, nullptr, "SetConsoleTitleW", &g_originals.set_title, set_title);
+    REQUIRE(hooks.commit());
+}
