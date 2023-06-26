797c5ef4b612daacd76f0925b8b0aa6733152acf
Chris Antos <sparrowhawk996@gmail.com>
2023-06-26 09:16:06 -0700

    Add code signing in `premake5 release`.

--- installer/premake5.lua
+++ installer/premake5.lua
@@ -25,6 +25,24 @@ if include_arm64 then
 end
 
 --------------------------------------------------------------------------------
+-- Some timestamp services for code signing:
+--  http://timestamp.digicert.com
+--  http://time.certum.pl
+--  http://sha256timestamp.ws.symantec.com/sha256/timestamp
+--  http://timestamp.comodoca.com/authenticode
+--  http://timestamp.comodoca.com
+--  http://timestamp.sectigo.com
+--  http://timestamp.globalsign.com
+--  http://tsa.starfieldtech.com
+--  http://timestamp.entrust.net/TSS/RFC3161sha2TS
+--  http://tsa.swisssign.net
+local cert_name = "Open Source Developer, Christopher Antos"
+--local timestamp_service = "http://timestamp.digicert.com"
+local timestamp_service = "http://time.certum.pl"
+local sign_command = string.format(' sign /n "%s" /fd sha256 /td sha256 /tr %s ', cert_name, timestamp_service)
+local verify_command = string.format(' verify /pa ')
+
+--------------------------------------------------------------------------------
 local function warn(msg)
     print("\x1b[0;33;1mWARNING: " .. msg.."\x1b[m")
     any_warnings_or_failures = true
@@ -34,6 +52,12 @@ end
 local function failed(msg)
     print("\x1b[0;31;1mFAILED: " .. msg.."\x1b[m")
     any_warnings_or_failures = true
+end
+
+--------------------------------------------------------------------------------
+local function print_reverse(msg, prolog)
+    prolog = prolog or "\n"
+    print(prolog .. "\x1b[0;7m " .. msg .. " \x1b[m")
 end
 
 --------------------------------------------------------------------------------
@@ -273,16 +297,19 @@ newaction {
         local root_dir = path.getabsolute(".build/release").."/"
 
         -- Check we have the tools we need.
+        print_reverse("Finding tools")
         local have_msbuild = have_required_tool("msbuild", { "c:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\MSBuild\\Current\\Bin", "c:\\Program Files (x86)\\Microsoft Visual Studio\\2017\\Enterprise\\MSBuild\\Current\\Bin" })
         local have_mingw = have_required_tool("mingw32-make")
         local have_nsis = have_required_tool("makensis", "c:\\Program Files (x86)\\NSIS")
         local have_7z = have_required_tool("7z", { "c:\\Program Files\\7-Zip", "c:\\Program Files (x86)\\7-Zip" })
+        local have_signtool = have_required_tool("signtool", { "c:\\Program Files (x86)\\Windows Kits\\10\\bin\\10.0.22000.0\\x64" })
 
         -- Clone repo in release folder and checkout the specified version
         local code_dir = root_dir.."~working/"
         rmdir(code_dir)
         mkdir(code_dir)
 
+        print_reverse("Cloning repo")
         exec("git clone . " .. code_dir)
         if not os.chdir(code_dir) then
             error("Failed to chdir to '" .. code_dir .. "'")
@@ -304,7 +331,7 @@ newaction {
 
                 x86_ok = exec(have_msbuild .. " /m /v:q /p:configuration=final /p:platform=win32 clink.sln /t:" .. target)
                 x64_ok = exec(have_msbuild .. " /m /v:q /p:configuration=final /p:platform=x64 clink.sln /t:" .. target)
-                if include_arm64 then
+                if include_arm64 and target ~= "luac" then
                     arm64_ok = exec(have_msbuild .. " /m /v:q /p:configuration=final /p:platform=arm64 clink.sln /t:" .. target)
                 end
 
@@ -318,7 +345,7 @@ newaction {
 
                 x86_ok = exec(have_mingw .. " CC=gcc config=final_x32 -j%number_of_processors% " .. target)
                 x64_ok = exec(have_mingw .. " CC=gcc config=final_x64 -j%number_of_processors% " .. target)
-                if include_arm64 then
+                if include_arm64 and target ~= "luac" then
                     arm64_ok = nil
                 end
 
@@ -329,8 +356,11 @@ newaction {
         end
 
         -- Build everything.
+        print_reverse("Build Lua compiler")
         build_code("luac")
+        print_reverse("Precompile Lua scripts")
         exec(premake .. " " .. (_OPTIONS["dbginfo"] and "embed_debug" or "embed"))
+        print_reverse("Build Clink executables")
         build_code()
 
         local src = path.getabsolute(".build/" .. toolchain .. "/bin/final").."/"
@@ -341,13 +371,15 @@ newaction {
         end
 
         -- Run tests.
+        if x86_ok or x64_ok then
+            print_reverse("Run Clink tests")
+        end
         if x86_ok then
             test_exe = path.translate(src.."/clink_test_x86.exe")
             if not exec(test_exe) then
                 error("x86 tests failed")
             end
         end
-
         if x64_ok then
             test_exe = path.translate(src.."/clink_test_x64.exe")
             if not exec(test_exe) then
@@ -355,7 +387,48 @@ newaction {
             end
         end
 
+        -- Now we can sign the files.
+        local sign = not _OPTIONS["nosign"]
+        local signed_ok -- nil means unknown, false means failed, true means ok.
+        if sign then
+            local sign_files = function ()
+                local orig_dir = os.getcwd()
+                os.chdir(src)
+
+                local file_table = {
+                    "clink_x86.exe",
+                    "clink_dll_x86.dll",
+                    "clink_x64.exe",
+                    "clink_dll_x64.dll",
+                    "clink_arm64.exe",
+                    "clink_dll_arm64.dll",
+                }
+
+                local files = ""
+                for _, file in ipairs(file_table) do
+                    files = files .. " " .. file
+                end
+
+                -- Sectigo requests to wait 15 seconds between timestamps.
+                -- Digicert and Certum don't mention any delay, so for now
+                -- just let signtool do the signatures and timestamps without
+                -- imposing external delays.
+                signed_ok = (exec('"' .. have_signtool .. sign_command .. files .. '"') and signed ~= false)
+                -- Note: FAILS: cmd.exe /c "program" args "more args"
+                --    SUCCEEDS: cmd.exe /c ""program" args "more args""
+
+                -- Verify the signatures.
+                signed_ok = (exec(have_signtool .. verify_command .. files) and signed ~= false)
+
+                os.chdir(orig_dir)
+            end
+
+            print_reverse("Code signing")
+            signed = sign_files()
+        end
+
         -- Now we can extract the version from the executables.
+        print_reverse("Extract version")
         local version = nil
         local clink_exe = x86_ok and "clink_x86.exe" or "clink_x64.exe"
         local ver_cmd = src:gsub("/", "\\")..clink_exe.." --version"
@@ -368,6 +441,7 @@ newaction {
         local docversion = version:match("%d+%.%d+%.%d+")
 
         -- Now we know the version we can create our output directory.
+        print_reverse("Copy release files")
         local target_dir = root_dir..os.date("%Y%m%d_%H%M%S").."_"..version.."/"
         rmdir(target_dir)
         mkdir(target_dir)
@@ -388,6 +462,7 @@ newaction {
         end
 
         -- Generate documentation.
+        print_reverse("Generate documentation")
         exec(premake .. " docs --docver="..docversion)
         copy(".build/docs/clink.html", dest)
 
@@ -400,10 +475,12 @@ newaction {
             nsis_cmd = nsis_cmd .. " /DCLINK_TAGVERSION=" .. docversion
             nsis_cmd = nsis_cmd .. " /DCLINK_SOURCE=" .. code_dir
             nsis_cmd = nsis_cmd .. " " .. code_dir .. "/installer/clink.nsi"
+            print_reverse("Build setup program")
             nsis_ok = exec(nsis_cmd)
         end
 
         -- Tidy up code directory.
+        print_reverse("Tidy output directories")
         rmdir(".build")
         rmdir(".git")
         unlink(".gitignore")
@@ -417,6 +494,7 @@ newaction {
 
         if have_7z then
             os.chdir(src_dir_name)
+            print_reverse("Zip source code")
             exec(have_7z .. " a -r  " .. target_dir..clink_suffix .. "_src.zip .")
         end
         os.chdir(target_dir)
@@ -426,13 +504,15 @@ newaction {
         os.chdir(dest)
         if have_7z then
             if have_msbuild then
+                print_reverse("Zip symbols")
                 exec(have_7z .. " a -r  ../"..clink_suffix .. "_symbols.zip  *.pdb")
             end
+            print_reverse("Zip Clink files")
             exec(have_7z .. " a -x!*.pdb -r  ../"..clink_suffix .. ".zip  *")
         end
 
         -- Report some facts about what just happened.
-        print("\n\n")
+        print_reverse("Status")
         if not have_7z then     warn("7-ZIP NOT FOUND -- Packing to .zip files was skipped.") end
         if not have_nsis then   warn("NSIS NOT FOUND -- No installer was created.") end
         if not nsis_ok then     warn("INSTALLER PACKAGE FAILED") end
@@ -444,6 +524,9 @@ newaction {
             any_warnings_or_failures = x
         elseif not arm64_ok then
             failed("arm64 BUILD FAILED")
+        end
+        if sign and not signed_ok then
+            failed("signing FAILED")
         end
         if not any_warnings_or_failures then
             print("\x1b[0;32;1mRelease "..version.." built successfully.\x1b[m")
@@ -470,6 +553,12 @@ newoption {
    trigger     = "commit",
    value       = "SPEC",
    description = "Clink: Git commit/tag to build Clink release from"
+}
+
+--------------------------------------------------------------------------------
+newoption {
+    trigger     = "nosign",
+    description = "Clink: don't sign the release files"
 }
 
 --------------------------------------------------------------------------------
