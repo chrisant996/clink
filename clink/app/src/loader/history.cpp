chrisant996/clink

commit 9aa8eaa2c276918a8a0589087e54c14ad242b864

Overhauled history.

clink/app/src/loader/history.cpp
@@ -2,7 +2,7 @@
 // License: http://opensource.org/licenses/MIT
 
 #include "pch.h"
-#include "rl/rl_history.h"
+#include "history/history_db.h"
 #include "utils/app_context.h"
 
 #include <core/base.h>
@@ -21,40 +21,23 @@ void puts_help(const char**, int);
 class history_scope
 {
 public:
-                    history_scope(bool save);
-                    ~history_scope();
-    rl_history*     operator -> ();
+                    history_scope();
+    history_db*     operator -> ()      { return &m_history; }
+    history_db&     operator * ()       { return m_history; }
 
 private:
     str<280>        m_path;
-    rl_history      m_history;
-    bool            m_save;
+    history_db      m_history;
 };
 
 //------------------------------------------------------------------------------
-history_scope::history_scope(bool save)
-: m_save(save)
+history_scope::history_scope()
 {
     // Load settings.
     app_context::get()->get_settings_path(m_path);
     settings::load(m_path.c_str());
-    
-    // Find and load the history
-    app_context::get()->get_history_path(m_path);
-    m_history.load(m_path.c_str());
-}
 
-//------------------------------------------------------------------------------
-history_scope::~history_scope()
-{
-    if (m_save)
-        m_history.save(m_path.c_str());
-}
-
-//------------------------------------------------------------------------------
-rl_history* history_scope::operator -> ()
-{
-    return &m_history;
+    m_history.initialise();
 }
 
 
@@ -62,18 +45,26 @@ rl_history* history_scope::operator -> ()
 //------------------------------------------------------------------------------
 static void print_history(unsigned int tail_count)
 {
-    history_scope history(false);
+    history_scope history;
 
-    unsigned int skip = 0;
-    if (tail_count)
-        skip = max<int>(0, history->get_count() - tail_count);
+    str_iter line;
+    char buffer[history_db::max_line_length];
 
-    history_iter iter;
-    iter.skip(skip);
+    int count = 0;
+    {
+        history_db::iter iter = history->read_lines(buffer);
+        while (iter.next(line))
+            ++count;
+    }
 
-    str<> line;
-    while (iter.next(line))
-        printf("%5d  %s\n", iter.get_index() + 1, line.c_str());
+    int index = 1;
+    history_db::iter iter = history->read_lines(buffer);
+
+    int skip = count - tail_count;
+    for (int i = 0; i < skip; ++i, ++index, iter.next(line));
+
+    for (; iter.next(line); ++index)
+        printf("%5d  %.*s\n", index, line.length(), line.get_pointer());
 }
 
 //------------------------------------------------------------------------------
@@ -81,7 +72,7 @@ static bool print_history(const char* arg)
 {
     if (arg == nullptr)
     {
-        print_history(unsigned(0));
+        print_history(INT_MIN);
         return true;
     }
 
@@ -97,7 +88,7 @@ static bool print_history(const char* arg)
 //------------------------------------------------------------------------------
 static int add(const char* line)
 {
-    history_scope history(true);
+    history_scope history;
     history->add(line);
 
     printf("Added '%s' to history.\n", line);
@@ -107,15 +98,24 @@ static int add(const char* line)
 //------------------------------------------------------------------------------
 static int remove(int index)
 {
-    history_scope history(true);
+    history_scope history;
 
-    unsigned int length = history->get_count();
-    if (index < 0)
-        index = length + index;
+    if (index <= 0)
+        return 1;
 
-    bool ok = history->remove(index);
+    char buffer[history_db::max_line_length];
+    history_db::line_id line_id = 0;
+    {
+        str_iter line;
+        history_db::iter iter = history->read_lines(buffer);
+        for (int i = index - 1; i > 0 && iter.next(line); --i);
 
-    auto fmt =  ok ?  "Deleted %d.\n" : "Unable to delete history item %d.\n";
+        line_id = iter.next(line);
+    }
+
+    bool ok = history->remove(line_id);
+
+    auto fmt =  ok ?  "Deleted item %d.\n" : "Unable to delete history item %d.\n";
     printf(fmt, index);
     return (ok != true);
 }
@@ -123,7 +123,7 @@ static int remove(int index)
 //------------------------------------------------------------------------------
 static int clear()
 {
-    history_scope history(true);
+    history_scope history;
     history->clear();
 
     puts("History cleared.");
@@ -133,7 +133,8 @@ static int clear()
 //------------------------------------------------------------------------------
 static int print_expansion(const char* line)
 {
-    history_scope history(false);
+    history_scope history;
+    history->load_rl_history();
     str<> out;
     history->expand(line, out);
     puts(out.c_str());

clink/app/test/history.cpp
@@ -2,10 +2,19 @@
 // License: http://opensource.org/licenses/MIT
 
 #include "pch.h"
+#include "env_fixture.h"
+#include "fs_fixture.h"
 #include "line_editor_tester.h"
 
+#include <core/base.h>
+#include <core/globber.h>
+#include <core/os.h>
+#include <core/settings.h>
 #include <core/str.h>
-#include <rl/rl_history.h>
+#include <history/history_db.h>
+#include <utils/app_context.h>
+
+#include <initializer_list>
 
 //------------------------------------------------------------------------------
 extern "C" {
@@ -20,79 +29,294 @@ char* tgetstr(char*, char**);
 #define CTRL_R "\x12"
 
 //------------------------------------------------------------------------------
-TEST_CASE("History") {
-    const char* history_lines[] = {
+struct test_history_db
+    : public history_db
+{
+    test_history_db()
+    {
+        initialise();
+    }
+};
+
+//------------------------------------------------------------------------------
+int count_files()
+{
+    globber file_iter("*");
+    file_iter.hidden(true);
+
+    int file_count = 0;
+    for (; file_iter.next(str<>()); ++file_count);
+
+    return file_count;
+}
+
+//------------------------------------------------------------------------------
+template <typename T>
+void expect_files(const std::initializer_list<T>& names, bool exclusive=true)
+{
+    for (const char* name : names)
+    {
+        REQUIRE(os::get_path_type(name) != os::path_type_invalid, [&] () {
+            printf("Missing '%s'\n", name);
+        });
+    }
+
+	if (exclusive)
+		REQUIRE(count_files() == names.size());
+}
+
+
+
+//------------------------------------------------------------------------------
+TEST_CASE("history db")
+{
+    const char* line_set0[] = {
+        "line_set0_0", "line_set0_1", "line_set0_2", "line_set0_3",
+        "line_set0_4", "line_set0_5", "line_set0_6", "line_set0_7",
+    };
+
+    const char* line_set1[] = {
+        "line_set1_0", "line_set1_1", "line_set1_2", "line_set1_3",
+    };
+
+    const char* master_path = "clink_history";
+    const char* session_path = "clink_history_493";
+    const char* alive_path = "clink_history_493~";
+
+    // Start with an empty state dir.
+    const char* empty_fs[] = { nullptr };
+    fs_fixture fs(empty_fs);
+    REQUIRE(count_files() == 0);
+
+    // This sets the state id to something explicit.
+    static const char* env_desc[] = {
+        "=clink.id", "493",
+        nullptr
+    };
+    env_fixture env(env_desc);
+
+    app_context::desc context_desc;
+    context_desc.inherit_id = true;
+    str_base(context_desc.state_dir).copy(fs.get_root());
+    app_context context(context_desc);
+
+    SECTION("Alive file")
+    {
+        // Shared
+        settings::find("history.shared")->set("true");
+        {
+            test_history_db history;
+            expect_files({master_path, alive_path});
+        }
+        expect_files({master_path});
+
+        // Sessioned
+        settings::find("history.shared")->set("false");
+        {
+            test_history_db history;
+            expect_files({master_path, session_path, alive_path});
+        }
+        expect_files({master_path});
+    }
+
+    SECTION("Shared")
+    {
+        settings::find("history.shared")->set("true");
+        settings::find("history.dupe_mode")->set("add");
+
+        int line_bytes = 0;
+
+        // Write a lot of lines, check it only goes to main file.
+        {
+            test_history_db history;
+            REQUIRE(count_files() == 2);
+
+            while (line_bytes < 64 * 1024)
+            {
+                for (const char* line : line_set0)
+                {
+                    REQUIRE(history.add(line));
+                    line_bytes += int(strlen(line)) + 1; // +1 for \n
+                }
+            }
+
+            REQUIRE(os::get_file_size(master_path) == line_bytes);
+        }
+
+        // Clear the history.
+        {
+            test_history_db history;
+            history.clear();
+            REQUIRE(os::get_file_size(master_path) == 0);
+        }
+
+        REQUIRE(count_files() == 1);
+    }
+
+    SECTION("Sessioned")
+    {
+        settings::find("history.shared")->set("false");
+        settings::find("history.dupe_mode")->set("add");
+
+        int line_bytes = 0;
+        {
+            test_history_db history;
+            REQUIRE(count_files() == 3);
+
+            REQUIRE(history.add(line_set0[0]));
+            line_bytes += int(strlen(line_set0[0])) + 1;
+
+            REQUIRE(count_files() == 3);
+            REQUIRE(os::get_file_size(session_path) == line_bytes);
+            REQUIRE(os::get_file_size(master_path) == 0);
+        }
+
+        REQUIRE(count_files() == 1);
+        REQUIRE(os::get_file_size(master_path) == line_bytes);
+    }
+
+    SECTION("line iter")
+    {
+        str<> lines;
+        for (char i = 0; i < 8; ++i)
+        {
+            char c = 'a' + (i * 2);
+            char line[] = { c, c + 1, '\n', 0 };
+            lines << line;
+        }
+
+        FILE* out = fopen(session_path, "wb");
+        fwrite(lines.c_str(), lines.length(), 1, out);
+        fclose(out);
+
+        test_history_db history;
+
+        char buffer[256];
+        str_iter line;
+        int j = 0;
+        for (int i = 0; i < sizeof_array(buffer); ++i)
+        {
+            history_db::iter iter = history.read_lines(buffer, i);
+            char c = 'a';
+            while (iter.next(line))
+            {
+                int line_length = line.length();
+                REQUIRE(line_length == 1 || line_length == 2);
+
+                REQUIRE(line.get_pointer()[0] == c++);
+                if (line_length == 2)
+                    REQUIRE(line.get_pointer()[1] == c++);
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------
+TEST_CASE("history rl")
+{
+    // Start with an empty state dir.
+    const char* empty_fs[] = { nullptr };
+    fs_fixture fs(empty_fs);
+
+    // This sets the state id to something explicit.
+    static const char* env_desc[] = {
+        "=clink.id", "493",
+        nullptr
+    };
+    env_fixture env(env_desc);
+
+    app_context::desc context_desc;
+    context_desc.inherit_id = true;
+    str_base(context_desc.state_dir).copy(fs.get_root());
+    app_context context(context_desc);
+
+    // Fill the history. reload() call will fill Readline.
+    static const char* history_lines[] = {
         "cmd1 arg1 arg2 arg3 arg4",
         "cmd2 arg1 arg2 arg3 arg4 extra",
         "cmd3 arg1 arg2 arg3 arg4",
     };
 
-    rl_history history;
+    test_history_db history;
     for (const char* line : history_lines)
         history.add(line);
+    history.load_rl_history();
 
-    SECTION("Navigation") {
+    // Here be the tests.
+    SECTION("Navigation")
+    {
         line_editor_tester tester;
 
-        SECTION("Ctrl-P 1") {
+        SECTION("Ctrl-P 1")
+        {
             tester.set_input(CTRL_P);
             tester.set_expected_output(history_lines[2]);
             tester.run();
         }
 
-        SECTION("Ctrl-P 2") {
+        SECTION("Ctrl-P 2")
+        {
             tester.set_input(CTRL_P CTRL_P);
             tester.set_expected_output(history_lines[1]);
             tester.run();
         }
 
-        SECTION("Ctrl-P 3") {
+        SECTION("Ctrl-P 3")
+        {
             tester.set_input(CTRL_P CTRL_P CTRL_P);
             tester.set_expected_output(history_lines[0]);
             tester.run();
         }
 
-        SECTION("Ctrl-P 4") {
+        SECTION("Ctrl-P 4")
+        {
             tester.set_input(CTRL_P CTRL_P CTRL_P CTRL_P);
             tester.set_expected_output(history_lines[0]);
             tester.run();
         }
 
-        SECTION("Ctrl-N 1") {
+        SECTION("Ctrl-N 1")
+        {
             tester.set_input("abc" CTRL_P CTRL_N);
             tester.set_expected_output("abc");
             tester.run();
         }
 
-        SECTION("Ctrl-N 2") {
+        SECTION("Ctrl-N 2")
+        {
             tester.set_input(CTRL_P CTRL_P CTRL_P CTRL_P CTRL_N);
             tester.set_expected_output(history_lines[1]);
             tester.run();
         }
     }
 
-    SECTION("Search") {
+    SECTION("Search")
+    {
         line_editor_tester tester;
 
-        SECTION("Ctrl-R Ctrl-E") {
+        SECTION("Ctrl-R Ctrl-E")
+        {
             tester.set_input(CTRL_R "extra" CTRL_E);
             tester.set_expected_output(history_lines[1]);
             tester.run();
         }
 
-        SECTION("Ctrl-R Ctrl-A") {
+        SECTION("Ctrl-R Ctrl-A")
+        {
             tester.set_input(CTRL_R "cmd1" CTRL_A);
             tester.set_expected_output(history_lines[0]);
             tester.run();
         }
 
-        SECTION("Ctrl-R <Esc>") {
+        SECTION("Ctrl-R <Esc>")
+        {
             tester.set_input(CTRL_R "cmd2" "\x1b");
             tester.set_expected_output(history_lines[1]);
             tester.run();
         }
 
-        SECTION("Ctrl-R <Home>") {
+        SECTION("Ctrl-R <Home>")
+        {
             char kh_cap[] = "kh";
             char* kh = tgetstr(kh_cap, nullptr);
 
@@ -107,82 +331,99 @@ TEST_CASE("History") {
         }
     }
 
-    SECTION("Expansion") {
+    SECTION("Expansion")
+    {
         str<> out;
 
-        SECTION("!0") {
-            REQUIRE(history.expand("!0", out) == -1);
+        SECTION("!0")
+        {
+            REQUIRE(history.expand("!0", out) == history_db::expand_error);
             REQUIRE(out.empty());
         }
 
-        SECTION("!!") {
-            REQUIRE(history.expand("!!", out) == 1);
+        SECTION("!!")
+        {
+            REQUIRE(history.expand("!!", out) == history_db::expand_ok);
             REQUIRE(out.equals(history_lines[2]));
         }
 
-        SECTION("!string") {
-            REQUIRE(history.expand("!cmd2", out) == 1);
+        SECTION("!string")
+        {
+            REQUIRE(history.expand("!cmd2", out) == history_db::expand_ok);
             REQUIRE(out.equals(history_lines[1]));
         }
 
-        SECTION("!1") {
-            REQUIRE(history.expand("!1", out) == 1);
+        SECTION("!1")
+        {
+            REQUIRE(history.expand("!1", out) == history_db::expand_ok);
             REQUIRE(out.equals(history_lines[0]));
         }
 
-        SECTION("!#") {
-            REQUIRE(history.expand("one two !#", out) == 1);
+        SECTION("!#")
+        {
+            REQUIRE(history.expand("one two !#", out) == history_db::expand_ok);
             REQUIRE(out.equals("one two one two "));
         }
 
-        SECTION("!?string") {
+        SECTION("!?string")
+        {
             history.add("one two");
-            REQUIRE(history.expand("three !?one", out) == 1);
+            history.load_rl_history();
+            REQUIRE(history.expand("three !?one", out) == history_db::expand_ok);
             REQUIRE(out.equals("three one two"));
         }
 
-        SECTION("!$") {
-            REQUIRE(history.expand("cmdX !$", out) == 1);
+        SECTION("!$")
+        {
+            REQUIRE(history.expand("cmdX !$", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg4"));
         }
 
-        SECTION("!!:$") {
-            REQUIRE(history.expand("cmdX !!:$", out) == 1);
+        SECTION("!!:$")
+        {
+            REQUIRE(history.expand("cmdX !!:$", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg4"));
         }
 
-        SECTION("!!:N-$") {
-            REQUIRE(history.expand("cmdX !!:3-$", out) == 1);
+        SECTION("!!:N-$")
+        {
+            REQUIRE(history.expand("cmdX !!:3-$", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg3 arg4"));
         }
 
-        SECTION("!!:N*") {
-            REQUIRE(history.expand("cmdX !!:2*", out) == 1);
+        SECTION("!!:N*")
+        {
+            REQUIRE(history.expand("cmdX !!:2*", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg2 arg3 arg4"));
         }
 
-        SECTION("!!:N") {
-            REQUIRE(history.expand("cmdX !!:2", out) == 1);
+        SECTION("!!:N")
+        {
+            REQUIRE(history.expand("cmdX !!:2", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg2"));
         }
 
-        SECTION("!!:-N") {
-            REQUIRE(history.expand("cmdX !!:-1", out) == 1);
+        SECTION("!!:-N")
+        {
+            REQUIRE(history.expand("cmdX !!:-1", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX cmd3 arg1"));
         }
 
-        SECTION("^X^Y^") {
-            REQUIRE(history.expand("^arg1^123^", out) == 1);
+        SECTION("^X^Y^")
+        {
+            REQUIRE(history.expand("^arg1^123^", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmd3 123 arg2 arg3 arg4"));
         }
 
-        SECTION("!X:s/Y/Z") {
-            REQUIRE(history.expand("!cmd1:s/arg1/123", out) == 1);
+        SECTION("!X:s/Y/Z")
+        {
+            REQUIRE(history.expand("!cmd1:s/arg1/123", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmd1 123 arg2 arg3 arg4"));
         }
 
-        SECTION("!?X?:") {
-            REQUIRE(history.expand("cmdX !?extra?:*", out) == 1);
+        SECTION("!?X?:")
+        {
+            REQUIRE(history.expand("cmdX !?extra?:*", out) == history_db::expand_ok);
             REQUIRE(out.equals("cmdX arg1 arg2 arg3 arg4 extra"));
         }
     }

clink/app/src/history/history_db.h
@@ -1,0 +1,71 @@
+// Copyright (c) 2017 Martin Ridgers
+// License: http://opensource.org/licenses/MIT
+
+#pragma once
+
+#include <core/str_iter.h>
+
+//------------------------------------------------------------------------------
+class history_db
+{
+public:
+    enum expand_result
+    {
+        // values match Readline's history_expand() return value.
+        expand_error            = -1,
+        expand_none             = 0,
+        expand_ok               = 1,
+        expand_print            = 2,
+    };
+
+    static const unsigned int   max_line_length = 8192;
+    typedef unsigned int        line_id;
+
+    class iter
+    {
+    public:
+                                ~iter();
+        line_id                 next(str_iter& out);
+
+    private:
+                                iter() = default;
+        friend                  history_db;
+        uintptr_t               impl = 0;
+    };
+
+                                history_db();
+                                ~history_db();
+    void                        initialise();
+    void                        load_rl_history();
+    void                        clear();
+    bool                        add(const char* line);
+    bool                        remove(line_id id);
+    line_id                     find(const char* line) const;
+    expand_result               expand(const char* line, str_base& out) const;
+    template <int S> iter       read_lines(char (&buffer)[S]);
+    iter                        read_lines(char* buffer, unsigned int buffer_size);
+
+private:
+    enum : char
+    {
+        bank_none               = -1,
+        bank_master,
+        bank_session,
+        bank_count,
+    };
+
+    friend                      class read_line_iter;
+    void                        reap();
+    template <typename T> void  for_each_bank(T&& callback);
+    template <typename T> void  for_each_bank(T&& callback) const;
+    unsigned int                get_bank_count() const;
+    void*                       get_bank(unsigned int index) const;
+    void*                       m_alive_file;
+    void*                       m_bank_handles[bank_count];
+};
+
+//------------------------------------------------------------------------------
+template <int S> history_db::iter history_db::read_lines(char (&buffer)[S])
+{
+    return read_lines(buffer, S);
+}

clink/app/src/host/host.cpp
@@ -6,7 +6,6 @@
 #include "host_lua.h"
 #include "host_module.h"
 #include "prompt.h"
-#include "rl/rl_history.h"
 #include "utils/app_context.h"
 #include "utils/scroller.h"
 
@@ -54,6 +53,11 @@ host::host(const char* name)
 }
 
 //------------------------------------------------------------------------------
+host::~host()
+{
+}
+
+//------------------------------------------------------------------------------
 bool host::edit_line(const char* prompt, str_base& out)
 {
     const app_context* app = app_context::get();
@@ -80,13 +84,6 @@ bool host::edit_line(const char* prompt, str_base& out)
     default:    cmp_mode = str_compare_scope::exact;    break;
     }
     str_compare_scope compare(cmp_mode);
-
-    // Initialise and load history.
-    str<288> history_file;
-    app->get_history_path(history_file);
-
-    rl_history history;
-    history.load(history_file.c_str());
 
     // Set up Lua and load scripts into it.
     host_lua lua;
@@ -129,13 +126,16 @@ bool host::edit_line(const char* prompt, str_base& out)
     editor->add_generator(lua);
     editor->add_generator(file_match_generator());
 
+    m_history.initialise();
+    m_history.load_rl_history();
+
     bool ret = false;
     while (1)
     {
         if (ret = editor->edit(out.data(), out.size()))
         {
             // Handle history event expansion.
-            if (history.expand(out.c_str(), out) == 2)
+            if (m_history.expand(out.c_str(), out) == history_db::expand_print)
             {
                 puts(out.c_str());
                 continue;
@@ -153,13 +153,10 @@ bool host::edit_line(const char* prompt, str_base& out)
             }
 
             // Add the line to the history.
-            history.load(history_file.c_str());
-            history.add(out.c_str());
+            m_history.add(out.c_str());
         }
         break;
     }
-
-    history.save(history_file.c_str());
 
     line_editor_destroy(editor);
     tab_completer_destroy(completer);

clink/app/src/host/host.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include "history/history_db.h"
+
 #include <lib/line_editor.h>
 
 class lua_state;
@@ -13,6 +15,7 @@ class host
 {
 public:
                     host(const char* name);
+    virtual         ~host();
     virtual bool    validate() = 0;
     virtual bool    initialise() = 0;
     virtual void    shutdown() = 0;
@@ -25,4 +28,5 @@ protected:
 private:
     void            filter_prompt(const char* in, str_base& out);
     const char*     m_name;
+    history_db      m_history;
 };

clink/app/src/host/host_cmd.cpp
@@ -263,7 +263,7 @@ void host_cmd::edit_line(const wchar_t* prompt, wchar_t* chars, int max_chars)
     // Call readline.
     while (1)
     {
-        str<1024> out;
+        str<4096> out;
         bool ok = host::edit_line(utf8_prompt.c_str(), out);
         if (ok)
         {

commit 570a1585a3dc2bb4fecb810082d9c7f4545d6a83

Fix `history delete` with a negative number.
The docs said it indexed backwards from the end, but commit 9aa8eaa2c2
accidentally removed that behavior.

clink/app/src/loader/history.cpp
@@ -256,7 +256,7 @@ static int32 remove(int32 index)
 {
     history_scope history;
 
-    if (index <= 0)
+    if (index == 0)
         return 1;
 
     history_read_buffer buffer;
@@ -264,9 +264,26 @@ static int32 remove(int32 index)
     {
         str_iter line;
         history_db::iter iter = history->read_lines(buffer.data(), buffer.size());
-        for (int32 i = index - 1; i > 0 && iter.next(line); --i);
 
-        line_id = iter.next(line);
+        if (index > 0)
+        {
+            for (int32 i = index - 1; i > 0 && iter.next(line); --i);
+
+            line_id = iter.next(line);
+        }
+        else
+        {
+            std::vector<history_db::line_id> lines;
+            while (history_db::line_id l = iter.next(line))
+                lines.emplace_back(l);
+
+            const size_t li = lines.size() + index;
+            if (li >= lines.size())
+                return 1;
+
+            line_id = lines[li];
+            index = int32(li) + 1;
+        }
     }
 
     bool ok = history->remove(line_id);