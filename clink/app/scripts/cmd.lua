cc031f4f0e043644be47c95863a19b5d755671d2
Chris Antos <sparrowhawk996@gmail.com>
2023-11-15 13:21:48 -0800

    Add flag descriptions in CMD argmatcher.
    This adds localized descriptions for CMD flags by parsing them from the
    output from "cmd.exe /?".

--- clink/app/scripts/cmd.lua
+++ clink/app/scripts/cmd.lua
@@ -58,9 +58,117 @@ function cmd_classifier:classify(commands) -- luacheck: no self
 end
 
 local chain = clink.argmatcher():chaincommand()
+local colors = clink.argmatcher():addarg({fromhistory=true})
+local onoff = clink.argmatcher():addarg("on", "off")
 
-local onoff = clink.argmatcher():addarg("ON", "OFF")
-clink.argmatcher("cmd")
-:addflags("/c"..chain, "/k"..chain, "/s", "/q", "/d", "/a", "/u")
-:addflags("/t:"..clink.argmatcher():addarg({fromhistory=true}), "/e:"..onoff, "/f:"..onoff, "/v:"..onoff)
-:nofiles()
+local function delayinit(argmatcher)
+    argmatcher:setdelayinit(nil)
+
+    local descriptions = {
+        ["/c"] = {},
+        ["/k"] = {},
+        ["/s"] = {},
+        ["/q"] = {},
+        ["/d"] = {},
+        ["/a"] = {},
+        ["/u"] = {},
+        ["/t:"] = {},
+        ["/e:on"] = {},
+        ["/e:off"] = {},
+        ["/f:on"] = {},
+        ["/f:off"] = {},
+        ["/v:on"] = {},
+        ["/v:off"] = {},
+    }
+
+    local function hide_cap_onoff()
+        clink.onfiltermatches(function()
+            return { "on", "off" }
+        end)
+        return {}
+    end
+
+    local onoff = {
+        ["e"] = clink.argmatcher():addarg("on", "off", "ON", "OFF", hide_cap_onoff),
+        ["f"] = clink.argmatcher():addarg("on", "off", "ON", "OFF", hide_cap_onoff),
+        ["v"] = clink.argmatcher():addarg("on", "off", "ON", "OFF", hide_cap_onoff),
+    }
+
+    local f = io.popen("2>nul cmd.exe /?")
+    if f then
+        local pending = {}
+        local function finish_pending()
+            if pending.flag and pending.desc then
+                local t = descriptions[pending.flag]
+                if pending.arginfo then
+                    table.insert(t, pending.arginfo)
+                end
+                local d = pending.desc:gsub("^%s+", ""):gsub("^([^.]*)%.%s.*$", "%1"):gsub("%(.*$", ""):gsub("[.%s]+$", "")
+                table.insert(t, d)
+                local o = onoff[pending.flag:match("[a-z]")]
+                if o then
+                    local arg = pending.flag:match("^/.:(.+)$")
+                    o:adddescriptions({[arg]={d}})
+                end
+            end
+            pending.flag = nil
+            pending.arginfo = nil
+            pending.desc = nil
+        end
+        for line in f:lines() do
+            local flag, text = line:match("^(/[A-Za-z][^%s]*)%s+([^%s].*)$")
+            if not flag and pending.flag then
+                text = line:match("^%s+([^%s].*)$")
+            end
+            if flag then
+                flag = flag:lower()
+                local stripped, arginfo = flag:match("^(/t:)([^%s]*)")
+                flag = stripped or flag
+                if not descriptions[flag][1] then
+                    finish_pending()
+                    pending.flag = flag
+                    pending.arginfo = arginfo
+                    pending.desc = text
+                end
+            elseif text and pending.flag then
+                pending.desc = pending.desc.." "..text
+            elseif pending.flag then
+                finish_pending()
+            end
+        end
+        f:close()
+    end
+
+    local desc_t = descriptions["/t:"]
+    local num_t = #desc_t
+    if num_t < 2 then
+        if num_t < 1 then
+            table.insert(desc_t, "fg")
+        end
+        table.insert(desc_t, "")
+    end
+
+    argmatcher
+    :addflags({
+        "/c"..chain, "/C"..chain,
+        "/k"..chain, "/K"..chain,
+        "/s", "/q", "/d", "/a", "/u",
+        "/S", "/Q", "/D", "/A", "/U",
+        "/t:"..colors, "/T:"..colors,
+        "/e:on", "/e:off", "/e:"..onoff["e"], "/E:"..onoff["e"],
+        "/f:on", "/f:off", "/f:"..onoff["f"], "/F:"..onoff["f"],
+        "/v:on", "/v:off", "/v:"..onoff["v"], "/V:"..onoff["v"],
+    })
+    :nofiles()
+    :adddescriptions(descriptions)
+    :hideflags({
+        "/C", "/K",
+        "/S", "/Q", "/D", "/A", "/U",
+        "/T:",
+        "/e:", "/E:",
+        "/f:", "/F:",
+        "/v:", "/V:",
+    })
+end
+
+clink.argmatcher("cmd"):setdelayinit(delayinit):nofiles()
