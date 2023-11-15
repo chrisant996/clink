0ed61a240b9f1b94caf3f63710808c326de2a460
Chris Antos <sparrowhawk996@gmail.com>
2023-11-15 13:22:11 -0800

    Add localized flag descriptions for CD and RD argmatchers.

--- clink/app/scripts/dir.lua
+++ clink/app/scripts/dir.lua
@@ -46,8 +46,101 @@ local function onarg_pushd(arg_index, word, word_index, line_state) -- luacheck:
 end
 
 --------------------------------------------------------------------------------
+local function cd_delayinit(argmatcher)
+    argmatcher:setdelayinit(nil)
+
+    local descriptions = {
+        ["/d"] = { "Also change drive" },
+    }
+
+    local f = io.popen("2>nul cd /?")
+    if f then
+        local seen = {}
+        local pending = {}
+        local function finish_pending()
+            if pending.flag and pending.desc then
+                local d = pending.desc:gsub("^%s+", ""):gsub("^([^.]*)%.%s.*$", "%1"):gsub("%(.*$", ""):gsub("[.%s]+$", "")
+                descriptions[pending.flag] = { d }
+            end
+            pending.flag = nil
+            pending.desc = nil
+        end
+        for line in f:lines() do
+            local flag, text
+            if line:find("/[Dd] ") and not line:find("[%[%]]") then
+                flag = "/D"
+            end
+            text = line:match("^%s*([^%s].*)$")
+            if flag then
+                flag = flag:lower()
+                if not seen[flag] then
+                    finish_pending()
+                    pending.flag = flag
+                    pending.desc = text
+                    seen[flag] = true
+                end
+            elseif text and pending.flag then
+                pending.desc = pending.desc.." "..text
+            elseif pending.flag then
+                finish_pending()
+            end
+        end
+        f:close()
+    end
+
+    argmatcher:adddescriptions(descriptions)
+end
+
+--------------------------------------------------------------------------------
+local function rd_delayinit(argmatcher)
+    argmatcher:setdelayinit(nil)
+
+    local descriptions = {
+        ["/s"] = { "Remove files and directories recursively" },
+        ["/q"] = { "Quiet mode, do not ask if ok to remove a directory tree with /S" },
+    }
+
+    local f = io.popen("2>nul rd /?")
+    if f then
+        local seen = {}
+        local pending = {}
+        local function finish_pending()
+            if pending.flag and pending.desc then
+                local d = pending.desc:gsub("^%s+", ""):gsub("^([^.]*)%.%s.*$", "%1"):gsub("%(.*$", ""):gsub("[.%s]+$", "")
+                descriptions[pending.flag] = { d }
+            end
+            pending.flag = nil
+            pending.desc = nil
+        end
+        for line in f:lines() do
+            local flag, text = line:match("^%s*(/[A-Za-z])%s+([^%s].*)$")
+            if not flag and pending.flag then
+                text = line:match("^%s+([^%s].*)$")
+            end
+            if flag then
+                flag = flag:lower()
+                if not seen[flag] then
+                    finish_pending()
+                    pending.flag = flag
+                    pending.desc = text
+                    seen[flag] = true
+                end
+            elseif text and pending.flag then
+                pending.desc = pending.desc.." "..text
+            elseif pending.flag then
+                finish_pending()
+            end
+        end
+        f:close()
+    end
+
+    argmatcher:adddescriptions(descriptions)
+end
+
+--------------------------------------------------------------------------------
 clink.argmatcher("cd", "chdir")
 :setcmdcommand()
+:setdelayinit(cd_delayinit)
 :addflags("/d")
 :adddescriptions({
     ["/d"] = "Also change drive"
@@ -70,10 +163,7 @@ clink.argmatcher("md", "mkdir")
 --------------------------------------------------------------------------------
 clink.argmatcher("rd", "rmdir")
 :setcmdcommand()
+:setdelayinit(rd_delayinit)
 :addflags("/s", "/q")
-:adddescriptions({
-    ["/s"] = "Remove files and directories recursively",
-    ["/q"] = "Quiet mode, do not ask if ok to remove a directory tree with /S",
-})
 :addarg(clink.dirmatches)
 :loop()
