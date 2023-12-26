c1e5c4f4884cbd2127c687739f416f5b22e4e068
Chris Antos <sparrowhawk996@gmail.com>
2023-12-26 01:14:09 -0800

    Update test script to allow running x86 exe.

--- test.cmd
+++ test.cmd
@@ -2,6 +2,7 @@
 setlocal
 set __DBG=
 set __ME=%~dp0
+set __CPU=x64
 
 :arg
 if x%1x == x?x goto :usage
@@ -12,11 +13,19 @@ if x%1x == x-hx goto :usage
 if x%1x == x/helpx goto :usage
 if x%1x == x--helpx goto :usage
 if x%1x == xhelpx goto :usage
-if x%1x == x/dbgx set __DBG=call devenv /debugexe
-if x%1x == x/dbgx shift & goto :arg
+if x%1x == x/x64x set __CPU=x64& goto :nextarg
+if x%1x == x-x64x set __CPU=x64& goto :nextarg
+if x%1x == x/x86x set __CPU=x86& goto :nextarg
+if x%1x == x-x86x set __CPU=x86& goto :nextarg
+if x%1x == x/dbgx set __DBG=call devenv /debugexe& goto:nextarg
+if x%1x == x-dbgx set __DBG=call devenv /debugexe& goto:nextarg
 
-%__DBG% %__ME%.build\vs2019\bin\debug\clink_test_x64.exe %1 %2 %3
+%__DBG% %__ME%.build\vs2019\bin\debug\clink_test_%__CPU%.exe %1 %2 %3
 goto :eof
+
+:nextarg
+shift
+goto :arg
 
 :usage
 echo Usage:  test [/? /dbg] [test name prefix]
@@ -27,6 +36,8 @@ echo.  /?        Show usage info.
 echo   /dbg      Run test under the debugger.
 echo   -d        Load Lua debugger.
 echo   -t        Show execution time.
+echo   -x64      Run clink_test_x64.exe (the default).
+echo   -x86      Run clink_test_x86.exe.
 echo.
 echo If [test name prefix] is included, then it only runs tests whose name begins
 echo with the specified prefix.
