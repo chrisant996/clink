chrisant996/clink

commit 82df15fbf54a8d4e2b6d94f77f3b309cad5b9db5

Fix Lua arg #n error messages.
Argument errors when calling lua_bindable objects reported the arg # one
lower than the correct number.

clink/lua/src/match_builder_lua.cpp
@@ -132,14 +132,14 @@ match_builder_lua::~match_builder_lua()
 int32 match_builder_lua::add_match(lua_State* state)
 {
     int32 ret = 0;
-    if (lua_gettop(state) > 0)
+    if (lua_gettop(state) > LUA_SELF)
     {
-        const char* type_str = optstring(state, 2, "");
+        const char* type_str = optstring(state, LUA_SELF + 2, "");
         if (!type_str)
             return 0;
 
         match_type type = to_match_type(type_str);
-        ret = !!add_match_impl(state, 1, type);
+        ret = !!add_match_impl(state, LUA_SELF + 1, type);
     }
 
     lua_pushboolean(state, ret);
@@ -168,7 +168,7 @@ int32 match_builder_lua::is_empty(lua_State* state)
 /// (rather than <code>set USERDOMAIN&nbsp;</code>).
 int32 match_builder_lua::set_append_character(lua_State* state)
 {
-    const char* append = optstring(state, 1, "");
+    const char* append = optstring(state, LUA_SELF + 1, "");
     if (!append)
         return 0;
 
@@ -186,8 +186,8 @@ int32 match_builder_lua::set_append_character(lua_State* state)
 int32 match_builder_lua::set_suppress_append(lua_State* state)
 {
     bool suppress = true;
-    if (lua_gettop(state) > 0)
-        suppress = (lua_toboolean(state, 1) != 0);
+    if (lua_gettop(state) > LUA_SELF)
+        suppress = (lua_toboolean(state, LUA_SELF + 1) != 0);
 
     m_builder->set_suppress_append(suppress);
 
@@ -205,9 +205,9 @@ int32 match_builder_lua::set_suppress_append(lua_State* state)
 int32 match_builder_lua::set_suppress_quoting(lua_State* state)
 {
     int32 suppress = 1;
-    if (lua_gettop(state) > 0)
+    if (lua_gettop(state) > LUA_SELF)
     {
-        int32 i = int32(lua_tointeger(state, 1));
+        int32 i = int32(lua_tointeger(state, LUA_SELF + 1));
         if (i >= 0 && i <= 2)
             suppress = i;
     }
@@ -235,8 +235,8 @@ int32 match_builder_lua::set_force_quoting(lua_State* state)
 int32 match_builder_lua::set_fully_qualify(lua_State* state)
 {
     bool fully_qualify = true;
-    if (lua_gettop(state) > 0)
-        fully_qualify = (lua_toboolean(state, 1) != 0);
+    if (lua_gettop(state) > LUA_SELF)
+        fully_qualify = (lua_toboolean(state, LUA_SELF + 1) != 0);
 
     m_builder->set_fully_qualify(fully_qualify);
 
@@ -292,12 +292,12 @@ int32 match_builder_lua::deprecated_add_match(lua_State* state)
 // backward compatibility.
 int32 match_builder_lua::set_matches_are_files(lua_State* state)
 {
-    if (lua_gettop(state) <= 0 || lua_isnil(state, 1))
+    if (lua_gettop(state) <= LUA_SELF || lua_isnil(state, LUA_SELF + 1))
         m_builder->set_matches_are_files(true);
-    else if (lua_isboolean(state, 1))
-        m_builder->set_matches_are_files(lua_toboolean(state, 1) != 0);
+    else if (lua_isboolean(state, LUA_SELF + 1))
+        m_builder->set_matches_are_files(lua_toboolean(state, LUA_SELF + 1) != 0);
     else
-        m_builder->set_matches_are_files(int32(lua_tointeger(state, 1)));
+        m_builder->set_matches_are_files(int32(lua_tointeger(state, LUA_SELF + 1)));
     return 0;
 }
 
@@ -314,7 +314,7 @@ int32 match_builder_lua::clear_toolkit(lua_State* state)
 // UNDOCUMENTED; internal use only.
 int32 match_builder_lua::set_input_line(lua_State* state)
 {
-    const char* text = checkstring(state, 1);
+    const char* text = checkstring(state, LUA_SELF + 1);
     if (!text)
         return 0;
 
@@ -329,7 +329,7 @@ int32 match_builder_lua::matches_ready(lua_State* state)
     if (!m_toolkit)
         return 0;
 
-    const auto id = checkinteger(state, 1);
+    const auto id = checkinteger(state, LUA_SELF + 1);
     if (!id.isnum())
         return 0;
 
@@ -361,24 +361,24 @@ int32 match_builder_lua::matches_ready(lua_State* state)
 /// -show:  })
 int32 match_builder_lua::add_matches(lua_State* state)
 {
-    if (lua_gettop(state) <= 0 || !lua_istable(state, 1))
+    if (lua_gettop(state) <= LUA_SELF || !lua_istable(state, LUA_SELF + 1))
     {
         lua_pushinteger(state, 0);
         lua_pushboolean(state, 0);
         return 2;
     }
 
-    const char* type_str = optstring(state, 2, "");
+    const char* type_str = optstring(state, LUA_SELF + 2, "");
     if (!type_str)
         return 0;
 
     match_type type = to_match_type(type_str);
 
     int32 count = 0;
-    int32 total = int32(lua_rawlen(state, 1));
+    int32 total = int32(lua_rawlen(state, LUA_SELF + 1));
     for (int32 i = 1; i <= total; ++i)
     {
-        lua_rawgeti(state, 1, i);
+        lua_rawgeti(state, LUA_SELF + 1, i);
         count += !!add_match_impl(state, -1, type);
         lua_pop(state, 1);
     }

clink/lua/src/rl_buffer_lua.cpp
@@ -134,7 +134,7 @@ int32 rl_buffer_lua::get_anchor(lua_State* state)
 int32 rl_buffer_lua::set_cursor(lua_State* state)
 {
     uint32 old = m_rl_buffer.get_cursor() + 1;
-    auto set = checkinteger(state, 1);
+    auto set = checkinteger(state, LUA_SELF + 1);
     if (!set.isnum())
         return 0;
     set.minus_one();
@@ -153,7 +153,7 @@ int32 rl_buffer_lua::set_cursor(lua_State* state)
 /// line.
 int32 rl_buffer_lua::insert(lua_State* state)
 {
-    const char* text = checkstring(state, 1);
+    const char* text = checkstring(state, LUA_SELF + 1);
     if (!text)
         return 0;
 
@@ -177,8 +177,8 @@ int32 rl_buffer_lua::insert(lua_State* state)
 /// Unicode character may have undesirable results.
 int32 rl_buffer_lua::remove(lua_State* state)
 {
-    auto from = checkinteger(state, 1);
-    auto to = checkinteger(state, 2);
+    auto from = checkinteger(state, LUA_SELF + 1);
+    auto to = checkinteger(state, LUA_SELF + 2);
     if (!from.isnum() || !to.isnum())
         return 0;
     from.minus_one();
@@ -271,7 +271,7 @@ int32 rl_buffer_lua::get_argument(lua_State* state)
 /// from having 0 as the numeric argument).
 int32 rl_buffer_lua::set_argument(lua_State* state)
 {
-    const auto arg = optinteger(state, 1, 0);
+    const auto arg = optinteger(state, LUA_SELF + 1, 0);
 
     _rl_reset_argument();
     if (arg.isnum())

clink/lua/include/lua/lua_state.h
@@ -20,6 +20,8 @@ class line_state;
 class terminal_in;
 typedef double lua_Number;
 
+#define LUA_SELF    (1)
+
 //------------------------------------------------------------------------------
 template<class T>
 class checked_num

clink/lua/src/line_state_lua.cpp
@@ -223,11 +223,11 @@ int32 line_state_lua::get_word_count(lua_State* state)
 /// -show:  -- t.redir      [boolean | nil] true if the word is a redirection arg, otherwise nil.
 int32 line_state_lua::get_word_info(lua_State* state)
 {
-    if (!lua_isnumber(state, 1))
+    if (!lua_isnumber(state, LUA_SELF + 1))
         return 0;
 
     const std::vector<word>& words = m_line->get_words();
-    uint32 index = int32(lua_tointeger(state, 1)) - 1 + m_shift;
+    uint32 index = int32(lua_tointeger(state, LUA_SELF + 1)) - 1 + m_shift;
     if (index >= words.size())
         return 0;
 
@@ -290,11 +290,11 @@ int32 line_state_lua::get_word_info(lua_State* state)
 /// could be garbled.
 int32 line_state_lua::get_word(lua_State* state)
 {
-    if (!lua_isnumber(state, 1))
+    if (!lua_isnumber(state, LUA_SELF + 1))
         return 0;
 
     str<32> word;
-    uint32 index = int32(lua_tointeger(state, 1)) - 1;
+    uint32 index = int32(lua_tointeger(state, LUA_SELF + 1)) - 1;
     m_line->get_word(m_shift + index, word);
     lua_pushlstring(state, word.c_str(), word.length());
     return 1;
@@ -332,7 +332,7 @@ int32 line_state_lua::get_end_word(lua_State* state)
 // UNDOCUMENTED; internal use only.
 int32 line_state_lua::shift(lua_State* state)
 {
-    uint32 num = optinteger(state, 1, 0);
+    uint32 num = optinteger(state, LUA_SELF + 1, 0);
 
     if (num > 0)
     {
@@ -381,14 +381,14 @@ inline bool is_unbreakchar(const char* unbreakchars, const char* line, uint32 le
 // UNDOCUMENTED; internal use only.
 int32 line_state_lua::unbreak(lua_State* state)
 {
-    const auto _index = checkinteger(state, 1);
-    const char* unbreakchars = checkstring(state, 2);
+    const auto _index = checkinteger(state, LUA_SELF + 1);
+    const char* unbreakchars = checkstring(state, LUA_SELF + 2);
     if (!_index.isnum() || !unbreakchars)
         return 0;
     if (!*unbreakchars) // No-op.
         return 0;
     if (!validate_unbreakchars(unbreakchars))
-        return luaL_argerror(state, 2, "must contain only ASCII characters");
+        return luaL_argerror(state, LUA_SELF + 2, "must contain only ASCII characters");
     const uint32 index = _index - 1 + m_shift;
 
     const std::vector<word>& words = m_line->get_words();
@@ -426,7 +426,7 @@ int32 line_state_lua::unbreak(lua_State* state)
 // UNDOCUMENTED; internal use only.
 int32 line_state_lua::overwrite_from(lua_State* state)
 {
-    line_state_lua* from = check(state, 1);
+    line_state_lua* from = check(state, LUA_SELF + 1);
     if (!from)
         return 0;
 

clink/lua/src/lua_bindable.h
@@ -272,10 +272,25 @@ int32 lua_bindable<T>::call(lua_State* state)
         return 0;
     }
 
+#ifdef DEBUG
+    const int32 top = lua_gettop(state);
+#endif
+
+    // Don't remove self before calling the method!  Removing it is convenient
+    // for the author of native C/C++ code in Clink, but it throws off all the
+    // arg# error reporting.  Use LUA_SELF + 1 to refer to arg #1, etc.
     lua_remove(state, 1);
+    const int32 ret = ((*self)->*(*ptr))(state);
+
+    // Now remove self to clean up the stack.
     lua_remove(state, 1);
 
-    return ((*self)->*(*ptr))(state);
+#ifdef DEBUG
+    const int32 new_top = lua_gettop(state);
+    assert(new_top == top - 2 + ret);
+#endif
+
+    return ret;
 }
 
 //------------------------------------------------------------------------------

clink/lua/src/lua_makeable.h
@@ -1,205 +1,0 @@
-// Copyright (c) 2021 Christopher Antos
-// License: http://opensource.org/licenses/MIT
-
-#pragma once
-
-#include <core/str.h>
-
-extern "C" {
-#include <lua.h>
-#include <lauxlib.h>
-}
-
-#include <assert.h>
-
-//------------------------------------------------------------------------------
-// The lua_makeable<T> template binds a C++ class into a Lua object, and its
-// lifetime is managed by Lua garbage collection.
-//
-// The make_new() method uses lua_newuserdata() to allocate a block of Lua
-// memory for T.  The method leaves a ref pushed on the Lua stack.
-//
-// A subclass must define two members:
-//  - static const char* const c_name.
-//  - static const method c_methods[], which must end with a {} element.
-//
-// Sample usage:
-//
-//      class foo_lua : public lua_makeable<foo_lua>
-//      {
-//      public:
-//          foo_lua(int32 x);
-//          ~foo_lua();
-//      private:
-//          friend class lua_makeable<foo_lua>;
-//          static const char* const c_name;
-//          static const method c_methods[];
-//          int32 getx(lua_State* state);
-//          int32 setx(lua_State* state);
-//          int32 m_x;
-//      }
-//
-//      const char* const foo_lua::c_name = "foo_lua";
-//
-//      const method foo_lua::c_methods[] = {
-//          { "getx", &getx },
-//          { "setx", &setx },
-//          {}
-//      }
-//
-//      foo_lua::foo_lua(int32 x) : m_x(x) {}
-//
-//      foo_lua::~foo_lua() {}
-//
-//      int32 foo_lua::getx(lua_State* state)
-//      {
-//          lua_pushinteger(state, m_x);
-//          return 1;
-//      }
-//
-//      int32 foo_lua::setx(lua_State* state)
-//      {
-//          m_x = lua_tointeger(state, 1);
-//          return 0;
-//      }
-template <class T>
-class lua_makeable _DBGOBJECT
-{
-public:
-    typedef int32       (T::*method_t)(lua_State*);
-
-    struct method
-    {
-        const char*     name;
-        method_t        ptr;
-    };
-
-    template<typename... Args> static T* make_new(lua_State* state, Args... args);
-
-private:
-                        lua_makeable();
-                        ~lua_makeable();
-    static int32        call(lua_State* state);
-    static int32        __gc(lua_State* state);
-    static int32        __tostring(lua_State* state);
-    void                make_metatable(lua_State* state);
-};
-
-//------------------------------------------------------------------------------
-template <class T>
-lua_makeable<T>::lua_makeable()
-{
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-lua_makeable<T>::~lua_makeable()
-{
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-void lua_makeable<T>::make_metatable(lua_State* state)
-{
-    if (luaL_newmetatable(state, T::c_name))
-    {
-        // Add __gc and __tostring directly to metatable.
-
-        lua_pushcfunction(state, &T::__gc);
-        lua_setfield(state, -2, "__gc");
-
-        lua_pushcfunction(state, &T::__tostring);
-        lua_setfield(state, -2, "__tostring");
-
-        // Add other methods to __index table.
-
-        lua_createtable(state, 0, 0);
-
-        const method* methods = T::c_methods;
-        while (methods != nullptr && methods->name != nullptr)
-        {
-            auto* ptr = (method_t*)lua_newuserdata(state, sizeof(method_t));
-            *ptr = methods->ptr;
-
-            if (luaL_newmetatable(state, "lua_makeable"))
-            {
-                lua_pushliteral(state, "__call");
-                lua_pushcfunction(state, &lua_makeable<T>::call);
-                lua_rawset(state, -3);
-            }
-
-            lua_setmetatable(state, -2);
-            lua_setfield(state, -2, methods->name);
-
-            ++methods;
-        }
-
-        lua_setfield(state, -2, "__index");
-    }
-
-    lua_setmetatable(state, -2);
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-template <typename... Args>
-T* lua_makeable<T>::make_new(lua_State* state, Args... args)
-{
-#ifdef DEBUG
-    int32 oldtop = lua_gettop(state);
-#endif
-
-    T* self = (T*)lua_newuserdata(state, sizeof(T));
-    new (self) T(args...);
-
-#ifdef DEBUG
-    assert(oldtop + 1 == lua_gettop(state));
-#endif
-
-    self->make_metatable(state);
-
-#ifdef DEBUG
-    int32 newtop = lua_gettop(state);
-    assert(oldtop + 1 == newtop);
-    T* test = (T*)luaL_checkudata(state, -1, T::c_name);
-    assert(test == self);
-#endif
-
-    return self;
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-int32 lua_makeable<T>::call(lua_State* state)
-{
-    T* self = (T*)lua_touserdata(state, 2);
-    if (self == nullptr)
-        return 0;
-
-    auto* const ptr = (method_t const*)lua_touserdata(state, 1);
-    if (ptr == nullptr)
-        return 0;
-
-    lua_remove(state, 1);
-    lua_remove(state, 1);
-
-    return (self->*(*ptr))(state);
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-int32 lua_makeable<T>::__gc(lua_State* state)
-{
-    T* self = (T*)luaL_checkudata(state, 1, T::c_name);
-    self->~T();
-    return 0;
-}
-
-//------------------------------------------------------------------------------
-template <class T>
-int32 lua_makeable<T>::__tostring(lua_State* state)
-{
-    T* self = (T*)luaL_checkudata(state, 1, T::c_name);
-    lua_pushfstring(state, "%s (%p)", T::c_name, self);
-    return 1;
-}

clink/lua/src/lua_word_classifications.cpp
@@ -74,14 +74,14 @@ lua_word_classifications::lua_word_classifications(word_classifications& classif
 /// information.
 int32 lua_word_classifications::classify_word(lua_State* state)
 {
-    const auto _index = checkinteger(state, 1);
-    const char* s = checkstring(state, 2);
-    bool overwrite = !lua_isboolean(state, 3) || lua_toboolean(state, 3);
+    const auto _index = checkinteger(state, LUA_SELF + 1);
+    const char* s = checkstring(state, LUA_SELF + 2);
+    bool overwrite = !lua_isboolean(state, LUA_SELF + 3) || lua_toboolean(state, LUA_SELF + 3);
     if (!_index.isnum() || !s)
         return 0;
     const uint32 index = _index - 1 + m_shift;
     if (index >= m_num_words)
-        return luaL_argerror(state, 1, "word index out of bounds");
+        return luaL_argerror(state, LUA_SELF + 1, "word index out of bounds");
 
     const bool has_argmatcher = (*s == 'm');
     if (has_argmatcher)
@@ -135,10 +135,10 @@ int32 lua_word_classifications::classify_word(lua_State* state)
 /// when the onbeginedit event is sent.
 int32 lua_word_classifications::apply_color(lua_State* state)
 {
-    auto start = checkinteger(state, 1);
-    const auto length = checkinteger(state, 2);
-    const char* color = checkstring(state, 3);
-    bool overwrite = !lua_isboolean(state, 4) || lua_toboolean(state, 4);
+    auto start = checkinteger(state, LUA_SELF + 1);
+    const auto length = checkinteger(state, LUA_SELF + 2);
+    const char* color = checkstring(state, LUA_SELF + 3);
+    bool overwrite = !lua_isboolean(state, LUA_SELF + 4) || lua_toboolean(state, LUA_SELF + 4);
     if (!start.isnum() || !length.isnum() || !color)
         return 0;
     start.minus_one();
@@ -155,8 +155,8 @@ int32 lua_word_classifications::apply_color(lua_State* state)
 // UNDOCUMENTED; internal use only.
 int32 lua_word_classifications::shift(lua_State* state)
 {
-    uint32 num = optinteger(state, 1, 0);
-    uint32 cmd = optinteger(state, 2, 1);
+    uint32 num = optinteger(state, LUA_SELF + 1, 0);
+    uint32 cmd = optinteger(state, LUA_SELF + 2, 1);
 
     if (num > 0)
     {
@@ -191,14 +191,14 @@ int32 lua_word_classifications::reset_shift(lua_State* state)
 // UNDOCUMENTED; internal use only.
 int32 lua_word_classifications::unbreak(lua_State* state)
 {
-    const auto _index = checkinteger(state, 1);
-    const auto length = checkinteger(state, 2);
-    bool skip_word = !lua_isboolean(state, 3) || lua_toboolean(state, 3);
+    const auto _index = checkinteger(state, LUA_SELF + 1);
+    const auto length = checkinteger(state, LUA_SELF + 2);
+    bool skip_word = !lua_isboolean(state, LUA_SELF + 3) || lua_toboolean(state, LUA_SELF + 3);
     if (!_index.isnum())
         return 0;
     const uint32 index = _index - 1 + m_shift;
     if (index >= m_num_words)
-        return luaL_argerror(state, 1, "word index out of bounds");
+        return luaL_argerror(state, LUA_SELF + 1, "word index out of bounds");
 
     m_classifications.unbreak(index, length, skip_word);
     return 0;

clink/lua/src/matches_lua.cpp
@@ -73,7 +73,7 @@ int32 matches_lua::get_count(lua_State* state)
 /// Returns the match text for the <span class="arg">index</span> match.
 int32 matches_lua::get_match(lua_State* state)
 {
-    const auto _index = checkinteger(state, 1);
+    const auto _index = checkinteger(state, LUA_SELF + 1);
     if (!_index.isnum())
         return 0;
     const uint32 index = _index - 1;
@@ -93,7 +93,7 @@ int32 matches_lua::get_match(lua_State* state)
 /// Returns the match type for the <span class="arg">index</span> match.
 int32 matches_lua::get_type(lua_State* state)
 {
-    const auto _index = checkinteger(state, 1);
+    const auto _index = checkinteger(state, LUA_SELF + 1);
     if (!_index.isnum())
         return 0;
     const uint32 index = _index - 1;

commit 51259e47c9caa1a11d9a3bce6ad2e4503d6b6d04

Fix `rl.setmatches()`.
Regression caused by 82df15fbf54a8d4e2b6d94f77f3b309cad5b9db5
("Fix Lua arg #n error messages"), but had not been released yet.

clink/lua/include/lua/lua_word_classifications.h
@@ -18,13 +18,15 @@ class lua_word_classifications
 {
 public:
                             lua_word_classifications(word_classifications& classifications, uint32 index_offset, uint32 command_word_index, uint32 num_words);
+
+    bool                    get_word_class(int32 word_index_zero_based, word_class& wc) const;
+
+protected:
     int32                   classify_word(lua_State* state);
     int32                   apply_color(lua_State* state);
     int32                   shift(lua_State* state);
     int32                   reset_shift(lua_State* state);
     int32                   unbreak(lua_State* state);
-
-    bool                    get_word_class(int32 word_index_zero_based, word_class& wc) const;
 
 private:
     word_classifications&   m_classifications;

clink/lua/src/async_lua_task.h
@@ -25,13 +25,14 @@ public:
                             async_yield_lua(const char* name, uint32 timeout=0);
                             ~async_yield_lua();
 
-    int32                   get_name(lua_State* state);
-    int32                   get_expiration(lua_State* state);
-    int32                   ready(lua_State* state);
-
     bool                    is_expired() const;
     void                    set_ready() { m_ready = true; }
     void                    clear_ready() { m_ready = false; }
+
+protected:
+    int32                   get_name(lua_State* state);
+    int32                   get_expiration(lua_State* state);
+    int32                   ready(lua_State* state);
 
 private:
     str_moveable            m_name;

clink/lua/src/line_state_lua.h
@@ -20,6 +20,10 @@ public:
                         line_state_lua(const line_state& line);
                         line_state_lua(line_state_copy* copy);
                         ~line_state_lua();
+
+    const line_state*   get_line_state() const { return m_line; }
+
+protected:
     int32               get_line(lua_State* state);
     int32               get_cursor(lua_State* state);
     int32               get_command_offset(lua_State* state);
@@ -34,8 +38,6 @@ public:
     int32               reset_shift(lua_State* state);
     int32               unbreak(lua_State* state);
     int32               overwrite_from(lua_State* state);
-
-    const line_state*   get_line_state() const { return m_line; }
 
 private:
     const line_state*   m_line;

clink/lua/src/lua_state.cpp
@@ -725,7 +725,7 @@ bool lua_state::call_lua_rl_global_function(const char* func_name, const line_st
     str<> msg;
     if (!push_named_function(L, func_name, &msg))
     {
-        buffer.begin_output(L);
+        buffer.do_begin_output();
         puts(msg.c_str());
         return false;
     }
@@ -747,7 +747,7 @@ bool lua_state::call_lua_rl_global_function(const char* func_name, const line_st
     {
         if (const char* error = lua_tostring(L, -1))
         {
-            buffer.begin_output(L);
+            buffer.do_begin_output();
             printf("error executing function '%s':\n", func_name);
             puts(error);
         }

clink/lua/src/match_builder_lua.cpp
@@ -53,6 +53,38 @@ match_builder_lua::~match_builder_lua()
 }
 
 //------------------------------------------------------------------------------
+int32 match_builder_lua::do_add_matches(lua_State* state, bool self_on_stack)
+{
+    const int32 lua_self = self_on_stack ? LUA_SELF : 0;
+
+    if (lua_gettop(state) <= lua_self || !lua_istable(state, lua_self + 1))
+    {
+        lua_pushinteger(state, 0);
+        lua_pushboolean(state, 0);
+        return 2;
+    }
+
+    const char* type_str = optstring(state, lua_self + 2, "");
+    if (!type_str)
+        return 0;
+
+    match_type type = to_match_type(type_str);
+
+    int32 count = 0;
+    int32 total = int32(lua_rawlen(state, lua_self + 1));
+    for (int32 i = 1; i <= total; ++i)
+    {
+        lua_rawgeti(state, lua_self + 1, i);
+        count += !!add_match_impl(state, -1, type);
+        lua_pop(state, 1);
+    }
+
+    lua_pushinteger(state, count);
+    lua_pushboolean(state, count == total);
+    return 2;
+}
+
+//------------------------------------------------------------------------------
 /// -name:  builder:addmatch
 /// -ver:   1.0.0
 /// -arg:   match:string|table
@@ -361,31 +393,7 @@ int32 match_builder_lua::matches_ready(lua_State* state)
 /// -show:  })
 int32 match_builder_lua::add_matches(lua_State* state)
 {
-    if (lua_gettop(state) <= LUA_SELF || !lua_istable(state, LUA_SELF + 1))
-    {
-        lua_pushinteger(state, 0);
-        lua_pushboolean(state, 0);
-        return 2;
-    }
-
-    const char* type_str = optstring(state, LUA_SELF + 2, "");
-    if (!type_str)
-        return 0;
-
-    match_type type = to_match_type(type_str);
-
-    int32 count = 0;
-    int32 total = int32(lua_rawlen(state, LUA_SELF + 1));
-    for (int32 i = 1; i <= total; ++i)
-    {
-        lua_rawgeti(state, LUA_SELF + 1, i);
-        count += !!add_match_impl(state, -1, type);
-        lua_pop(state, 1);
-    }
-
-    lua_pushinteger(state, count);
-    lua_pushboolean(state, count == total);
-    return 2;
+    return do_add_matches(state, true/*self_on_stack*/);
 }
 
 //------------------------------------------------------------------------------

clink/lua/src/match_builder_lua.h
@@ -19,6 +19,10 @@ public:
                     match_builder_lua(match_builder& builder);
                     match_builder_lua(std::shared_ptr<match_builder_toolkit>& toolkit);
                     ~match_builder_lua();
+
+    int32           do_add_matches(lua_State* state, bool self_on_stack);
+
+protected:
     int32           add_match(lua_State* state);
     int32           add_matches(lua_State* state);
     int32           is_empty(lua_State* state);

clink/lua/src/matches_lua.h
@@ -17,6 +17,8 @@ public:
                         matches_lua(const matches& matches);
                         matches_lua(std::shared_ptr<match_builder_toolkit>& toolkit);
                         ~matches_lua();
+
+protected:
     int32               get_prefix(lua_State* state);
     int32               get_count(lua_State* state);
     int32               get_match(lua_State* state);

clink/lua/src/os_api.cpp
@@ -111,6 +111,8 @@ class globber_lua
 {
 public:
                         globber_lua(const char* pattern, int32 extrainfo, const glob_flags& flags, bool dirs_only, bool back_compat=false);
+
+protected:
     int32               next(lua_State* state);
     int32               close(lua_State* state);
 
@@ -1257,9 +1259,13 @@ class enumshares_lua
 public:
                         enumshares_lua(const std::shared_ptr<enumshares_async_lua_task>& task) : m_task(task) {}
                         ~enumshares_lua() {}
-    static int32        iter_aux(lua_State* state);
 
     bool                next(str_base& out) { return m_task->next(out); }
+
+    void                pushcclosure(lua_State* state) { lua_pushcclosure(state, iter_aux, 2); }
+
+protected:
+    static int32        iter_aux(lua_State* state);
 
 private:
     std::shared_ptr<enumshares_async_lua_task> m_task;
@@ -1432,7 +1438,7 @@ static int32 enum_shares(lua_State* state)
 
     // es was already pushed by make_new.
     // asyncyield was already pushed by make_new.
-    lua_pushcclosure(state, es->iter_aux, 2);
+    es->pushcclosure(state);
     return 1;
 }
 

clink/lua/src/rl_api.cpp
@@ -707,7 +707,7 @@ static int32 set_matches(lua_State* state)
     match_builder builder(*matches);
     match_builder_lua builder_lua(builder);
 
-    return builder_lua.add_matches(state);
+    return builder_lua.do_add_matches(state, false/*self_on_stack*/);
 }
 
 //------------------------------------------------------------------------------

clink/lua/src/rl_buffer_lua.cpp
@@ -59,6 +59,16 @@ rl_buffer_lua::~rl_buffer_lua()
 }
 
 //------------------------------------------------------------------------------
+void rl_buffer_lua::do_begin_output()
+{
+    if (!m_began_output)
+    {
+        end_prompt(true/*crlf*/);
+        m_began_output = true;
+    }
+}
+
+//------------------------------------------------------------------------------
 /// -name:  rl_buffer:getbuffer
 /// -ver:   1.0.0
 /// -ret:   string
@@ -226,11 +236,7 @@ int32 rl_buffer_lua::end_undo_group(lua_State* state)
 /// so that subsequent output doesn't overwrite the input buffer display.
 int32 rl_buffer_lua::begin_output(lua_State* state)
 {
-    if (!m_began_output)
-    {
-        end_prompt(true/*crlf*/);
-        m_began_output = true;
-    }
+    do_begin_output();
     return 0;
 }
 

clink/lua/src/rl_buffer_lua.h
@@ -18,6 +18,9 @@ public:
                             rl_buffer_lua(line_buffer& buffer);
                             ~rl_buffer_lua();
 
+    void                    do_begin_output();
+
+protected:
     int32                   get_buffer(lua_State* state);
     int32                   get_length(lua_State* state);
     int32                   get_cursor(lua_State* state);