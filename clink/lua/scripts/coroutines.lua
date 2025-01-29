chrisant996/clink

commit 6dea3d630b91750f8cdc533211cd1540c993f952

Fix coroutine resume/cancel.

clink/lua/scripts/coroutines.lua
@@ -44,6 +44,7 @@ local print = clink.print
 --      lastclock:      The os.clock() from the end of the last resume.
 --      infinite:       Use INFINITE wait for this coroutine; it's actively inside popenyield.
 --      queued:         Use INFINITE wait for this coroutine; it's queued inside popenyield.
+--      yieldguard:     Yielding due to io.popen, os.execute, etc.
 
 --------------------------------------------------------------------------------
 local function clear_coroutines()
@@ -104,10 +105,9 @@ local function release_coroutine_yieldguard()
 end
 
 --------------------------------------------------------------------------------
-local function get_coroutine_generation()
-    local t = coroutine.running()
-    if t and _coroutines[t] then
-        return _coroutines[t].generation
+local function get_coroutine_generation(c)
+    if c and _coroutines[c] then
+        return _coroutines[c].generation
     end
 end
 
@@ -151,7 +151,7 @@ end
 
 --------------------------------------------------------------------------------
 local function check_generation(c)
-    if get_coroutine_generation() == _coroutine_generation then
+    if get_coroutine_generation(c) == _coroutine_generation then
         return true
     end
     local entry = _coroutines[c]
@@ -234,9 +234,12 @@ function clink._resume_coroutines()
     -- Protected call to resume coroutines.
     local remove = {}
     local impl = function()
-        for _,entry in pairs(_coroutines) do
-            if coroutine.status(entry.coroutine) == "dead" or not check_generation(entry.coroutine) then
-                table.insert(remove, _)
+        for c,entry in pairs(_coroutines) do
+            if coroutine.status(c) == "dead" then
+                table.insert(remove, c)
+            elseif not check_generation(c) and not entry.yieldguard then
+                entry.canceled = true
+                table.insert(remove, c)
             else
                 _coroutines_resumable = true
                 local now = os.clock()
@@ -246,23 +249,26 @@ function clink._resume_coroutines()
                     end
                     entry.resumed = entry.resumed + 1
                     clink._set_coroutine_context(entry.context)
-                    local ok, ret = coroutine.resume(entry.coroutine, true--[[async]])
+                    local ok, ret
+                    if entry.isprompt or entry.isgenerator then
+                        ok, ret = coroutine.resume(c, true--[[async]])
+                    else
+                        ok, ret = coroutine.resume(c)
+                    end
                     if ok then
                         -- Use live clock so the interval excludes the execution
                         -- time of the coroutine.
                         entry.lastclock = os.clock()
                     else
-                        if _coroutine_canceled then
-                            entry.canceled = true
-                        else
+                        if not entry.canceled then
                             print("")
                             print("coroutine failed:")
                             print(ret)
                             entry.error = ret
                         end
                     end
-                    if coroutine.status(entry.coroutine) == "dead" then
-                        table.insert(remove, _)
+                    if coroutine.status(c) == "dead" then
+                        table.insert(remove, c)
                     end
                 end
             end

commit de4caf6c30518224a39fd07a5b20a894ab9b9d84

Fix #718; error about orphaned coroutine.
When autosuggest.strategy lists `complete` before `history` (or if
`history` isn't listed), then it's possible for an edge case timing
condition to occur which can result in a "coroutine is orphaned" error.

The fix is to discard a coroutine that doesn't pass the generation
check, regardless whether it has a yieldguard.  But that means also
`release_coroutine_yieldguard` must release a yieldguard that doesn't
pass the generation check, regardless whether the yieldguard is ready.

Commit 6dea3d630b91750f8cdc533211cd1540c993f952 introduced this issue
while fixing a different issue, because it didn't use quite the right
approach.

clink/lua/scripts/coroutines.lua
@@ -80,10 +80,13 @@ end
 clink.onbeginedit(clear_coroutines)
 
 --------------------------------------------------------------------------------
-local function release_coroutine_yieldguard()
+local check_generation
+
+--------------------------------------------------------------------------------
+local function release_coroutine_yieldguard(c)
     local nil_cats = {}
     for category, cyg in pairs(_coroutine_yieldguard) do
-        if cyg.yieldguard:ready() then
+        if cyg.yieldguard:ready() or not check_generation(cyg.coroutine) then
             local entry = _coroutines[cyg.coroutine]
             if entry and entry.yieldguard == cyg.yieldguard then
                 entry.throttleclock = os.clock()
@@ -153,7 +156,7 @@ local function cancel_coroutine(message)
 end
 
 --------------------------------------------------------------------------------
-local function check_generation(c)
+check_generation = function(c)
     if get_coroutine_generation(c) == _coroutine_generation then
         return true
     end
@@ -270,7 +273,7 @@ function clink._resume_coroutines()
             co = c
             if coroutine.status(c) == "dead" then
                 table.insert(remove, c)
-            elseif not check_generation(c) and not entry.yieldguard then
+            elseif not check_generation(c) then
                 entry.canceled = true
                 table.insert(remove, c)
             else