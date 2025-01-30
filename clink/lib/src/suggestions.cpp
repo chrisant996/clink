chrisant996/clink

commit 524d08977e4a9a4e0002b19ceb686f1a9b002e3f

Add suggestion functions to `rl_buffer`: - `rl_buffer:hassuggestion()` - `rl_buffer:insertsuggestion()`

clink/lib/src/suggestions.cpp
@@ -5,14 +5,13 @@
 #include "line_buffer.h"
 #include "line_state.h"
 #include "display_readline.h"
-
 #include "matches_impl.h"
-#include "rl_commands.h"
-#include "rl_suggestions.h"
+#include "suggestions.h"
 
 #include <core/base.h>
 #include <core/str_compare.h>
 #include <core/settings.h>
+#include <rl/rl_commands.h>
 #include <terminal/ecma48_iter.h>
 
 extern "C" {
@@ -91,6 +90,8 @@ bool suggestion_manager::get_visible(str_base& out, bool* includes_hint) const
     out.clear();
     if (!g_rl_buffer)
         return false;
+    if (m_suggestion_offset >= g_rl_buffer->get_length())
+        return false;
 
     // Do not allow relaxed comparison for suggestions, as it is too confusing,
     // as a result of the logic to respect original case.
@@ -126,6 +127,13 @@ bool suggestion_manager::get_visible(str_base& out, bool* includes_hint) const
 #endif  // USE_SUGGESTION_HINT_INLINE
 
     return true;
+}
+
+//------------------------------------------------------------------------------
+bool suggestion_manager::has_suggestion() const
+{
+    str<> tmp;
+    return get_visible(tmp);
 }
 
 //------------------------------------------------------------------------------

clink/lib/include/lib/line_editor_integration.h
@@ -59,5 +59,3 @@ bool has_deprecated_argmatcher(const char* name);
 //------------------------------------------------------------------------------
 bool host_can_suggest(const line_state& line);
 bool host_suggest(const line_states& lines, matches* matches, int32 generation_id);
-void set_suggestion_started(const char* line);
-void set_suggestion(const char* line, uint32 endword_offset, const char* suggestion, uint32 offset);

clink/lib/include/lib/suggestions.h
@@ -17,6 +17,7 @@ class suggestion_manager
 public:
     bool            more() const;
     bool            get_visible(str_base& out, bool* includes_hint=nullptr) const;
+    bool            has_suggestion() const;
     void            clear();
     bool            can_suggest(const line_state& line);
     bool            can_update_matches();
@@ -40,8 +41,11 @@ private:
 };
 
 //------------------------------------------------------------------------------
-bool pause_suggestions(bool pause);
-bool insert_suggestion(suggestion_action action);
-void suppress_suggestions();
+bool has_suggestion();
 extern "C" void clear_suggestion();
 bool can_show_suggestion_hint();
+void suppress_suggestions();
+void set_suggestion_started(const char* line);
+void set_suggestion(const char* line, uint32 endword_offset, const char* suggestion, uint32 offset);
+bool insert_suggestion(suggestion_action action);
+bool pause_suggestions(bool pause);

clink/lib/src/display_readline.cpp
@@ -23,7 +23,7 @@
 #include "ellipsify.h"
 #ifdef USE_SUGGESTION_HINT_COMMENTROW
 #include "rl/rl_commands.h"
-#include "rl/rl_suggestions.h"
+#include "suggestions.h"
 #endif
 
 #include <core/base.h>

clink/lib/src/line_editor_impl.cpp
@@ -16,6 +16,7 @@
 #include "clink_ctrlevent.h"
 #include "clink_rl_signal.h"
 #include "line_editor_integration.h"
+#include "suggestions.h"
 #include "recognizer.h"
 
 #include <core/base.h>
@@ -28,7 +29,6 @@
 #include <terminal/terminal_out.h>
 #include <terminal/input_idle.h>
 #include <rl/rl_commands.h>
-#include <rl/rl_suggestions.h>
 extern "C" {
 #include <compat/config.h>
 #include <readline/readline.h>

clink/lib/src/line_editor_integration.cpp
@@ -7,7 +7,7 @@
 #include "host_callbacks.h"
 #include "match_pipeline.h"
 #include "reclassify.h"
-#include "rl/rl_suggestions.h"
+#include "suggestions.h"
 
 #include <core/str.h>
 #include <core/str_transform.h>

clink/lib/src/rl/rl_commands.cpp
@@ -19,8 +19,7 @@
 #include "clink_rl_signal.h"
 #include "rl_integration.h"
 #include "line_editor_integration.h"
-
-#include "rl_suggestions.h"
+#include "suggestions.h"
 
 #include <core/base.h>
 #include <core/log.h>

clink/lib/src/rl/rl_module.cpp
@@ -21,8 +21,7 @@
 #include "sticky_search.h"
 #include "line_editor_integration.h"
 #include "rl_integration.h"
-
-#include "rl_suggestions.h"
+#include "suggestions.h"
 
 #include <core/base.h>
 #include <core/os.h>
@@ -1143,6 +1142,12 @@ bool can_suggest_internal(const line_state& line)
 void suppress_suggestions()
 {
     s_suggestion.suppress_suggestions();
+}
+
+//------------------------------------------------------------------------------
+bool has_suggestion()
+{
+    return s_suggestion.has_suggestion();
 }
 
 //------------------------------------------------------------------------------

commit 8fb36325da44594d9ceecb6535bcb8541e03af72

Fix mistaken suppression of some auto suggestions.
An off by one accidentally suppressed any suggestion anchored to the end
of the input line.

Regression introduced by 524d08977e4a9a4e0002b19ceb686f1a9b002e3f.

clink/lib/src/suggestions.cpp
@@ -90,7 +90,7 @@ bool suggestion_manager::get_visible(str_base& out, bool* includes_hint) const
     out.clear();
     if (!g_rl_buffer)
         return false;
-    if (m_suggestion_offset >= g_rl_buffer->get_length())
+    if (m_suggestion_offset > g_rl_buffer->get_length())
         return false;
 
     // Do not allow relaxed comparison for suggestions, as it is too confusing,