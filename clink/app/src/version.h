aa75c6154cba928b6bc12b38f8dcd22f00cb3b43
Chris Antos <sparrowhawk996@gmail.com>
2025-11-22 13:16:09 -0800

    Update version to v1.9.0

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -13,8 +13,8 @@
 #endif
 
 #define CLINK_VERSION_MAJOR     1
-#define CLINK_VERSION_MINOR     8
-#define CLINK_VERSION_PATCH     8
+#define CLINK_VERSION_MINOR     9
+#define CLINK_VERSION_PATCH     0
 
 #define ORIGINAL_COPYRIGHT_STR  "Copyright (c) 2012-2018 Martin Ridgers"
 #define CLINK_COPYRIGHT_STR     "Copyright (c) 2012-2018 Martin Ridgers, Portions Copyright (c) 2020-2025 Christopher Antos"
