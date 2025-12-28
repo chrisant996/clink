f497f94284347083096ef8a8d16f5d47cc2b9bc2
Chris Antos <sparrowhawk996@gmail.com>
2025-12-28 00:05:20 -0800

    Increment version.

--- clink/app/src/version.h
+++ clink/app/src/version.h
@@ -14,7 +14,7 @@
 
 #define CLINK_VERSION_MAJOR     1
 #define CLINK_VERSION_MINOR     9
-#define CLINK_VERSION_PATCH     6
+#define CLINK_VERSION_PATCH     7
 
 #define ORIGINAL_COPYRIGHT_STR  "Copyright (c) 2012-2018 Martin Ridgers"
 #define CLINK_COPYRIGHT_STR     "Copyright (c) 2012-2018 Martin Ridgers, Portions Copyright (c) 2020-2025 Christopher Antos"
