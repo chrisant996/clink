2fbed190d933a904a6fd4bcc1e9f1ac9ed487586
Chris Antos <sparrowhawk996@gmail.com>
2026-09-17 13:55:47 -0700

    Fix obscure input edge case.
    The input driver hasn't needed to check !m_buffer_count for a very long
    time (since before I took over maintenance).
    
    I think the case doesn't matter in practice because the input driver
    never queues more than one key sequence at a time into its buffer.  But
    porting the input driver to terminal-input-box highlighted that the
    condition is conceptually inaccurate).

--- clink/terminal/src/win_terminal_in.cpp
+++ clink/terminal/src/win_terminal_in.cpp
@@ -1525,13 +1525,13 @@ void win_terminal_in::process_input(KEY_EVENT_RECORD const& record, bool peek)
 
     // Special treatment for variations of tab and space. Do this before
     // clearing AltGr flags, otherwise ctrl-space gets converted into space.
-    if (key_vk == VK_TAB && (key_char == 0x09 || !key_char) && !m_buffer_count)
+    if (key_vk == VK_TAB && (key_char == 0x09 || !key_char))
         return push(terminfo::ktab[terminfo::keymod_index(key_flags)]);
-    if (key_vk == VK_SPACE && (key_char == 0x20 || !key_char) && !m_buffer_count)
+    if (key_vk == VK_SPACE && (key_char == 0x20 || !key_char))
         return push(terminfo::kspc[terminfo::keymod_index(key_flags)]);
 
     // Special treatment for enter + modifiers.
-    if (key_vk == VK_RETURN && key_flags && !m_buffer_count)
+    if (key_vk == VK_RETURN && key_flags)
         return push(terminfo::kret[terminfo::keymod_index(key_flags)]);
 
     // If the input was formed using AltGr or LeftAlt-LeftCtrl then things get
@@ -1542,7 +1542,7 @@ void win_terminal_in::process_input(KEY_EVENT_RECORD const& record, bool peek)
         key_flags &= ~(CTRL_PRESSED|ALT_PRESSED);
 
     // Special case for ctrl-shift-I (to behave like shift-tab aka. back-tab).
-    if (key_char == '\t' && !m_buffer_count && (key_flags & SHIFT_PRESSED) && !g_differentiate_keys.get())
+    if (key_char == '\t' && (key_flags & SHIFT_PRESSED) && !g_differentiate_keys.get())
         return push(terminfo::kcbt);
 
     // Function keys (kf1-kf48 from xterm+pcf2)
